f79fdf1a7b5fc93b23e94c06d58723e76141335b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-15 23:05:05 +0000

    Add package abusify-id/0.2/abusify-id-0.2.tar.gz

---
+++ packages/abusify-id/abusify-id-0.2.tar.gz/abusify-id-0.2/abusify_id/abusiveness_detector.py
@@ -0,0 +1,31 @@
+import pickle
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+module_dir = os.path.dirname(__file__)
+
+model_path = os.path.join(module_dir, 'model.pkl')
+vectorizer_path = os.path.join(module_dir, 'tfidf_vectorizer.pkl')
+
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open(vectorizer_path, 'rb') as vectorizer_file:
+    tfidf_vectorizer = pickle.load(vectorizer_file)
+
+def predict_abusiveness(text):
+    tfidf_text = tfidf_vectorizer.transform([text])
+    abusiveness_score = model.predict_proba(tfidf_text)[0]
+    return abusiveness_score[0], abusiveness_score[1]
+
+def predict_abusiveness_file(input_file, output_file):
+    with open(input_file, 'r') as file:
+        kalimat_list = file.read().splitlines()
+
+    with open(output_file, 'w') as output:
+        for kalimat in kalimat_list:
+            tfidf_text = tfidf_vectorizer.transform([kalimat])
+            abusiveness_score = model.predict_proba(tfidf_text)[0]
+            output.write(f"{kalimat}\n")
+            output.write(f"Positive: {abusiveness_score[0]:.2f}, Negative: {abusiveness_score[1]:.2f}\n\n")
+    print(f"The abusiveness prediction results have been saved in a file: {output_file}")
---
+++ packages/abusify-id/abusify-id-0.2.tar.gz/abusify-id-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abusify-id',
+    version='0.2',
+    packages=find_packages(),
+    package_data={
+        'abusify_id': ['model.pkl', 'tfidf_vectorizer.pkl'],
+    },
+    install_requires=[
+        'scikit-learn',
+        'pandas',
+        'nltk',
+    ],
+)
