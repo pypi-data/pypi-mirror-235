8fba739e0ac2809f8eca0ed075772b63d69f82b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-15 22:39:14 +0000

    Add package looperator/1.2.0/looperator-1.2.0.tar.gz

---
+++ packages/looperator/looperator-1.2.0.tar.gz/looperator-1.2.0/looperator/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+from looperator.operator import *
+from looperator.operation import *
+from looperator.record import *
+from looperator.process import *
+from looperator.handler import *
