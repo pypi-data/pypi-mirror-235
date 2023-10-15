363eb82bd5719aa81494619b609c4744cb2dc0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-15 22:39:03 +0000

    Add package browsr/1.17.1/browsr-1.17.1.tar.gz

---
+++ packages/browsr/browsr-1.17.1.tar.gz/browsr-1.17.1/browsr/__init__.py
@@ -0,0 +1,8 @@
+"""
+browsr
+"""
+
+from .browsr import Browsr
+from .universal_directory_tree import BrowsrDirectoryTree
+
+__all__ = ["Browsr", "BrowsrDirectoryTree"]
---
+++ packages/browsr/browsr-1.17.1.tar.gz/browsr-1.17.1/browsr/universal_directory_tree.py
@@ -0,0 +1,65 @@
+"""
+A universal directory tree widget for Textual.
+"""
+
+from __future__ import annotations
+
+from typing import ClassVar, Iterable, List, Optional, Type
+
+from textual.binding import BindingType
+from textual.widgets._directory_tree import DirEntry
+from textual.widgets._tree import TreeNode
+from textual_universal_directorytree import UniversalDirectoryTree
+from upath import UPath as Path
+
+from browsr._base import BrowsrPath, vim_cursor_bindings
+
+
+class BrowsrDirectoryTree(UniversalDirectoryTree):
+    """
+    A DirectoryTree that can handle any filesystem.
+    """
+
+    PATH: Type[BrowsrPath] = BrowsrPath
+
+    BINDINGS: ClassVar[List[BindingType]] = [
+        *UniversalDirectoryTree.BINDINGS,
+        *vim_cursor_bindings,
+    ]
+
+    @classmethod
+    def _handle_top_level_bucket(cls, dir_path: Path) -> Optional[Iterable[Path]]:
+        """
+        Handle scenarios when someone wants to browse all of s3
+
+        This is because S3FS handles the root directory differently than other filesystems
+        """
+        if str(dir_path) == "s3:/":
+            sub_buckets = sorted(
+                Path(f"s3://{bucket.name}") for bucket in dir_path.iterdir()
+            )
+            return sub_buckets
+        return None
+
+    def _populate_node(self, node: TreeNode[DirEntry], content: Iterable[Path]) -> None:  # type: ignore[override]
+        """
+        Populate the given tree node with the given directory content.
+
+        This function overrides the original textual method to handle root level
+        cloud buckets.
+        """
+        top_level_buckets = self._handle_top_level_bucket(dir_path=node.data.path)  # type: ignore[union-attr, arg-type]
+        if top_level_buckets is not None:
+            content = top_level_buckets
+        node.remove_children()
+        for path in content:
+            if top_level_buckets is not None:
+                path_name = str(path).replace("s3://", "").rstrip("/")
+            else:
+                path_name = path.name
+            node.add(
+                path_name,
+                data=DirEntry(path),
+                allow_expand=self._safe_is_dir(path),
+            )
+        node.expand()
