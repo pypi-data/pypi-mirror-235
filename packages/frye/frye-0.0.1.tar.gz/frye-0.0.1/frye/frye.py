f722c4bb596824b9bd2ebf866065b623b76823cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-15 22:39:27 +0000

    Add package frye/0.0.1/frye-0.0.1.tar.gz

---
+++ packages/frye/frye-0.0.1.tar.gz/frye-0.0.1/frye/frye.py
@@ -0,0 +1,10 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Start Frye')
+parser.add_argument('-m', '--message', default='Message', help='Passes a Message')
+
+args = parser.parse_args()
+
+def hello_world():
+    print(args.message)
