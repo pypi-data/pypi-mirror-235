4f75f12ca91676b3b49f18f14adf2d9bf83dc07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 00:05:56 +0000

    Add package pybatteryid/1.0.0/pybatteryid-1.0.0.tar.gz

---
+++ packages/pybatteryid/pybatteryid-1.0.0.tar.gz/pybatteryid-1.0.0/setup.py
@@ -0,0 +1,26 @@
+"""Setup for the PyBatteryID package"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='pybatteryid',
+    version='1.0.0',
+    description='Data-driven Battery Model Identification in Python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url="https://github.com/muizabdul29/PyBatteryID",
+    packages=find_packages(include=['pybatteryid', 'pybatteryid.*']),
+    install_requires=[
+        'numpy>=1.20.0',
+        'cvxopt',
+        'scikit-learn',
+        'rich'
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
