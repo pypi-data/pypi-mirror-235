87c0b7b0cbf336125e381dd614a7728496d8dd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:07:29 +0000

    Add package gdsfactory/7.8.9/gdsfactory-7.8.9-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-7.8.9-py3-none-any.whl/gdsfactory/components/ring_double.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import gdsfactory as gf
+from gdsfactory.component import Component
+from gdsfactory.components.bend_euler import bend_euler
+from gdsfactory.components.coupler_ring import coupler_ring as coupler_ring_function
+from gdsfactory.components.straight import straight
+from gdsfactory.typings import ComponentSpec, CrossSectionSpec
+
+
+@gf.cell
+def ring_double(
+    gap: float = 0.2,
+    radius: float = 10.0,
+    length_x: float = 0.01,
+    length_y: float = 0.01,
+    coupler_ring: ComponentSpec = coupler_ring_function,
+    bend: ComponentSpec = bend_euler,
+    cross_section: CrossSectionSpec = "xs_sc",
+    **kwargs,
+) -> Component:
+    """Returns a double bus ring.
+
+    two couplers (ct: top, cb: bottom)
+    connected with two vertical straights (sl: left, sr: right)
+
+    Args:
+        gap: gap between for coupler.
+        radius: for the bend and coupler.
+        length_x: ring coupler length.
+        length_y: vertical straight length.
+        coupler: ring coupler spec.
+        bend: bend spec.
+        cross_section: cross_section spec.
+        **kwargs: cross_section settings.
+
+    .. code::
+
+         --==ct==--
+          |      |
+          sl     sr length_y
+          |      |
+         --==cb==-- gap
+
+          length_x
+    """
+    gap = gf.snap.snap_to_grid(gap, grid_factor=2)
+    xs = gf.get_cross_section(cross_section, **kwargs)
+    radius = radius or xs.radius
+    cross_section = xs.copy(radius=radius)
+
+    coupler_component = gf.get_component(
+        coupler_ring,
+        gap=gap,
+        radius=radius,
+        length_x=length_x,
+        bend=bend,
+        cross_section=cross_section,
+    )
+    straight_component = straight(length=length_y, cross_section=cross_section)
+
+    c = Component()
+    cb = c.add_ref(coupler_component)
+    ct = c.add_ref(coupler_component)
+
+    sl = straight_component.ref()
+    sr = straight_component.ref()
+
+    if length_y > 0:
+        c.add(sl)
+        c.add(sr)
+
+    sl.connect(port="o1", destination=cb.ports["o2"])
+    ct.connect(port="o3", destination=sl.ports["o2"])
+    sr.connect(port="o2", destination=ct.ports["o2"])
+    c.add_port("o1", port=cb.ports["o1"])
+    c.add_port("o2", port=cb.ports["o4"])
+    c.add_port("o3", port=ct.ports["o4"])
+    c.add_port("o4", port=ct.ports["o1"])
+    return c
+
+
+if __name__ == "__main__":
+    c = ring_double(length_y=0, length_x=0, layer=(2, 0))
+    c.get_netlist()
+    c.show(show_subports=False)
