12319e4c82e1aa9a89daf2af186f46b0798b2e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 07:05:36 +0000

    Add package redys/0.9.20/redys-0.9.20.tar.gz

---
+++ packages/redys/redys-0.9.20.tar.gz/redys-0.9.20/redys/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2023 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htagweb
+# #############################################################################
+
+from .redys import Server,Client,AClient
+
+__version__ = "0.9.20" # auto-updated
+
+__all__ = ["Server","Client","AClient"]
+
+
