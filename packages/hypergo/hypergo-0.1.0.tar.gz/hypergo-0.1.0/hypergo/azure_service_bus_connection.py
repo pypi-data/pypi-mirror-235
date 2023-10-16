97823d32e95b658deae910dab7f949897a23a67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:50:41 +0000

    Add package hypergo/0.1.0/hypergo-0.1.0.tar.gz

---
+++ packages/hypergo/hypergo-0.1.0.tar.gz/hypergo-0.1.0/hypergo/azure_service_bus_connection.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+import azure.functions as func
+from azure.servicebus import (ServiceBusClient, ServiceBusMessage,
+                              ServiceBusSender)
+
+from hypergo.config import ConfigType
+from hypergo.connection import Connection
+from hypergo.message import Message, MessageType
+from hypergo.storage import Storage
+
+
+class AzureServiceBusConnection(Connection):
+    def __init__(self, conn_str: str) -> None:
+        self._service_bus_client: ServiceBusClient = ServiceBusClient.from_connection_string(conn_str)
+
+    def send(self, message: MessageType, namespace: str) -> None:
+        azure_message: ServiceBusMessage = Message.to_azure_service_bus_service_bus_message(message)
+        with self._service_bus_client:
+            sender: ServiceBusSender = self._service_bus_client.get_topic_sender(namespace)
+            sender.send_messages(azure_message)
+
+    def consume(self, azure_message: func.ServiceBusMessage, config: ConfigType, storage: Union[Storage, None]) -> None:
+        message: MessageType = Message.from_azure_functions_service_bus_message(azure_message)
+        self.general_consume(message, config, storage)
