bc0d5d070ccb4b7c37295dade901bbd62fdbaa49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:03 +0000

    Add package bradleystevenson2015-webscraper/0.0.53/bradleystevenson2015_webscraper-0.0.53.tar.gz

---
+++ packages/bradleystevenson2015-webscraper/bradleystevenson2015_webscraper-0.0.53.tar.gz/bradleystevenson2015_webscraper-0.0.53/src/bradleystevenson2015_webscraper/url_generator.py
@@ -0,0 +1,22 @@
+class URLGenerator:
+
+    def __init__(self, base_url, iterator=None):
+        self.iterator = iterator
+        self.base_url = base_url
+
+    def generate_urls(self, webscraper_object_collection):
+        return_strings = []
+        if self.iterator is None:
+            return_strings.append(self.base_url)
+            return return_strings
+
+class URLGeneratorFactory:
+
+    def __init__(self, url_dict):
+        self.url_dict = url_dict
+
+    def get_url_generator(self):
+        if 'iterator' in self.url_dict.keys():
+            pass
+        else:
+            return URLGenerator(self.url_dict['base_url'])
