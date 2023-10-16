3e4b649f9a1a3bbda7037b55e4fb269a51a52f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 18:10:25 +0000

    Add package qgate-perf/0.4.1rc5/qgate_perf-0.4.1rc5-py3-none-any.whl

---
+++ packages/qgate-perf/qgate_perf-0.4.1rc5-py3-none-any.whl/qgate_perf/run_return.py
@@ -0,0 +1,19 @@
+
+class RunReturn:
+    """Wrapper for return data from executor"""
+    def __init__(self, return_key, return_dict):
+        self._return_key=return_key
+        self._return_dict=return_dict
+        # experimental initial dictionar value
+        self.probe="init"
+
+    @property
+    def probe(self):
+        return self._return_dict[self._return_key]
+
+    @probe.setter
+    def probe(self, ret):
+       self._return_dict[self._return_key]=ret
+
+    def __str__(self):
+        return f"{self._return_key}={self._return_dict[self._return_key]}"
