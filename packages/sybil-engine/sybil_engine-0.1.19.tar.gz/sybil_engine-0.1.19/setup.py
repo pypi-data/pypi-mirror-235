7853a40ee0e3057b561151782b4900fd5b4f4ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 03:05:11 +0000

    Add package sybil-engine/0.1.19/sybil_engine-0.1.19.tar.gz

---
+++ packages/sybil-engine/sybil_engine-0.1.19.tar.gz/sybil_engine-0.1.19/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sybil_engine',
+    version='0.1.19',
+    py_modules=['sybil_engine'],
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Arsenii Venherak',
+    author_email='indeooars@gmail.com',
+    description='A brief description of your package.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Indeoo/sybil-engine',
+)
