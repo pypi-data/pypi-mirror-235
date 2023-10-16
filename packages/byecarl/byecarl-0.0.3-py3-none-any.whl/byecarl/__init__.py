ccc60c4a477275ab13665bca301b036b54dfa686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:13:40 +0000

    Add package byecarl/0.0.3/byecarl-0.0.3-py3-none-any.whl

---
+++ packages/byecarl/byecarl-0.0.3-py3-none-any.whl/byecarl/__init__.py
@@ -0,0 +1,2 @@
+print("Thanks for being a great mentor and teaching me so much over your time at Aginic. I really enjoyed working with you and I’ll miss all the random things you show me. Keep doing all the awesome stuff you do and I hope you enjoy Macquarie and get to move to the UK down the line :P")
+print(' - Jordan')
