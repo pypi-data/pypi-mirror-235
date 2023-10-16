05a979f789149d866f2e287918bf7fd8b27eb9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:08:43 +0000

    Add package swiftgalaxy/1.0.5/swiftgalaxy-1.0.5-py3-none-any.whl

---
+++ packages/swiftgalaxy/swiftgalaxy-1.0.5-py3-none-any.whl/swiftgalaxy/__init__.py
@@ -0,0 +1,6 @@
+from .reader import SWIFTGalaxy
+from .halo_finders import Velociraptor, Caesar, Standalone
+from .masks import MaskCollection
+from .__version__ import __version__
+
+name = "swiftgalaxy"
