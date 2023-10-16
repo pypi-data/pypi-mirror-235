5c268324d5f17226bb5f9202fd32cd3a578e210b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:05:05 +0000

    Add package aimos/0.0.1/aimos-0.0.1.tar.gz

---
+++ packages/aimos/aimos-0.0.1.tar.gz/aimos-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aimos',  # Replace with your desired project name
+    version='0.0.1',
+    packages=find_packages(),
+    author='AimStack',  # Replace with your name or organization's name
+    author_email='gor@aimhub.io',  # Replace with your email
+    description='A placeholder package to reserve the project name on PyPI.',
+    long_description='This is a placeholder package for reserving the project name on PyPI. Future updates will provide the actual functionality.',
+    url='https://github.com/aimhubio/aimos',  # Replace with the URL to your project, if applicable
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # Update if you're using a different license
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',  # Adjust based on your requirements
+)
+
