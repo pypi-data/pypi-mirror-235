cbb6ba2b978a162bef2016fb1b22ff789d11f4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:34 +0000

    Add package pyhaloxml/2.5.4/pyhaloxml-2.5.4-cp310-abi3-win_amd64.whl

---
+++ packages/pyhaloxml/pyhaloxml-2.5.4-cp310-abi3-win_amd64.whl/pyhaloxml/shapely/to_shapely.py
@@ -0,0 +1,34 @@
+from ..Layer import Layer
+from ..Region import Region
+from ..misc import RegionType
+
+try:
+    import shapely.geometry as sg
+except ImportError as e:
+    raise ImportError(
+        "Shapely is not installed. Cannot use the shapely converters of haloxml."
+    )
+
+
+def region_to_shapely(region: Region) -> sg.Polygon:
+    """
+    Return the region as a shapeply polygon
+    :return:
+    """
+    if region.type == RegionType.Ruler:
+        polygon = sg.LineString(region.getvertices())
+    else:
+        polygon = sg.Polygon(
+            region.getvertices(), [x.getvertices() for x in region.holes]
+        )
+    return polygon
+
+
+def layer_to_shapely(layer: Layer) -> sg.MultiPolygon:
+    """
+    Return the layer as shaply multipolygon
+
+    :return: A shapely multipolygon contain all the regions in this layer.
+    """
+    polygons = [region_to_shapely(x) for x in layer.regions]
+    return sg.MultiPolygon(polygons)
