b3855fbd803098aeb17ed179d558d2c0a3316716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:07:48 +0000

    Add package rlviser-py/0.5.0/rlviser_py-0.5.0-cp37-abi3-macosx_10_7_x86_64.whl

---
+++ packages/rlviser-py/rlviser_py-0.5.0-cp37-abi3-macosx_10_7_x86_64.whl/rlviser_py/__init__.pyi
@@ -0,0 +1,23 @@
+from typing import List, Tuple
+
+def set_boost_pad_locations(locations: List[Tuple[float, float, float]]) -> ...:
+    pass
+
+try:
+    from RocketSim import BallState, CarConfig, CarState, Team
+
+    def render(tick_count: int, tick_rate: float, boost_pad_states: List[bool], ball: BallState, cars: List[Tuple[int, Team, CarConfig, CarState]]) -> ...:
+        pass
+except ImportError:
+    pass
+
+try:
+    from rlgym.rocket_league.engine.game_state import GameState
+
+    def render_rlgym(tick_count: int, tick_rate: float, gym_state: GameState) -> ...:
+        pass
+except ImportError:
+    pass
+
+def quit() -> ...:
+    pass
