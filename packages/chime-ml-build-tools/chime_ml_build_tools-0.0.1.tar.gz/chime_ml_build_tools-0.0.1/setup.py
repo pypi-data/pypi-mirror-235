c6408d6c33b43b410cf6183e0a5b0d65904e8fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:11 +0000

    Add package chime-ml-build-tools/0.0.1/chime_ml_build_tools-0.0.1.tar.gz

---
+++ packages/chime-ml-build-tools/chime_ml_build_tools-0.0.1.tar.gz/chime_ml_build_tools-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime_ml_build_tools',
+    packages=find_packages(include=['chime_ml_build_tools']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
