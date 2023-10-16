6f7285b38d6708c706d0f5f9c0f1602b87714c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:08:02 +0000

    Add package python-antsy/0.0.8/python-antsy-0.0.8.tar.gz

---
+++ packages/python-antsy/python-antsy-0.0.8.tar.gz/python-antsy-0.0.8/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-antsy",
+    version="0.0.8",
+    description="Antsy API Python Client",
+    long_description=open("README.md").read().strip(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Juan F. Duque",
+    author_email="jfelipe@grupodyd.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/grupodyd/python-antsy",
+    keywords="antsy",
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+)
---
+++ packages/python-antsy/python-antsy-0.0.8.tar.gz/python-antsy-0.0.8/src/python-antsy/client.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+
+class AntsyClient(object):
+
+    def __init__(self):
+        pass
