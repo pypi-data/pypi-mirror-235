59320a7b49b9a2a2b00854d1e8a66aaab8793181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 18:07:44 +0000

    Add package latestTouki/1.0.0/latestTouki-1.0.0-py3-none-any.whl

---
+++ packages/latestTouki/latestTouki-1.0.0-py3-none-any.whl/latestTouki/__init__.py
@@ -0,0 +1,10 @@
+from morita_toukiDateList_to_Latestyyyymmdd import *
+
+__copyright__    = 'Copyright (C) 2023 Takao Morita'
+__version__      = '1.0.0'
+__license__      = 'MIT'
+__author__       = 'Takao Morita'
+__author_email__ = 'tmolita077@gmail.com'
+__url__          = 'https://github.com/taotaotao3/latestTouki'
+
+__all__ = ['pandas', 'io', 're']
