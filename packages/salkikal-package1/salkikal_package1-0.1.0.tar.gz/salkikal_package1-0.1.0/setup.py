12c3b0a31dc0c1d2f7e730097c4cd63bcbb259e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:09:04 +0000

    Add package salkikal-package1/0.1.0/salkikal_package1-0.1.0.tar.gz

---
+++ packages/salkikal-package1/salkikal_package1-0.1.0.tar.gz/salkikal_package1-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="salkikal_package1",
+    version="0.1.0",
+    description="A package with a Base class",
+    author="Salome Kikalishvili",
+    author_email="salomekikalishvili2000@gmail.com",
+    packages=["salkikal_package1"]
+)
