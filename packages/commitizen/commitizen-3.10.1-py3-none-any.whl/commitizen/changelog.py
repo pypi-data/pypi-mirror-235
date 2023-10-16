426defc11f12991b198087c7e42206177ebde191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:55 +0000

    Add package commitizen/3.10.1/commitizen-3.10.1-py3-none-any.whl

---
+++ packages/commitizen/commitizen-3.10.1-py3-none-any.whl/commitizen/changelog.py
@@ -0,0 +1,382 @@
+"""Design
+
+## Metadata CHANGELOG.md
+
+1. Identify irrelevant information (possible: changelog title, first paragraph)
+2. Identify Unreleased area
+3. Identify latest version (to be able to write on top of it)
+
+## Parse git log
+
+1. get commits between versions
+2. filter commits with the current cz rules
+3. parse commit information
+4. yield tree nodes
+5. format tree nodes
+6. produce full tree
+7. generate changelog
+
+Extra:
+- [x] Generate full or partial changelog
+- [x] Include in tree from file all the extra comments added manually
+- [x] Add unreleased value
+- [x] hook after message is parsed (add extra information like hyperlinks)
+- [x] hook after changelog is generated (api calls)
+- [x] add support for change_type maps
+"""
+from __future__ import annotations
+
+import os
+import re
+from collections import OrderedDict, defaultdict
+from datetime import date
+from typing import TYPE_CHECKING, Callable, Iterable, Type, cast
+
+from jinja2 import Environment, PackageLoader
+
+from commitizen import out
+from commitizen.bump import normalize_tag
+from commitizen.defaults import encoding
+from commitizen.exceptions import InvalidConfigurationError, NoCommitsFoundError
+from commitizen.git import GitCommit, GitTag
+from commitizen.version_schemes import (
+    DEFAULT_SCHEME,
+    BaseVersion,
+    InvalidVersion,
+    Pep440,
+)
+
+if TYPE_CHECKING:
+    from commitizen.version_schemes import VersionScheme
+
+
+def get_commit_tag(commit: GitCommit, tags: list[GitTag]) -> GitTag | None:
+    return next((tag for tag in tags if tag.rev == commit.rev), None)
+
+
+def tag_included_in_changelog(
+    tag: GitTag,
+    used_tags: list,
+    merge_prerelease: bool,
+    scheme: VersionScheme = DEFAULT_SCHEME,
+) -> bool:
+    if tag in used_tags:
+        return False
+
+    try:
+        version = scheme(tag.name)
+    except InvalidVersion:
+        return False
+
+    if merge_prerelease and version.is_prerelease:
+        return False
+
+    return True
+
+
+def get_version_tags(scheme: Type[BaseVersion], tags: list[GitTag]) -> list[GitTag]:
+    valid_tags: list[GitTag] = []
+    for tag in tags:
+        try:
+            scheme(tag.name)
+        except InvalidVersion:
+            out.warn(f"InvalidVersion {tag}")
+        else:
+            valid_tags.append(tag)
+
+    return valid_tags
+
+
+def generate_tree_from_commits(
+    commits: list[GitCommit],
+    tags: list[GitTag],
+    commit_parser: str,
+    changelog_pattern: str,
+    unreleased_version: str | None = None,
+    change_type_map: dict[str, str] | None = None,
+    changelog_message_builder_hook: Callable | None = None,
+    merge_prerelease: bool = False,
+    scheme: VersionScheme = DEFAULT_SCHEME,
+) -> Iterable[dict]:
+    pat = re.compile(changelog_pattern)
+    map_pat = re.compile(commit_parser, re.MULTILINE)
+    body_map_pat = re.compile(commit_parser, re.MULTILINE | re.DOTALL)
+    current_tag: GitTag | None = None
+
+    # Check if the latest commit is not tagged
+    if commits:
+        latest_commit = commits[0]
+        current_tag = get_commit_tag(latest_commit, tags)
+
+    current_tag_name: str = unreleased_version or "Unreleased"
+    current_tag_date: str = ""
+    if unreleased_version is not None:
+        current_tag_date = date.today().isoformat()
+    if current_tag is not None and current_tag.name:
+        current_tag_name = current_tag.name
+        current_tag_date = current_tag.date
+
+    changes: dict = defaultdict(list)
+    used_tags: list = [current_tag]
+    for commit in commits:
+        commit_tag = get_commit_tag(commit, tags)
+
+        if commit_tag is not None and tag_included_in_changelog(
+            commit_tag, used_tags, merge_prerelease, scheme=scheme
+        ):
+            used_tags.append(commit_tag)
+            yield {
+                "version": current_tag_name,
+                "date": current_tag_date,
+                "changes": changes,
+            }
+            current_tag_name = commit_tag.name
+            current_tag_date = commit_tag.date
+            changes = defaultdict(list)
+
+        matches = pat.match(commit.message)
+        if not matches:
+            continue
+
+        # Process subject from commit message
+        message = map_pat.match(commit.message)
+        if message:
+            parsed_message: dict = message.groupdict()
+            # change_type becomes optional by providing None
+            change_type = parsed_message.pop("change_type", None)
+
+            if change_type_map:
+                change_type = change_type_map.get(change_type, change_type)
+            if changelog_message_builder_hook:
+                parsed_message = changelog_message_builder_hook(parsed_message, commit)
+            changes[change_type].append(parsed_message)
+
+        # Process body from commit message
+        body_parts = commit.body.split("\n\n")
+        for body_part in body_parts:
+            message_body = body_map_pat.match(body_part)
+            if not message_body:
+                continue
+            parsed_message_body: dict = message_body.groupdict()
+
+            change_type = parsed_message_body.pop("change_type", None)
+            if change_type_map:
+                change_type = change_type_map.get(change_type, change_type)
+            changes[change_type].append(parsed_message_body)
+
+    yield {"version": current_tag_name, "date": current_tag_date, "changes": changes}
+
+
+def order_changelog_tree(tree: Iterable, change_type_order: list[str]) -> Iterable:
+    if len(set(change_type_order)) != len(change_type_order):
+        raise InvalidConfigurationError(
+            f"Change types contain duplicates types ({change_type_order})"
+        )
+
+    sorted_tree = []
+    for entry in tree:
+        ordered_change_types = change_type_order + sorted(
+            set(entry["changes"].keys()) - set(change_type_order)
+        )
+        changes = [
+            (ct, entry["changes"][ct])
+            for ct in ordered_change_types
+            if ct in entry["changes"]
+        ]
+        sorted_tree.append({**entry, **{"changes": OrderedDict(changes)}})
+    return sorted_tree
+
+
+def render_changelog(tree: Iterable) -> str:
+    loader = PackageLoader("commitizen", "templates")
+    env = Environment(loader=loader, trim_blocks=True)
+    jinja_template = env.get_template("keep_a_changelog_template.j2")
+    changelog: str = jinja_template.render(tree=tree)
+    return changelog
+
+
+def parse_version_from_markdown(
+    value: str, scheme: VersionScheme = Pep440
+) -> str | None:
+    if not value.startswith("#"):
+        return None
+    m = scheme.parser.search(value)
+    if not m:
+        return None
+    return cast(str, m.group("version"))
+
+
+def parse_title_type_of_line(value: str) -> str | None:
+    md_title_parser = r"^(?P<title>#+)"
+    m = re.search(md_title_parser, value)
+    if not m:
+        return None
+    return m.groupdict().get("title")
+
+
+def get_metadata(
+    filepath: str, scheme: VersionScheme = Pep440, encoding: str = encoding
+) -> dict:
+    unreleased_start: int | None = None
+    unreleased_end: int | None = None
+    unreleased_title: str | None = None
+    latest_version: str | None = None
+    latest_version_position: int | None = None
+    if not os.path.isfile(filepath):
+        return {
+            "unreleased_start": None,
+            "unreleased_end": None,
+            "latest_version": None,
+            "latest_version_position": None,
+        }
+
+    with open(filepath, "r", encoding=encoding) as changelog_file:
+        for index, line in enumerate(changelog_file):
+            line = line.strip().lower()
+
+            unreleased: str | None = None
+            if "unreleased" in line:
+                unreleased = parse_title_type_of_line(line)
+            # Try to find beginning and end lines of the unreleased block
+            if unreleased:
+                unreleased_start = index
+                unreleased_title = unreleased
+                continue
+            elif (
+                isinstance(unreleased_title, str)
+                and parse_title_type_of_line(line) == unreleased_title
+            ):
+                unreleased_end = index
+
+            # Try to find the latest release done
+            version = parse_version_from_markdown(line, scheme)
+            if version:
+                latest_version = version
+                latest_version_position = index
+                break  # there's no need for more info
+        if unreleased_start is not None and unreleased_end is None:
+            unreleased_end = index
+    return {
+        "unreleased_start": unreleased_start,
+        "unreleased_end": unreleased_end,
+        "latest_version": latest_version,
+        "latest_version_position": latest_version_position,
+    }
+
+
+def incremental_build(new_content: str, lines: list[str], metadata: dict) -> list[str]:
+    """Takes the original lines and updates with new_content.
+
+    The metadata governs how to remove the old unreleased section and where to place the
+    new content.
+
+    Args:
+        lines: The lines from the changelog
+        new_content: This should be placed somewhere in the lines
+        metadata: Information about the changelog
+
+    Returns:
+        Updated lines
+    """
+    unreleased_start = metadata.get("unreleased_start")
+    unreleased_end = metadata.get("unreleased_end")
+    latest_version_position = metadata.get("latest_version_position")
+    skip = False
+    output_lines: list[str] = []
+    for index, line in enumerate(lines):
+        if index == unreleased_start:
+            skip = True
+        elif index == unreleased_end:
+            skip = False
+            if (
+                latest_version_position is None
+                or isinstance(latest_version_position, int)
+                and isinstance(unreleased_end, int)
+                and latest_version_position > unreleased_end
+            ):
+                continue
+
+        if skip:
+            continue
+
+        if index == latest_version_position:
+            output_lines.extend([new_content, "\n"])
+
+        output_lines.append(line)
+    if not isinstance(latest_version_position, int):
+        if output_lines and output_lines[-1].strip():
+            # Ensure at least one blank line between existing and new content.
+            output_lines.append("\n")
+        output_lines.append(new_content)
+    return output_lines
+
+
+def get_smart_tag_range(
+    tags: list[GitTag], newest: str, oldest: str | None = None
+) -> list[GitTag]:
+    """Smart because it finds the N+1 tag.
+
+    This is because we need to find until the next tag
+    """
+    accumulator = []
+    keep = False
+    if not oldest:
+        oldest = newest
+    for index, tag in enumerate(tags):
+        if tag.name == newest:
+            keep = True
+        if keep:
+            accumulator.append(tag)
+        if tag.name == oldest:
+            keep = False
+            try:
+                accumulator.append(tags[index + 1])
+            except IndexError:
+                pass
+            break
+    return accumulator
+
+
+def get_oldest_and_newest_rev(
+    tags: list[GitTag],
+    version: str,
+    tag_format: str,
+    scheme: VersionScheme | None = None,
+) -> tuple[str | None, str | None]:
+    """Find the tags for the given version.
+
+    `version` may come in different formats:
+    - `0.1.0..0.4.0`: as a range
+    - `0.3.0`: as a single version
+    """
+    oldest: str | None = None
+    newest: str | None = None
+    try:
+        oldest, newest = version.split("..")
+    except ValueError:
+        newest = version
+
+    newest_tag = normalize_tag(newest, tag_format=tag_format, scheme=scheme)
+
+    oldest_tag = None
+    if oldest:
+        oldest_tag = normalize_tag(oldest, tag_format=tag_format, scheme=scheme)
+
+    tags_range = get_smart_tag_range(tags, newest=newest_tag, oldest=oldest_tag)
+    if not tags_range:
+        raise NoCommitsFoundError("Could not find a valid revision range.")
+
+    oldest_rev: str | None = tags_range[-1].name
+    newest_rev = newest_tag
+
+    # check if it's the first tag created
+    # and it's also being requested as part of the range
+    if oldest_rev == tags[-1].name and oldest_rev == oldest_tag:
+        return None, newest_rev
+
+    # when they are the same, and it's also the
+    # first tag created
+    if oldest_rev == newest_rev:
+        return None, newest_rev
+
+    return oldest_rev, newest_rev
---
+++ packages/commitizen/commitizen-3.10.1-py3-none-any.whl/commitizen/commands/changelog.py
@@ -0,0 +1,198 @@
+from __future__ import annotations
+
+import os.path
+from difflib import SequenceMatcher
+from operator import itemgetter
+from typing import Callable
+
+from commitizen import bump, changelog, defaults, factory, git, out
+from commitizen.config import BaseConfig
+from commitizen.exceptions import (
+    DryRunExit,
+    NoCommitsFoundError,
+    NoPatternMapError,
+    NoRevisionError,
+    NotAGitProjectError,
+    NotAllowed,
+)
+from commitizen.git import GitTag, smart_open
+from commitizen.version_schemes import get_version_scheme
+
+
+class Changelog:
+    """Generate a changelog based on the commit history."""
+
+    def __init__(self, config: BaseConfig, args):
+        if not git.is_git_project():
+            raise NotAGitProjectError()
+
+        self.config: BaseConfig = config
+        self.encoding = self.config.settings["encoding"]
+        self.cz = factory.commiter_factory(self.config)
+
+        self.start_rev = args.get("start_rev") or self.config.settings.get(
+            "changelog_start_rev"
+        )
+        self.file_name = args.get("file_name") or self.config.settings.get(
+            "changelog_file"
+        )
+        self.incremental = args["incremental"] or self.config.settings.get(
+            "changelog_incremental"
+        )
+        self.dry_run = args["dry_run"]
+
+        self.scheme = get_version_scheme(self.config, args.get("version_scheme"))
+
+        current_version = (
+            args.get("current_version", config.settings.get("version")) or ""
+        )
+        self.current_version = self.scheme(current_version) if current_version else None
+
+        self.unreleased_version = args["unreleased_version"]
+        self.change_type_map = (
+            self.config.settings.get("change_type_map") or self.cz.change_type_map
+        )
+        self.change_type_order = (
+            self.config.settings.get("change_type_order")
+            or self.cz.change_type_order
+            or defaults.change_type_order
+        )
+        self.rev_range = args.get("rev_range")
+        self.tag_format: str = (
+            args.get("tag_format") or self.config.settings["tag_format"]
+        )
+        self.merge_prerelease = args.get(
+            "merge_prerelease"
+        ) or self.config.settings.get("changelog_merge_prerelease")
+
+    def _find_incremental_rev(self, latest_version: str, tags: list[GitTag]) -> str:
+        """Try to find the 'start_rev'.
+
+        We use a similarity approach. We know how to parse the version from the markdown
+        changelog, but not the whole tag, we don't even know how's the tag made.
+
+        This 'smart' function tries to find a similarity between the found version number
+        and the available tag.
+
+        The SIMILARITY_THRESHOLD is an empirical value, it may have to be adjusted based
+        on our experience.
+        """
+        SIMILARITY_THRESHOLD = 0.89
+        tag_ratio = map(
+            lambda tag: (SequenceMatcher(None, latest_version, tag.name).ratio(), tag),
+            tags,
+        )
+        try:
+            score, tag = max(tag_ratio, key=itemgetter(0))
+        except ValueError:
+            raise NoRevisionError()
+        if score < SIMILARITY_THRESHOLD:
+            raise NoRevisionError()
+        start_rev = tag.name
+        return start_rev
+
+    def write_changelog(
+        self, changelog_out: str, lines: list[str], changelog_meta: dict
+    ):
+        if not isinstance(self.file_name, str):
+            raise NotAllowed(
+                "Changelog file name is broken.\n"
+                "Check the flag `--file-name` in the terminal "
+                f"or the setting `changelog_file` in {self.config.path}"
+            )
+
+        changelog_hook: Callable | None = self.cz.changelog_hook
+        with smart_open(self.file_name, "w", encoding=self.encoding) as changelog_file:
+            partial_changelog: str | None = None
+            if self.incremental:
+                new_lines = changelog.incremental_build(
+                    changelog_out, lines, changelog_meta
+                )
+                changelog_out = "".join(new_lines)
+                partial_changelog = changelog_out
+
+            if changelog_hook:
+                changelog_out = changelog_hook(changelog_out, partial_changelog)
+            changelog_file.write(changelog_out)
+
+    def __call__(self):
+        commit_parser = self.cz.commit_parser
+        changelog_pattern = self.cz.changelog_pattern
+        start_rev = self.start_rev
+        unreleased_version = self.unreleased_version
+        changelog_meta: dict = {}
+        change_type_map: dict | None = self.change_type_map
+        changelog_message_builder_hook: None | (
+            Callable
+        ) = self.cz.changelog_message_builder_hook
+        merge_prerelease = self.merge_prerelease
+
+        if not changelog_pattern or not commit_parser:
+            raise NoPatternMapError(
+                f"'{self.config.settings['name']}' rule does not support changelog"
+            )
+
+        if self.incremental and self.rev_range:
+            raise NotAllowed("--incremental cannot be combined with a rev_range")
+
+        # Don't continue if no `file_name` specified.
+        assert self.file_name
+
+        tags = changelog.get_version_tags(self.scheme, git.get_tags()) or []
+
+        end_rev = ""
+        if self.incremental:
+            changelog_meta = changelog.get_metadata(
+                self.file_name,
+                self.scheme,
+                encoding=self.encoding,
+            )
+            latest_version = changelog_meta.get("latest_version")
+            if latest_version:
+                latest_tag_version: str = bump.normalize_tag(
+                    latest_version,
+                    tag_format=self.tag_format,
+                    scheme=self.scheme,
+                )
+                start_rev = self._find_incremental_rev(latest_tag_version, tags)
+
+        if self.rev_range:
+            start_rev, end_rev = changelog.get_oldest_and_newest_rev(
+                tags,
+                version=self.rev_range,
+                tag_format=self.tag_format,
+                scheme=self.scheme,
+            )
+
+        commits = git.get_commits(start=start_rev, end=end_rev, args="--topo-order")
+        if not commits and (
+            self.current_version is None or not self.current_version.is_prerelease
+        ):
+            raise NoCommitsFoundError("No commits found")
+
+        tree = changelog.generate_tree_from_commits(
+            commits,
+            tags,
+            commit_parser,
+            changelog_pattern,
+            unreleased_version,
+            change_type_map=change_type_map,
+            changelog_message_builder_hook=changelog_message_builder_hook,
+            merge_prerelease=merge_prerelease,
+            scheme=self.scheme,
+        )
+        if self.change_type_order:
+            tree = changelog.order_changelog_tree(tree, self.change_type_order)
+        changelog_out = changelog.render_changelog(tree)
+        changelog_out = changelog_out.lstrip("\n")
+
+        if self.dry_run:
+            out.write(changelog_out)
+            raise DryRunExit()
+
+        lines = []
+        if self.incremental and os.path.isfile(self.file_name):
+            with open(self.file_name, "r", encoding=self.encoding) as changelog_file:
+                lines = changelog_file.readlines()
+
+        self.write_changelog(changelog_out, lines, changelog_meta)
