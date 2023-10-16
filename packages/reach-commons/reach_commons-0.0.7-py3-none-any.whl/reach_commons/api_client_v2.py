b7843b4a2a8e8b6863e43b2f11ecd5e3ef05d9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:45:41 +0000

    Add package reach-commons/0.0.7/reach_commons-0.0.7-py3-none-any.whl

---
+++ packages/reach-commons/reach_commons-0.0.7-py3-none-any.whl/reach_commons/api_client_v2.py
@@ -0,0 +1,31 @@
+import json
+
+import requests
+from requests.structures import CaseInsensitiveDict
+
+
+class ReachApiGatewayV2:
+    def __init__(self, base_url, access_token):
+        self.base_url = base_url
+        self.headers = CaseInsensitiveDict(
+            {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "authorization": f"Bearer {access_token}",
+            }
+        )
+
+    def stripe_create_subscription(self, user_id, business_id):
+        resp = requests.post(
+            f"{self.base_url}/stripe/user/{user_id}/business/{business_id}/subscription",
+            headers=self.headers
+        )
+        return resp
+
+    def create_booking_guarantee(self, business_id, booking_price):
+        resp = requests.post(
+            f"{self.base_url}/stripe/business/{business_id}/booking-guarantee/",
+            headers=self.headers,
+            params={'booking_price': booking_price}
+        )
+        return resp
