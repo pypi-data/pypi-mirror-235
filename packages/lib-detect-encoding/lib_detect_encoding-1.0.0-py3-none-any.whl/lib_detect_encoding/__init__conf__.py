88dc0139bd8f89c143ae7cb797696df394e75956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:48 +0000

    Add package lib-detect-encoding/1.0.0/lib_detect_encoding-1.0.0-py3-none-any.whl

---
+++ packages/lib-detect-encoding/lib_detect_encoding-1.0.0-py3-none-any.whl/lib_detect_encoding/__init__conf__.py
@@ -0,0 +1,22 @@
+# CONF
+
+name = 'lib_detect_encoding'
+title = 'detects encodings of raw files, or the system default encoding'
+version = 'v1.0.0'
+url = 'https://github.com/bitranox/lib_detect_encoding'
+author = 'Robert Nowotny'
+author_email = 'bitranox@gmail.com'
+shell_command = 'lib_detect_encoding'
+
+
+def print_info() -> None:
+    print("""\
+
+Info for lib_detect_encoding:
+
+    detects encodings of raw files, or the system default encoding
+
+    Version : v1.0.0
+    Url     : https://github.com/bitranox/lib_detect_encoding
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com""")
