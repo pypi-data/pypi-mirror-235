c578e2a898ee6c5fc3b727e33911492164c40778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:45:47 +0000

    Add package Aialgo/1.0.1/Aialgo-1.0.1.tar.gz

---
+++ packages/Aialgo/Aialgo-1.0.1.tar.gz/Aialgo-1.0.1/my_package/b.py
@@ -0,0 +1,10 @@
+def print_prolog_code():
+    prolog_code = """solution([]).
+solution([X/Y|Others]):-solution(Others),member(Y,[1,2,3,4]),noattack(X/Y,Others).
+noattack(_,[]).
+noattack(X/Y,[X1/Y1|Others]):-Y=\\=Y1,Y1-Y=\\=X1-X,Y1-Y=\\=X-X1,noattack(X/Y,Others).
+template([1/Y1,2/Y2,3/Y3,4/Y4])."""
+    print(prolog_code)
+
+# Call the function to print the Prolog code
+
