631f7ffa20f48c46908cbf532ab8e74da87299ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 08:06:18 +0000

    Add package hotreloader/0.2.2/hotreloader-0.2.2.tar.gz

---
+++ packages/hotreloader/hotreloader-0.2.2.tar.gz/hotreloader-0.2.2/hotreloader/models.py
@@ -0,0 +1,14 @@
+from .MentoDB import BaseModel, PrimaryKey, UniqueMatch
+from dataclasses import dataclass
+
+@dataclass
+class FileModel:
+    id: int
+    size: int
+    directory: str
+    name: PrimaryKey(str)
+    created_unix: int
+    modified_unix: int
+    status: str
+    # unique constraint
+    unique_match: UniqueMatch("name", "id")
