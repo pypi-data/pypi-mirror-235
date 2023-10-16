0954aa428bd3bd88d4846e9d9b125cdd32bc87fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:06:26 +0000

    Add package yupnets/0.1.8/yupnets-0.1.8.tar.gz

---
+++ packages/yupnets/yupnets-0.1.8.tar.gz/yupnets-0.1.8/src/yupnets/__init__.py
@@ -0,0 +1,42 @@
+from .nets import *
+from .test import *
+from .utils import *
+from .xray import *
+
+__all__ = [
+    # nets
+    "load_net",
+
+    # test
+    "AccuracyTest",
+    "CalibrationTest",
+    "LinearityTest",
+
+    # utils
+    "Activation",
+    "BatchManager",
+    "Normalization",
+
+    # xray
+    "get_layer_ids",
+    "get_layer",
+    "FeatureExtractor",
+]
+
+
+def load_net(
+        net: str,
+        **kwargs,
+):
+    if net in resnet.nets or net == "resnet":
+        model = resnet.resnet_loader(net=net, **kwargs)
+
+    elif net in vit.nets or net == "vit":
+        model = vit.vit_loader(net=net, **kwargs)
+
+    else:
+        raise ValueError(f"Unsupported net: {net}")
+
+    setattr(model, "name", net)
+
+    return model
