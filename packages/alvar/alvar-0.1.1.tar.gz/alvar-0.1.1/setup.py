ed590909e31038599e99c2c3c84c1d04cfad90ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 06:05:32 +0000

    Add package alvar/0.1.1/alvar-0.1.1.tar.gz

---
+++ packages/alvar/alvar-0.1.1.tar.gz/alvar-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from io import open
+
+
+def read(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+setup(
+    name="alvar",
+    version="0.1.1",
+    description="Allan variance calculation",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="sisteralab",
+    author_email="ya.vodzyanovskiy@lebedev.ru",
+    url="https://github.com/sisteralab/alvar",
+    keywords="allan variance",
+    packages=find_packages(),
+    requires=["numpy"]
+)
