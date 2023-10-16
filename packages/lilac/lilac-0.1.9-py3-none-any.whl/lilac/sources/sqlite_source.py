e0eb4eefc82782be8e22bc51006e7a5af5766a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:54:18 +0000

    Add package lilac/0.1.9/lilac-0.1.9-py3-none-any.whl

---
+++ packages/lilac/lilac-0.1.9-py3-none-any.whl/lilac/sources/sqlite_source.py
@@ -0,0 +1,84 @@
+"""SQLite source."""
+
+import os
+import sqlite3
+from typing import ClassVar, Iterable, Optional, cast
+
+import duckdb
+import pyarrow as pa
+from fastapi import APIRouter
+from pydantic import Field
+from typing_extensions import override
+
+from lilac.utils import file_exists
+
+from ..schema import Item, arrow_schema_to_schema
+from ..source import Source, SourceSchema
+from .duckdb_utils import duckdb_setup
+
+router = APIRouter()
+
+
+@router.get('/tables')
+def get_tables(db_file: str) -> list[str]:
+  """List the table names in sqlite."""
+  if not file_exists(db_file) or os.path.isdir(db_file):
+    return []
+  conn = sqlite3.connect(db_file)
+  cursor = conn.cursor()
+  cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+  res = [row[0] for row in cursor.fetchall()]
+  cursor.close()
+  conn.close()
+  return res
+
+
+class SQLiteSource(Source):
+  """SQLite data loader."""
+  name: ClassVar[str] = 'sqlite'
+  router: ClassVar[APIRouter] = router
+
+  db_file: str = Field(title='Database file', description='Path to the database file.')
+  table: str = Field(description='Table name to read from.')
+
+  _source_schema: Optional[SourceSchema] = None
+  _reader: Optional[pa.RecordBatchReader] = None
+  _con: Optional[duckdb.DuckDBPyConnection] = None
+
+  @override
+  def setup(self) -> None:
+    self._con = duckdb.connect(database=':memory:')
+
+    # DuckDB expects s3 protocol: https://duckdb.org/docs/guides/import/s3_import.html.
+    db_file = self.db_file.replace('gs://', 's3://')
+
+    self._con.execute(f"""
+      {duckdb_setup(self._con)}
+      CREATE VIEW t as (SELECT * FROM sqlite_scan('{db_file}', '{self.table}'));
+    """)
+
+    res = self._con.execute('SELECT COUNT(*) FROM t').fetchone()
+    num_items = cast(tuple[int], res)[0]
+
+    self._reader = self._con.execute('SELECT * from t').fetch_record_batch(rows_per_batch=10_000)
+    # Create the source schema in prepare to share it between process and source_schema.
+    schema = arrow_schema_to_schema(self._reader.schema)
+    self._source_schema = SourceSchema(fields=schema.fields, num_items=num_items)
+
+  @override
+  def source_schema(self) -> SourceSchema:
+    """Return the source schema."""
+    assert self._source_schema is not None
+    return self._source_schema
+
+  @override
+  def process(self) -> Iterable[Item]:
+    """Process the source."""
+    if not self._reader or not self._con:
+      raise RuntimeError('SQLite source is not initialized.')
+
+    for batch in self._reader:
+      yield from batch.to_pylist()
+
+    self._reader.close()
+    self._con.close()
---
+++ packages/lilac/lilac-0.1.9-py3-none-any.whl/lilac/splitters/text_splitter_test_utils.py
@@ -0,0 +1,36 @@
+"""Utilities for testing text splitters."""
+
+from typing import Optional, Union
+
+from ..schema import SPAN_KEY, TEXT_SPAN_END_FEATURE, TEXT_SPAN_START_FEATURE, Item, lilac_span
+
+
+def spans_to_text(text: str, spans: Optional[list[Item]]) -> list[str]:
+  """Convert text and a list of spans to a list of strings."""
+  if not spans:
+    return []
+  return [
+    text[span[SPAN_KEY][TEXT_SPAN_START_FEATURE]:span[SPAN_KEY][TEXT_SPAN_END_FEATURE]]
+    for span in spans
+  ]
+
+
+def text_to_expected_spans(text: str, splits: Union[list[str], list[tuple[str,
+                                                                          Item]]]) -> list[Item]:
+  """Convert text and a list of splits to a list of expected spans."""
+  start_offset = 0
+  expected_spans: list[Item] = []
+  for split in splits:
+    item: Item
+    if isinstance(split, str):
+      split, item = split, {}
+    elif isinstance(split, tuple):
+      split, item = split
+    else:
+      raise ValueError('Split should be a string or a tuple of (string, item dict).')
+    start = text.find(split, start_offset)
+    end = start + len(split)
+    expected_spans.append(lilac_span(start=start, end=end, metadata=item))
+    start_offset = end
+
+  return expected_spans
