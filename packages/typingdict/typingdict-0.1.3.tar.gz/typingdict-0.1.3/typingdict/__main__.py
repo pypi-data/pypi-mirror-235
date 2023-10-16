5d7bb052e43cdab6d5e314a210cf75b20a568bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:09:21 +0000

    Add package typingdict/0.1.3/typingdict-0.1.3.tar.gz

---
+++ packages/typingdict/typingdict-0.1.3.tar.gz/typingdict-0.1.3/typingdict/__main__.py
@@ -0,0 +1,40 @@
+# import sys
+# from pathlib import Path
+# import json
+# from . import typerify
+# from .beautifier import beautify
+
+import json
+import sys
+from io import TextIOWrapper
+
+import click
+
+
+@click.command()
+@click.argument(
+    "input",
+    type=click.File("rb"),
+    required=True,
+)
+@click.option(
+    "--output",
+    "-o",
+    type=click.File("w", encoding="utf-8"),
+    default=None,
+    help="Output file path",
+)
+def main(input: TextIOWrapper, output: TextIOWrapper):
+    from . import typerify
+    from .beautifier import beautify
+
+    print = output.write if output else sys.stdout.write
+
+    data = json.load(input)
+    code = typerify(data)
+    code = beautify(code)
+    print(code)
+
+
+if __name__ == "__main__":
+    main()
