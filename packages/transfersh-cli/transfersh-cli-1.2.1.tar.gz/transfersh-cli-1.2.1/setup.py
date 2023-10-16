d5159eeb1e1fcc55d7a7a32a768a890ff9035dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:04:52 +0000

    Add package transfersh-cli/1.2.1/transfersh-cli-1.2.1.tar.gz

---
+++ packages/transfersh-cli/transfersh-cli-1.2.1.tar.gz/transfersh-cli-1.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="transfersh-cli",
+    py_modules=["transfersh"],
+    version="1.2.1",
+    python_requires=">3.6",
+    description="Client to upload files to the transfer.sh service",
+    author="Andros Fenollosa",
+    author_email="andros@fenollosa.email",
+    url="https://github.com/tanrax/transfersh-cli",
+    keywords=["transfer", "upload", "client"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "click==8.1.7",
+        "progressbar2==4.2.0",
+        "pyperclip==1.8.2",
+        "requests==2.31.0",
+        "requests-toolbelt==1.0.0",
+        "tqdm==4.66.1",
+    ],
+    entry_points="""
+      [console_scripts]
+      transfersh=transfersh:transfersh_cli
+  """,
+)
