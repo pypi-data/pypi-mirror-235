75bc5c2df8da5f572511f6b4d6de79dd7e3219b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:09:06 +0000

    Add package cvehunter/1.0.6/cvehunter-1.0.6-py3-none-any.whl

---
+++ packages/cvehunter/cvehunter-1.0.6-py3-none-any.whl/cvehunter/Cvss.py
@@ -0,0 +1,19 @@
+class Cvss:
+    def __init__(self):
+        self.score: float = None
+        self.vector: str = None
+        self.severity: str = None
+        self.version: float = None
+        self.exploitability: float = None
+        self.impact: float = None
+
+    def __eq__(self, other):
+        if isinstance(other, dict):
+            return vars(self) == other
+        elif isinstance(other, Cvss):
+            return vars(self) == vars(other)
+        return False
+
+    def __repr__(self) -> str:
+        # Return a string representation that can recreate the object
+        return (str(vars(self)))
