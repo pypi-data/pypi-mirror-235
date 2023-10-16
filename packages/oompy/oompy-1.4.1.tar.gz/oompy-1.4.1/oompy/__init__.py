e4066dd1cc33b404b17dc2c83c7df59e6e27149d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:11 +0000

    Add package oompy/1.4.1/oompy-1.4.1.tar.gz

---
+++ packages/oompy/oompy-1.4.1.tar.gz/oompy-1.4.1/oompy/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "1.4.1"
+
+from .oom import UnitsClass, ConstantsClass, Quantity, Assumptions
+
+Units = UnitsClass()
+Constants = ConstantsClass()
+
+__all__ = ["Quantity", "Units", "Constants", "Assumptions", "Utils"]
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
