186e6e3ef232d7cf28f438280838a27404bcb285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:07:09 +0000

    Add package swarms/1.8.1/swarms-1.8.1-py3-none-any.whl

---
+++ packages/swarms/swarms-1.8.1-py3-none-any.whl/swarms/models/base.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractModel(ABC):
+    # abstract base class for language models
+    def __init__():
+        pass
+
+    @abstractmethod
+    def run(self, prompt):
+        # generate text using language model
+        pass
+
+    def chat(self, prompt, history):
+        pass
+
+    def __call__(self, task):
+        pass
