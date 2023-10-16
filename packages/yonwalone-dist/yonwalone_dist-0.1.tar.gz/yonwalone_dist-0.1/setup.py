68b58db087df7f1473f279dcef4ab66fcf7be419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:08:41 +0000

    Add package yonwalone-dist/0.1/yonwalone_dist-0.1.tar.gz

---
+++ packages/yonwalone-dist/yonwalone_dist-0.1.tar.gz/yonwalone_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='yonwalone_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['yonwalone_dist'],
+      zip_safe=False)
