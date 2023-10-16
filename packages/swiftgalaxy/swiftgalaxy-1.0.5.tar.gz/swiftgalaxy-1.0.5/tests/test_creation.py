d3111eb95bc04ac5f89d16e51594dcbb4b3e4a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:08:43 +0000

    Add package swiftgalaxy/1.0.5/swiftgalaxy-1.0.5.tar.gz

---
+++ packages/swiftgalaxy/swiftgalaxy-1.0.5.tar.gz/swiftgalaxy-1.0.5/tests/test_creation.py
@@ -0,0 +1,9 @@
+class TestCreation:
+    def test_sg_creation(self, sg):
+        pass  # fixture created SWIFTGalaxy
+
+    def test_vr_creation(self, vr):
+        pass  # fixture created Velociraptor interface
+
+    def test_sg_vr_creation(self, sg_vr):
+        pass  # fixture created SWIFTGalaxy with Velociraptor interface
