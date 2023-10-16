c67cccf266a279e62799255825d573c89c04e5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:06:51 +0000

    Add package RAGchain/0.1.0/RAGchain-0.1.0-py3-none-any.whl

---
+++ packages/RAGchain/RAGchain-0.1.0-py3-none-any.whl/RAGchain/utils/linker/redisdbSingleton.py
@@ -0,0 +1,54 @@
+import os
+from typing import Union
+from uuid import UUID
+
+import redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class RedisDBSingleton:
+    """
+    RedisDBSingleton is a singleton class that manages redis.
+    We use redis to link DB and passage ids that stores in retrievals.
+    """
+    __instance = None
+    _is_initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self):
+        if self._is_initialized:
+            return
+
+        host = os.getenv("REDIS_HOST")
+        port = os.getenv("REDIS_PORT")
+        db_name = os.getenv("REDIS_DB_NAME")
+        password = os.getenv("REDIS_PW")
+
+        self.client = redis.Redis(
+            host=host,
+            port=port,
+            db=db_name,
+            decode_responses=True,
+            password=password
+        )
+        self._is_initialized = True
+
+    def get_json(self, ids: list[Union[UUID, str]]):
+        # redis only accept str type key
+        str_ids = [str(find_id) for find_id in ids]
+        return [self.client.json().get(find_id) for find_id in str_ids]
+
+    def connection_check(self):
+        return self.client.ping()
+
+    def flush_db(self):
+        self.client.flushdb()
+
+    def __del__(self):
+        self.client.close()
