d0d02b88ec4839dfd63820a223d267854aea4506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:05:02 +0000

    Add package c8y-api/1.9.1/c8y_api-1.9.1-py3-none-any.whl

---
+++ packages/c8y-api/c8y_api-1.9.1-py3-none-any.whl/c8y_api/model/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2020 Software AG,
+# Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA,
+# and/or its subsidiaries and/or its affiliates and/or their licensors.
+# Use, reproduction, transfer, publication or disclosure is prohibited except
+# as specifically provided for in your License Agreement with Software AG.
+
+from c8y_api.model.administration import *
+from c8y_api.model.applications import *
+from c8y_api.model.alarms import *
+from c8y_api.model.audit import *
+from c8y_api.model.binaries import *
+from c8y_api.model.events import *
+from c8y_api.model.identity import *
+from c8y_api.model.inventory import *
+from c8y_api.model.managedobjects import *
+from c8y_api.model.measurements import *
+from c8y_api.model.notification2 import *
+from c8y_api.model.operations import *
+
+__all__ = ['administration', 'inventory', 'measurements', 'events',
+           'User', 'GlobalRole', 'InventoryRole', 'Users', 'GlobalRoles', 'InventoryRoles', 'InventoryRoleAssignment',
+           'Permission', 'ReadPermission', 'WritePermission', 'AnyPermission',
+           'Application',
+           'AuditRecord', 'AuditRecords',
+           'Operation', 'BulkOperation', 'Operations', 'BulkOperations',
+           'ManagedObject', 'Device', 'DeviceGroup', 'Availability', 'Fragment', 'NamedObject',
+           'Inventory', 'DeviceInventory', 'DeviceGroupInventory',
+           'Identity', 'ExternalId', 'Binary', 'Binaries',
+           'Measurement', 'Measurements', 'Series', 'Units',
+           'Value', 'Count', 'Grams', 'Kilograms', 'Kelvin', 'Celsius', 'Percentage',
+           'Event', 'Events', 'Alarm', 'Alarms',
+           'Subscription', 'Subscriptions', 'Tokens',
+           'Meters', 'Centimeters', 'Liters', 'CubicMeters']
