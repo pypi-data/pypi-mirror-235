678754588c5bb59a3849e7c208c2475145cd4766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:50:37 +0000

    Add package monkey-types/0.3.0/monkey_types-0.3.0.tar.gz

---
+++ packages/monkey-types/monkey_types-0.3.0.tar.gz/monkey_types-0.3.0/monkeytypes/credentials/nt_hash.py
@@ -0,0 +1,25 @@
+import re
+
+from pydantic import SecretStr, field_serializer, field_validator
+
+from .. import InfectionMonkeyBaseModel
+from .encoding import get_plaintext
+from .validators import ntlm_hash_regex
+
+
+class NTHash(InfectionMonkeyBaseModel):
+    nt_hash: SecretStr
+
+    def __hash__(self) -> int:
+        return hash(self.nt_hash)
+
+    @field_validator("nt_hash")
+    @classmethod
+    def validate_hash_format(cls, nt_hash):
+        if not re.match(ntlm_hash_regex, get_plaintext(nt_hash)):
+            raise ValueError("Invalid NT hash provided")
+        return nt_hash
+
+    @field_serializer("nt_hash", when_used="json")
+    def dump_secret(self, v):
+        return get_plaintext(v)
