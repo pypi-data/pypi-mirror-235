60fb9e57d6c44ccb890cc66be95074372bbfec6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 01:27:30 +0000

    Add package odoo14-addon-sale-order-mitxelena/14.0.1.0.0/odoo14_addon_sale_order_mitxelena-14.0.1.0.0-py2.py3-none-any.whl

---
+++ packages/odoo14-addon-sale-order-mitxelena/odoo14_addon_sale_order_mitxelena-14.0.1.0.0-py2.py3-none-any.whl/odoo/addons/sale_order_mitxelena/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sale Order Mitxelena Custom',
+    'version': '14.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Customization for sale order',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_line_views.xml',
+    ],
+    'installable': True,
+}
