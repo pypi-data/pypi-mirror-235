b5798638eaff2d8a25fc9d034367f43566248461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:45:06 +0000

    Add package reach-commons/0.0.6/reach_commons-0.0.6.tar.gz

---
+++ packages/reach-commons/reach_commons-0.0.6.tar.gz/reach_commons-0.0.6/reach_commons/api_client_v2.py
@@ -0,0 +1,23 @@
+import json
+
+import requests
+from requests.structures import CaseInsensitiveDict
+
+
+class ReachApiGatewayV2:
+    def __init__(self, base_url, access_token):
+        self.base_url = base_url
+        self.headers = CaseInsensitiveDict(
+            {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "authorization": f"Bearer {access_token}",
+            }
+        )
+
+    def stripe_create_subscription(self, user_id, business_id):
+        resp = requests.post(
+            f"{self.base_url}/stripe/user/{user_id}/business/{business_id}/subscription",
+            headers=self.headers
+        )
+        return resp
