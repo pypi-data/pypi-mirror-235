5b71f4143b0a834d4c30c021ad00072c27186947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:28 +0000

    Add package unipoll-api/0.11.1/unipoll_api-0.11.1-py3-none-any.whl

---
+++ packages/unipoll-api/unipoll_api-0.11.1-py3-none-any.whl/unipoll_api/mongo_db.py
@@ -0,0 +1,22 @@
+import motor.motor_asyncio  # type: ignore
+from unipoll_api import documents as Documents
+from unipoll_api.config import get_settings
+
+settings = get_settings()
+
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    host=settings.mongodb_url,
+    uuidRepresentation="standard",
+)
+
+mainDB = client["unipoll-api"]
+
+documentModels = [
+    Documents.AccessToken,
+    Documents.Resource,
+    Documents.Account,
+    Documents.Group,
+    Documents.Workspace,
+    Documents.Policy,
+    Documents.Poll
+]
