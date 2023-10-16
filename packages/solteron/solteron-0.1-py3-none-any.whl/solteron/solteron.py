e77f1025fca1c000ba1155722eb3922ba64ca974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:04:42 +0000

    Add package solteron/0.1/solteron-0.1-py3-none-any.whl

---
+++ packages/solteron/solteron-0.1-py3-none-any.whl/solteron/solteron.py
@@ -0,0 +1,34 @@
+"""
+===============
+solteron.py
+===============
+Patrón 'Singleton' en Python 3.
+
+* Repositorio del proyecto: https://github.com/Hernanatn/solteron.py
+* Documentación: https://github.com/Hernanatn/solteron.py/blob/master/README.MD
+
+Derechos de autor (c) 2023 Hernán A. Teszkiewicz Novick. Distribuído bajo licencia MIT.
+Hernan ATN | herni@cajadeideas.ar 
+"""
+
+__author__ = "Hernan ATN"
+__copyright__ = "(c) 2023, Hernán A. Teszkiewicz Novick."
+__license__ = "MIT"
+__version__ = "0.1"
+__email__ = "herni@cajadeideas.ar"
+
+__all__ = ['Solteron', 'Singleton']
+
+
+class Solteron(type):
+    __instancias : dict = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instancias:
+            cls.__instancias[cls] = super(Solteron, cls).__call__(*args, **kwargs)
+        return cls.__instancias[cls]
+
+Singleton = Solteron
+
+if __name__ == '__main__':
+    print(__doc__)
