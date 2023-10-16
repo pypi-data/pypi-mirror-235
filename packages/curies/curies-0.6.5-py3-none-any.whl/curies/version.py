ff171aa751e4a08021b897daee09616f5c2877c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:07:43 +0000

    Add package curies/0.6.5/curies-0.6.5-py3-none-any.whl

---
+++ packages/curies/curies-0.6.5-py3-none-any.whl/curies/version.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Version information for :mod:`curies`."""
+
+__all__ = [
+    "VERSION",
+    "get_version",
+]
+
+VERSION = "0.6.5"
+
+
+def get_version() -> str:
+    """Get the :mod:`curies` version string."""
+    return VERSION
