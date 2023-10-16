beb9b7d3ab75374fc4592eabf52227be7e65b9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:34 +0000

    Add package pyhaloxml/2.5.4/pyhaloxml-2.5.4.tar.gz

---
+++ packages/pyhaloxml/pyhaloxml-2.5.4.tar.gz/pyhaloxml-2.5.4/examples/example3.py
@@ -0,0 +1,12 @@
+"""
+This example opens a datafile and prints the wkt representation of the shapely polygon
+"""
+from pathlib import Path
+from pyhaloxml import HaloXML
+from pyhaloxml.shapely import layer_to_shapely
+
+pth = Path(Path.cwd(), "exampledata", "multiple_holes.annotations")
+hx = HaloXML()
+hx.load(pth)
+sl = layer_to_shapely(hx.layers[0])
+print(f"{sl.wkt}")
