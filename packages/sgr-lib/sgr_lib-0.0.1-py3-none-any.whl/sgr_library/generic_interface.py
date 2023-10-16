2f8e83791b8646fa2317ada309de9b6f9dccda74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:09:29 +0000

    Add package sgr-lib/0.0.1/sgr_lib-0.0.1-py3-none-any.whl

---
+++ packages/sgr-lib/sgr_lib-0.0.1-py3-none-any.whl/sgr_library/generic_interface.py
@@ -0,0 +1,43 @@
+from sgr_library.modbus_interface import SgrModbusInterface
+from sgr_library.restapi_client_async import SgrRestInterface
+import os
+
+import asyncio
+from sgr_library.auxiliary_functions import get_protocol, get_modbusInterfaceSelection
+from sgr_library.modbusRTU_interface_async import SgrModbusRtuInterface
+
+
+class GenericInterface:
+
+    def __new__(cls, xml_file: str, config_file=None):
+        if not os.path.exists(xml_file):
+            raise FileNotFoundError(f"The specified XML file does not exist: {xml_file}")
+
+        try:
+            protocol_type = get_protocol(xml_file)
+        except Exception as e:
+            raise Exception(f"Error in getting protocol from XML: {e}")
+
+        if protocol_type == "modbus":
+            try:
+                modbus_protocol_type = get_modbusInterfaceSelection(xml_file)
+            except Exception as e:
+                raise Exception(f"Error in getting Modbus interface selection: {e}")
+
+            if modbus_protocol_type == "TCPIP":
+                obj = object.__new__(SgrModbusInterface)
+                obj.__init__(xml_file)
+            elif modbus_protocol_type == "RTU":
+                obj = object.__new__(SgrModbusRtuInterface)
+                obj.__init__(xml_file)
+            else:
+                raise ValueError(f"Unsupported Modbus protocol type: {modbus_protocol_type}")
+            return obj
+            
+        elif protocol_type == "restapi":
+            obj = object.__new__(SgrRestInterface)
+            obj.__init__(xml_file, config_file)
+            return obj
+            
+        else:
+            raise ValueError(f"Unsupported protocol type: {protocol_type}")
