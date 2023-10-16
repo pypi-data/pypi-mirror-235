0ba6da07c84eec774716f48505a58e50634faf90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:06:59 +0000

    Add package reait/0.0.18/reait-0.0.18.tar.gz

---
+++ packages/reait/reait-0.0.18.tar.gz/reait-0.0.18/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+        name="reait",
+        version="0.0.18",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/RevEng-AI/reait",
+        author="James Patrick-Evans",
+        packages=setuptools.find_packages(where='src', exclude=['tests']),
+        package_dir={"": "src"},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent"
+            ],
+        install_requires=[
+                'tqdm', 'argparse', 'requests', 'rich', 'tomli', 'pandas', 'numpy', "scipy", "scikit-learn"
+            ],
+    )
+
