37f212bea4bd736c11878fcd8375d535db723446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:54:25 +0000

    Add package sgr-demo-v0.0.4/0.0.7/sgr_demo_v0.0.4-0.0.7-py3-none-any.whl

---
+++ packages/sgr-demo-v0.0.4/sgr_demo_v0.0.4-0.0.7-py3-none-any.whl/sgr_library/__init__.py
@@ -0,0 +1,13 @@
+from sgr_library import modbus_client, modbus_interface, payload_decoder, data_classes, restapi_client_async, exceptions, auxiliary_functions
+
+
+
+__all__ = [
+    "modbus_client",
+    "modbus_interface",
+    "payload_decoder",
+    "data_classes",
+    "restapi_client_async",
+    "exceptions",
+    "auxiliary_functions"
+]
