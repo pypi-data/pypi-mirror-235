ea2331b35f67c752d795315b5744042ae949a94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:04:57 +0000

    Add package complex-valued-transformer/0.0.11/complex-valued-transformer-0.0.11.tar.gz

---
+++ packages/complex-valued-transformer/complex-valued-transformer-0.0.11.tar.gz/complex-valued-transformer-0.0.11/complex_valued_transformer/__init__.py
@@ -0,0 +1,9 @@
+from complex_valued_transformer.complex_valued_transformer import (
+    ComplexMultiheadAttention,
+    ComplexRMSNorm,
+    ComplexFeedForward,
+    ComplexTransformer,
+    complex_attention_real,
+    complex_attention_complete,
+    modulate_with_rotation
+)
