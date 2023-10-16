0d83c183e74b1f8daa9fe87ea2047104b2b1bb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:08:53 +0000

    Add package multi-cam-compose-pro-RUFSHOD/0.0.1/multi_cam_compose_pro_RUFSHOD-0.0.1-py3-none-any.whl

---
+++ packages/multi-cam-compose-pro-RUFSHOD/multi_cam_compose_pro_RUFSHOD-0.0.1-py3-none-any.whl/src/multicamcomposepro/main.py
@@ -0,0 +1,26 @@
+from camera import CameraManager
+from utils import CameraConfigurator, CameraIdentifier, Warehouse
+
+# TODO Summarized:
+# Make modular grid of camera streams (i.e. not only a row, but columns as well)
+
+
+def main():
+    camera_identifier = CameraIdentifier()
+    camera_identifier.camera_identifier()  # Check if camera_config.json exists
+
+    camera_configurator = CameraConfigurator()
+    camera_configurator.camera_configurator()
+
+    warehouse = Warehouse()
+    warehouse.build(
+        object_name="o b j e ct",
+        anomalies=["a n o m a ly"],
+    )
+    print(warehouse)
+    camera_manager = CameraManager(warehouse, 2, 3)
+    camera_manager.run()
+
+
+if __name__ == "__main__":
+    main()
