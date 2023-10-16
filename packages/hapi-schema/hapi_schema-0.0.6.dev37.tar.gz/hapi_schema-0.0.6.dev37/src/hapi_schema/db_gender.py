68ae725577ec1bb504ddcbf921413c6209425269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 06:04:22 +0000

    Add package hapi-schema/0.0.6.dev37/hapi_schema-0.0.6.dev37.tar.gz

---
+++ packages/hapi-schema/hapi_schema-0.0.6.dev37.tar.gz/hapi_schema-0.0.6.dev37/src/hapi_schema/db_gender.py
@@ -0,0 +1,21 @@
+"""Gender table and view."""
+
+from hdx.database.no_timezone import Base
+from sqlalchemy import CHAR, String, select
+from sqlalchemy.orm import Mapped, mapped_column
+
+from hapi_schema.view import view
+
+
+class DBGender(Base):
+    __tablename__ = "gender"
+
+    code: Mapped[str] = mapped_column(CHAR(1), primary_key=True)
+    description: Mapped[str] = mapped_column(String(256), nullable=False)
+
+
+gender_view = view(
+    name="gender_view",
+    metadata=Base.metadata,
+    selectable=select(*DBGender.__table__.columns),
+)
