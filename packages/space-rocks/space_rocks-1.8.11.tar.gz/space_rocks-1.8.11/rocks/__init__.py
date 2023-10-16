f38b9a5ece22fde0e7f8e4c9299a8cd479601de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:08:26 +0000

    Add package space-rocks/1.8.11/space_rocks-1.8.11.tar.gz

---
+++ packages/space-rocks/space_rocks-1.8.11.tar.gz/space_rocks-1.8.11/rocks/__init__.py
@@ -0,0 +1,19 @@
+"""For space rocks."""
+
+import inspect
+
+# Welcome to rocks
+__version__ = "1.8.11"
+
+# Only define user API if rocks is not called via command line
+context = inspect.stack()[-1].code_context
+if context is None or "rocks.cli" not in context[0]:
+    # Expose API to user
+    from .bft import load_bft
+    from .core import Rock
+    from .core import rocks_ as rocks
+    from .resolve import identify
+    from .logging import set_log_level
+
+    # Alias id to identify
+    id = identify
