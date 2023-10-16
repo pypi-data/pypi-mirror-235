6c7eba998e7008bfa0d32ecc588c766cd4654178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:08:19 +0000

    Add package FileUse/1.0.2/FileUse-1.0.2.tar.gz

---
+++ packages/FileUse/FileUse-1.0.2.tar.gz/FileUse-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FileUse",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[],  # Lista de dependencias si las tienes
+)
