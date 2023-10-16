0fed0ae1fbef7ae786d204e8677c424a0c00a2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:22 +0000

    Add package my-mimic/0.3.0/my_mimic-0.3.0-py3-none-any.whl

---
+++ packages/my-mimic/my_mimic-0.3.0-py3-none-any.whl/mr/config/__init__.py
@@ -0,0 +1,43 @@
+"""
+Config for Mr Mime
+"""
+from contextlib import asynccontextmanager, contextmanager
+from dataclasses import dataclass, field
+from typing import Type
+
+from mr.states.interface import IState
+
+
+@dataclass(slots=True, frozen=True)
+class Config:
+    """
+    Config class
+    """
+
+    state: Type[IState]
+    kwargs: dict = field(default_factory=dict)
+    _state: IState = None
+
+    def __post_init__(self):
+        if not issubclass(self.state, IState):
+            raise TypeError("State class does not implement IState interface")
+        self.__init_state()
+
+    def __init_state(self):
+        object.__setattr__(self, "_state", self.state(**self.kwargs))
+
+    @contextmanager
+    def sync_acquire_state(self) -> IState:
+        """
+        Get sync state
+        :return: SyncRedis
+        """
+        yield self._state
+
+    @asynccontextmanager
+    async def async_acquire_state(self) -> IState:
+        """
+        Get sync state
+        :return: SyncRedis
+        """
+        yield self._state
