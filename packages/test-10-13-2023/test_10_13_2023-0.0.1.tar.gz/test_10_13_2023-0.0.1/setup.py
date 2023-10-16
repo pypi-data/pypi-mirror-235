d1babd586181e214087b9a015a804c4a5a2a4d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:06:43 +0000

    Add package test-10-13-2023/0.0.1/test_10_13_2023-0.0.1.tar.gz

---
+++ packages/test-10-13-2023/test_10_13_2023-0.0.1.tar.gz/test_10_13_2023-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='test_10_13_2023',
+    packages=find_packages(include=['test_10_13_2023']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
