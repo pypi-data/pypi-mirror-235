8a4cb3940adbd5e498f7952b5cc34a27b5a0bc7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:10:14 +0000

    Add package nonebot-plugin-alconna/0.27.3/nonebot_plugin_alconna-0.27.3.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.27.3.tar.gz/nonebot_plugin_alconna-0.27.3/src/nonebot_plugin_alconna/rule.py
@@ -0,0 +1,244 @@
+import asyncio
+import traceback
+from typing import Dict, List, Type, Union, Literal, Optional
+
+from nonebot import get_driver
+from nonebot.typing import T_State
+from nonebot.params import EventMessage
+from nonebot.plugin.on import on_message
+from nonebot.internal.matcher import matchers
+from nonebot.internal.rule import Rule as Rule
+from nonebot.adapters import Bot, Event, Message
+from tarina import lang, init_spec, is_awaitable
+from arclet.alconna.exceptions import SpecialOptionTriggered
+from arclet.alconna import (
+    Args,
+    Alconna,
+    Arparma,
+    AllParam,
+    CommandMeta,
+    CompSession,
+    output_manager,
+    command_manager,
+)
+
+from .config import Config
+from .uniseg import UniMessage
+from .model import CompConfig, CommandResult
+from .extension import Extension, ExtensionExecutor
+from .consts import ALCONNA_RESULT, ALCONNA_EXTENSION, ALCONNA_EXEC_RESULT, log
+
+
+class AlconnaRule:
+    """检查消息字符串是否能够通过此 Alconna 命令。
+
+    参数:
+        command: Alconna 命令
+        skip_for_unmatch: 是否在命令不匹配时跳过该响应
+        auto_send_output: 是否自动发送输出信息并跳过响应
+        comp_config: 自动补全配置
+        extensions: 需要加载的匹配扩展
+        exclude_ext: 需要排除的匹配扩展
+        use_origin: 是否使用未经 to_me 等处理过的消息
+        use_cmd_start: 是否使用 nb 全局配置里的命令前缀
+    """
+
+    __slots__ = (
+        "command",
+        "skip",
+        "auto_send",
+        "comp_config",
+        "use_origin",
+        "executor",
+    )
+
+    def __init__(
+        self,
+        command: Alconna,
+        skip_for_unmatch: bool = True,
+        auto_send_output: bool = False,
+        comp_config: Optional[CompConfig] = None,
+        extensions: Optional[List[Union[Type[Extension], Extension]]] = None,
+        exclude_ext: Optional[List[Union[Type[Extension], str]]] = None,
+        use_origin: bool = False,
+        use_cmd_start: bool = False,
+        use_cmd_sep: bool = False,
+    ):
+        self.comp_config = comp_config
+        self.use_origin = use_origin
+        try:
+            global_config = get_driver().config
+            config = Config.parse_obj(global_config)
+            self.auto_send = auto_send_output or config.alconna_auto_send_output
+            if (
+                not command.prefixes
+                and (use_cmd_start or config.alconna_use_command_start)
+                and global_config.command_start
+            ):
+                command_manager.delete(command)
+                command.prefixes = list(global_config.command_start)
+                command._hash = command._calc_hash()
+                command_manager.register(command)
+            if (use_cmd_sep or config.alconna_use_command_sep) and global_config.command_sep:
+                command.separators = tuple(global_config.command_sep)
+                command_manager.resolve(command).separators = tuple(global_config.command_sep)
+            if config.alconna_auto_completion and not self.comp_config:
+                self.comp_config = {}
+            self.use_origin = use_origin or config.alconna_use_origin
+        except ValueError:
+            self.auto_send = auto_send_output
+        self.command = command
+        self.skip = skip_for_unmatch
+        self.executor = ExtensionExecutor(extensions, exclude_ext)
+        self.executor.post_init(self.command)
+
+    def __repr__(self) -> str:
+        return f"Alconna(command={self.command!r})"
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, AlconnaRule) and self.command.path == other.command.path
+
+    def __hash__(self) -> int:
+        return hash(self.command.__hash__())
+
+    async def handle(self, bot: Bot, event: Event, msg: Message):
+        interface = CompSession(self.command)
+        if self.comp_config is None:
+            return self.command.parse(msg)
+        res = None
+        with interface:
+            res = self.command.parse(msg)
+        if res:
+            return res
+        meta = CommandMeta(compact=True, hide=True)
+        _tab = Alconna(self.comp_config.get("tab", ".tab"), Args["offset", int, 1], [], meta=meta)
+        _enter = Alconna(
+            self.comp_config.get("enter", ".enter"),
+            Args["content", AllParam, []],
+            [],
+            meta=meta,
+        )
+        _exit = Alconna(self.comp_config.get("exit", ".exit"), [], meta=meta)
+
+        _waiter = on_message(priority=self.comp_config.get("priority", -1), block=True)
+        _futures: Dict[str, asyncio.Future] = {}
+        res = Arparma(
+            self.command.path,
+            msg,
+            False,
+            error_info=SpecialOptionTriggered("completion"),
+        )
+
+        @_waiter.handle()
+        async def _waiter_handle(_bot: Bot, _event: Event, content: Message = EventMessage()):
+            if _exit.parse(content).matched:
+                _futures["_"].set_result(False)
+                await _waiter.finish()
+            if (mat := _tab.parse(content)).matched:
+                interface.tab(mat.query[int]("offset", 1))
+                if self.comp_config.get("lite", False):  # type: ignore
+                    out = interface.current()
+                else:
+                    out = "\n".join(interface.lines())
+                await self._send(out, _bot, _event, res)  # type: ignore
+                _waiter.skip()
+            if (mat := _enter.parse(content)).matched:
+                _futures["_"].set_result(mat.content)
+                await _waiter.finish()
+            await self._send(interface.current(), _bot, _event, res)  # type: ignore
+            _waiter.skip()
+
+        def clear():
+            interface.clear()
+            _waiter.destroy()
+            _waiter.handlers.clear()
+            matchers.pop(-1)
+            command_manager.delete(_tab)
+            command_manager.delete(_enter)
+            command_manager.delete(_exit)
+
+        help_text = (
+            f"{lang.require('comp/nonebot', 'tab').format(cmd=_tab.command)}\n"
+            f"{lang.require('comp/nonebot', 'enter').format(cmd=_enter.command)}\n"
+            f"{lang.require('comp/nonebot', 'exit').format(cmd=_exit.command)}"
+        )
+
+        while interface.available:
+            await self._send(str(interface), bot, event, res)
+            await self._send(help_text, bot, event, res)
+            _future = _futures.setdefault("_", asyncio.get_running_loop().create_future())
+            _future.add_done_callback(lambda x: _futures.pop("_"))
+            try:
+                await asyncio.wait_for(_future, timeout=self.comp_config.get("timeout", 60))
+            except asyncio.TimeoutError:
+                await self._send(lang.require("comp/nonebot", "timeout"), bot, event, res)
+                clear()
+                return res
+            ans: Union[Message, Literal[False]] = _future.result()
+            if ans is False:
+                await self._send(lang.require("comp/nonebot", "exited"), bot, event, res)
+                clear()
+                return res
+            param = list(ans)
+            if not param or not param[0]:
+                param = None
+            try:
+                with interface:
+                    res = interface.enter(param)
+            except Exception as e:
+                traceback.print_exc()
+                await self._send(str(e), bot, event, res)
+        clear()
+        return res
+
+    async def __call__(self, event: Event, state: T_State, bot: Bot) -> bool:
+        self.executor.select(bot, event)
+        if not (msg := await self.executor.message_provider(event, state, bot, self.use_origin)):
+            return False
+        msg = await self.executor.receive_wrapper(bot, event, msg)
+        if isinstance(msg, UniMessage):
+            msg = await msg.export(bot, fallback=True)
+        Arparma._additional.update(bot=lambda: bot, event=lambda: event, state=lambda: state)
+        with output_manager.capture(self.command.name) as cap:
+            output_manager.set_action(lambda x: x, self.command.name)
+            try:
+                arp = await self.handle(bot, event, msg)
+            except Exception as e:
+                arp = Arparma(self.command.path, msg, False, error_info=e)
+            may_help_text: Optional[str] = cap.get("output", None)
+        if not arp.matched and not may_help_text and self.skip:
+            log("TRACE", lang.require("nbp-alc", "log.parse").format(msg=msg, cmd=self.command.path, arp=arp))
+            return False
+        if arp.head_matched:
+            log("DEBUG", lang.require("nbp-alc", "log.parse").format(msg=msg, cmd=self.command.path, arp=arp))
+        if not may_help_text and arp.error_info:
+            may_help_text = repr(arp.error_info)
+        if self.auto_send and may_help_text:
+            await self._send(may_help_text, bot, event, arp)
+            return False
+        await self.executor.parse_wrapper(bot, state, event, arp)
+        state[ALCONNA_RESULT] = CommandResult(self.command, arp, may_help_text)
+        exec_result = self.command.exec_result
+        for key, value in exec_result.items():
+            if is_awaitable(value):
+                exec_result[key] = await value
+            elif isinstance(value, (str, Message)):
+                exec_result[key] = await bot.send(event, value)
+        state[ALCONNA_EXEC_RESULT] = exec_result
+        state[ALCONNA_EXTENSION] = self.executor.context
+        return True
+
+    async def _send(self, text: str, bot: Bot, event: Event, arp: Arparma) -> Message:
+        _t = str(arp.error_info) if isinstance(arp.error_info, SpecialOptionTriggered) else "help"
+        try:
+            msg = await self.executor.output_converter(_t, text)  # type: ignore
+            if isinstance(msg, UniMessage):
+                msg = await msg.export(bot, fallback=True)
+            return await bot.send(event, msg)  # type: ignore
+        except NotImplementedError:
+            return await bot.send(event, event.get_message().__class__(text))
+
+
+@init_spec(AlconnaRule)
+def alconna(rule: AlconnaRule) -> Rule:
+    return Rule(rule)
---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.27.3.tar.gz/nonebot_plugin_alconna-0.27.3/src/nonebot_plugin_alconna/uniseg/segment.py
@@ -0,0 +1,537 @@
+"""通用标注, 无法用于创建 MS对象"""
+import re
+import json
+import contextlib
+from pathlib import Path
+from dataclasses import field, asdict, dataclass
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    Type,
+    Union,
+    Generic,
+    Literal,
+    TypeVar,
+    Callable,
+    Iterable,
+    Optional,
+)
+
+from nonebot.internal.adapter import Message, MessageSegment
+from nepattern import MatchMode, BasePattern, create_local_patterns
+
+if TYPE_CHECKING:
+    from .message import UniMessage
+
+
+TS = TypeVar("TS", bound="Segment")
+TS1 = TypeVar("TS1", bound="Segment")
+
+
+class UniPattern(BasePattern[TS], Generic[TS]):
+    additional: Optional[Callable[..., bool]] = None
+
+    def __init__(self):
+        origin: Type[TS] = self.__class__.__orig_bases__[0].__args__[0]  # type: ignore
+
+        def _converter(_, seg: MessageSegment) -> Optional[TS]:
+            if (res := self.solve(seg)) and not hasattr(res, "origin"):
+                res.origin = seg
+            return res
+
+        super().__init__(
+            model=MatchMode.TYPE_CONVERT,
+            origin=origin,
+            converter=_converter,  # type: ignore
+            alias=origin.__name__,
+            accepts=[MessageSegment],
+            validators=[self.additional] if self.additional else [],
+        )
+
+    def solve(self, seg: MessageSegment) -> Optional[TS]:
+        raise NotImplementedError
+
+
+@dataclass
+class Segment:
+    """基类标注"""
+
+    origin: MessageSegment = field(init=False, repr=False, compare=False)
+
+    def __str__(self):
+        return f"[{self.__class__.__name__.lower()}]"
+
+    def __add__(self: TS, item: Union[TS1, Iterable[TS1]]) -> "UniMessage[Union[TS, TS1]]":
+        from .message import UniMessage
+
+        return UniMessage(self) + item
+
+    def __radd__(self: TS, item: Union[TS1, Iterable[TS1]]) -> "UniMessage[Union[TS, TS1]]":
+        from .message import UniMessage
+
+        return UniMessage(item) + self
+
+    def is_text(self) -> bool:
+        return False
+
+    @property
+    def type(self) -> str:
+        return self.__class__.__name__.lower()
+
+    @property
+    def data(self) -> Dict[str, Any]:
+        return asdict(self)
+
+
+@dataclass
+class Text(Segment):
+    """Text对象, 表示一类文本元素"""
+
+    text: str
+    style: Optional[str] = field(default=None)
+
+    def __post_init__(self):
+        self.text = str(self.text)
+
+    def is_text(self) -> bool:
+        return True
+
+    def __str__(self):
+        return self.text
+
+
+@dataclass
+class At(Segment):
+    """At对象, 表示一类提醒某用户的元素"""
+
+    flag: Literal["user", "role", "channel"]
+    target: str
+    display: Optional[str] = field(default=None)
+
+
+@dataclass
+class AtAll(Segment):
+    """AtAll对象, 表示一类提醒所有人的元素"""
+
+    here: bool = field(default=False)
+
+
+@dataclass
+class Emoji(Segment):
+    """Emoji对象, 表示一类表情元素"""
+
+    id: str
+    name: Optional[str] = field(default=None)
+
+
+@dataclass
+class Media(Segment):
+    url: Optional[str] = field(default=None)
+    id: Optional[str] = field(default=None)
+    path: Optional[Union[str, Path]] = field(default=None)
+    raw: Optional[bytes] = field(default=None)
+    name: Optional[str] = field(default=None)
+
+    def __post_init__(self):
+        if self.path:
+            self.name = Path(self.path).name
+
+
+@dataclass
+class Image(Media):
+    """Image对象, 表示一类图片元素"""
+
+    name: str = field(default="image.png")
+
+
+@dataclass
+class Audio(Media):
+    """Audio对象, 表示一类音频元素"""
+
+    name: str = field(default="audio.mp3")
+
+
+@dataclass
+class Voice(Media):
+    """Voice对象, 表示一类语音元素"""
+
+    name: str = field(default="voice.wav")
+
+
+@dataclass
+class Video(Media):
+    """Video对象, 表示一类视频元素"""
+
+    name: str = field(default="video.mp4")
+
+
+@dataclass
+class File(Segment):
+    """File对象, 表示一类文件元素"""
+
+    id: Optional[str] = field(default=None)
+    name: Optional[str] = field(default=None)
+    raw: Optional[bytes] = field(default=None)
+
+
+@dataclass
+class Reply(Segment):
+    """Reply对象，表示一类回复消息"""
+
+    origin: Any
+    id: str
+    """此处不一定是消息ID，可能是其他ID，如消息序号等"""
+    msg: Optional[Union[Message, str]] = field(default=None)
+
+
+@dataclass
+class Card(Segment):
+    """Card对象，表示一类卡片消息"""
+
+    flag: Literal["xml", "json"]
+    raw: str
+    content: Optional[Union[dict, list]] = field(default=None)
+
+    def __post_init__(self):
+        with contextlib.suppress(json.JSONDecodeError):
+            self.content = json.loads(self.raw)
+
+
+@dataclass
+class Other(Segment):
+    """其他 Segment"""
+
+    origin: MessageSegment
+
+    def __str__(self):
+        return f"[{self.origin.type}]"
+
+
+class _Other(UniPattern[Other]):
+    def solve(self, seg: MessageSegment):
+        return Other(seg)
+
+
+other = _Other()
+
+
+class _Text(UniPattern[Text]):
+    def solve(self, seg: MessageSegment):
+        if seg.is_text():
+            return Text(seg.data["text"], seg.type if seg.type != "text" else None)
+
+
+text = _Text()
+
+
+class _At(UniPattern[At]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "at":  # ob11, feishu, red, satori
+            if "qq" in seg.data and seg.data["qq"] != "all":
+                return At("user", str(seg.data["qq"]))
+            if "user_id" in seg.data:
+                return At("user", str(seg.data["user_id"]))
+            if "id" in seg.data:
+                return At("user", seg.data["id"], seg.data["name"])
+            if "role" in seg.data:
+                return At("role", seg.data["role"], seg.data["name"])
+        if seg.type == "sharp":  # satori
+            return At("channel", seg.data["channel_id"], seg.data["name"])
+        if seg.type == "mention":  # ob12, tg
+            if "user_id" in seg.data:
+                return At("user", str(seg.data["user_id"]))
+            if "text" in seg.data:
+                return At("user", seg.data["text"])
+        if seg.type == "mention_user":  # qq, qqguild, discord, villa
+            if "user_id" in seg.data:
+                return At("user", str(seg.data["user_id"]))
+            if "mention_user" in seg.data:
+                return At("user", str(seg.data["mention_user"].user_id))
+        if seg.type == "mention_channel":  # discord, qq, qqguild
+            return At("channel", str(seg.data["channel_id"]))
+        if seg.type == "mention_role":  # discord
+            return At("role", str(seg.data["role_id"]))
+        if seg.type == "mention_robot":  # villa
+            return At("user", str(seg.data["mention_robot"].bot_id))
+        if seg.type == "At":  # mirai
+            return At("user", str(seg.data["target"]))
+        if seg.type == "kmarkdown":  # kook
+            content = seg.data["content"]
+            if not content.startswith("(met)"):
+                return None
+            if (end := content.find("(met)", 5)) == -1:
+                return None
+            return content[5:end] not in ("here", "all") and At("user", content[5:end])
+        if seg.type == "room_link":  # villa
+            return At(
+                "channel",
+                f'{seg.data["room_link"].villa_id}:{seg.data["room_link"].room_id}',
+            )
+
+
+at = _At()
+
+
+class _AtAll(UniPattern[AtAll]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "at":
+            if "qq" in seg.data and seg.data["qq"] == "all":  # ob11
+                return AtAll()
+            if "type" in seg.data and seg.data["type"] in ("all", "here"):
+                return AtAll(here=seg.data["type"] == "here")
+        if seg.type in {"at_all", "AtAll", "mention_everyone", "mention_all"}:
+            return AtAll()
+        if seg.type == "kmarkdown":
+            content = seg.data["content"]
+            if not content.startswith("(met)"):
+                return None
+            if (end := content.find("(met)", 5)) == -1:
+                return None
+            return content[5:end] in ("here", "all") and AtAll(content[5:end] == "here")
+
+
+at_all = _AtAll()
+
+
+class _Emoji(UniPattern[Emoji]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "emoji":
+            if "id" in seg.data:
+                return Emoji(seg.data["id"])
+            if "name" in seg.data:
+                return Emoji(seg.data["name"])
+        if seg.type == "Face":
+            return Emoji(str(seg.data["faceId"]), seg.data["name"])
+        if seg.type == "face":
+            if "id" in seg.data:
+                return Emoji(str(seg.data["id"]))
+            if "face_id" in seg.data:
+                return Emoji(str(seg.data["face_id"]))
+        if seg.type == "custom_emoji":
+            if "custom_emoji_id" in seg.data:  # telegram
+                return Emoji(seg.data["custom_emoji_id"], seg.data["text"])
+            if "id" in seg.data:  # discord
+                return Emoji(seg.data["id"], seg.data["name"])
+        if seg.type == "kmarkdown":
+            content = seg.data["content"]
+            if content.startswith("(emj)"):
+                mat = re.search(r"\(emj\)(?P<name>[^()\[\]]+)\(emj\)\[(?P<id>[^\[\]]+)\]", content)
+                return mat and Emoji(mat["id"], mat["name"])
+            if content.startswith(":"):
+                mat = re.search(r":(?P<id>[^:]+):", content)
+                return mat and Emoji(mat["id"])
+        if seg.type == "sticker" and "id" in seg.data:
+            return Emoji(seg.data["id"])
+
+
+emoji = _Emoji()
+
+
+class _Image(UniPattern[Image]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "image":
+            if "uuid" in seg.data:  # red
+                return Image(
+                    id=seg.data["uuid"],
+                    url=f"https://gchat.qpic.cn/gchatpic_new/0/0-0-{seg.data['md5'].upper()}/0",
+                    path=seg.data["path"],
+                    name=seg.data["md5"],
+                )
+            if "file_id" in seg.data:  # ob12
+                return Image(id=seg.data["file_id"])
+            if "image" in seg.data:  # villa
+                return Image(url=seg.data["image"].url)
+            if "image_key" in seg.data:  # feishu
+                return Image(id=seg.data["image_key"])
+            if "file_key" in seg.data:  # kook
+                return Image(url=seg.data["file_key"])
+            if "url" in seg.data:  # ob11
+                return Image(url=seg.data["url"], id=seg.data["file"])
+            if "msgData" in seg.data:  # minecraft
+                return Image(url=seg.data["msgData"])
+            if "file_path" in seg.data:  # ntchat
+                return Image(id=seg.data["file_path"], path=seg.data["file_path"])
+            if "picURL" in seg.data:  # ding
+                return Image(url=seg.data["picURL"])
+        if seg.type == "photo":  # tg
+            return Image(id=seg.data["file"])
+        if seg.type == "attachment":
+            if "url" in seg.data:  # qq, qqguild
+                return Image(url=seg.data["url"])
+            if "attachment" in seg.data:  # discord
+                return Image(id=seg.data["attachment"].filename)
+        if seg.type == "Image":  # mirai
+            return Image(seg.data["url"], seg.data["imageId"])
+        if seg.type == "img":
+            return Image(seg.data["src"], seg.data["src"])
+
+
+image = _Image()
+
+
+class _Video(UniPattern[Video]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "video":
+            if "videoMd5" in seg.data:  # red
+                return Video(
+                    id=seg.data["videoMd5"],
+                    path=seg.data["filePath"],
+                    name=seg.data["fileName"],
+                )
+            if "file_id" in seg.data:  # ob12, telegram
+                return Video(id=seg.data["file_id"])
+            if "file" in seg.data:  # ob11
+                return Video(url=seg.data["file"])
+            if "file_key" in seg.data:  # kook
+                return Video(url=seg.data["file_key"])
+            if "msgData" in seg.data:  # minecraft
+                return Video(url=seg.data["msgData"])
+            if "file_path" in seg.data:  # ntchat
+                return Video(id=seg.data["file_path"], path=seg.data["file_path"])
+            if "src" in seg.data:
+                return Video(seg.data["src"], seg.data["src"])
+        if seg.type == "video":
+            return Video(seg.data["url"], seg.data["videoId"])
+        if seg.type == "animation":
+            return Video(id=seg.data["file_id"])
+        if seg.type == "media":  # feishu
+            return Video(id=seg.data["file_key"], name=seg.data["file_name"])
+
+
+video = _Video()
+
+
+class _Voice(UniPattern[Voice]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "voice":
+            if "md5" in seg.data:  # red
+                return Voice(
+                    id=seg.data["md5"],
+                    path=seg.data["path"],
+                    name=seg.data["name"],
+                )
+            if "file_id" in seg.data:  # ob12, telegram
+                return Voice(id=seg.data["file_id"])
+            if "file_key" in seg.data:  # kook
+                return Voice(url=seg.data["file_key"])
+            if "file_path" in seg.data:  # ntchat
+                return Voice(id=seg.data["file_path"], path=seg.data["file_path"])
+        if seg.type == "record":
+            return Voice(seg.data["url"])
+        if seg.type == "Voice":
+            return Voice(seg.data["url"], seg.data["voiceId"])
+
+
+voice = _Voice()
+
+
+class _Audio(UniPattern[Audio]):
+    def solve(self, seg: MessageSegment):
+        if seg.type != "audio":
+            return
+        if "file_id" in seg.data:  # ob12, telegram
+            return Audio(id=seg.data["file_id"])
+        if "file_key" in seg.data:  # kook, feishu
+            return Audio(url=seg.data["file_key"])
+        if "file_path" in seg.data:  # ntchat
+            return Audio(id=seg.data["file_path"], path=seg.data["file_path"])
+        if "src" in seg.data:
+            return Audio(seg.data["src"], seg.data["src"])
+
+
+audio = _Audio()
+
+
+class _File(UniPattern[File]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "file":
+            if "md5" in seg.data:  # red
+                return File(
+                    id=seg.data["md5"],
+                    name=seg.data["name"],
+                )
+            if "file_id" in seg.data:  # ob12
+                return File(id=seg.data["file_id"])
+            if "file_key" in seg.data:  # feishu, kook
+                return File(
+                    id=seg.data["file_key"],
+                    name=seg.data.get("file_name", seg.data.get("title")),
+                )
+            if "file_path" in seg.data:  # ntchat
+                return File(id=seg.data["file_path"])
+            if "src" in seg.data:
+                return File(seg.data["src"])
+        if seg.type == "document":
+            return File(seg.data["file_id"], seg.data["file_name"])
+        if seg.type == "File":
+            return File(seg.data["id"], seg.data["name"])
+
+
+file = _File()
+
+
+class _Reply(UniPattern[Reply]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "reference":
+            if "message_id" in seg.data:  # telegram
+                return Reply(seg, seg.data["message_id"])
+            if "reference" in seg.data:  # discord, qq, qqguild
+                return Reply(seg, seg.data["reference"].message_id)
+        if seg.type == "reply":
+            if "id" in seg.data:  # ob11
+                return Reply(seg, seg.data["id"])
+            if "message_id" in seg.data:  # ob12
+                return Reply(seg, seg.data["message_id"])
+            if "msg_id" in seg.data:  # red
+                return Reply(seg.data["_origin"], seg.data["msg_seq"])
+        if seg.type == "quote":
+            if "id" in seg.data:  # satori
+                return Reply(seg, seg.data["id"], seg.data["content"])
+            if "msg_id" in seg.data:  # kook:
+                return Reply(seg, seg.data["msg_id"])
+            if "quoted_message_id" in seg.data:  # villa
+                return Reply(seg, seg.data["quoted_message_id"])
+        if seg.type == "Quote":  # mirai
+            return Reply(seg, str(seg.data["id"]), str(seg.data["origin"]))
+
+
+reply = _Reply()
+
+
+class _Card(UniPattern[Card]):
+    def solve(self, seg: MessageSegment):
+        if seg.type == "card":
+            if "content" in seg.data:  # kook
+                return Card("json", seg.data["content"])
+            if "card_wxid" in seg.data:  # ntchat
+                return Card("json", seg.data["card_wxid"])
+        if seg.type == "Xml":  # mirai
+            return Card("xml", seg.data["xml"])
+        if seg.type == "Json":  # mirai
+            return Card("json", seg.data["json"])
+        if seg.type == "App":  # mirai
+            return Card("json", seg.data["content"])
+        if seg.type == "xml":  # ob12
+            return Card("xml", seg.data["data"])
+        if seg.type == "json":  # ob11
+            return Card("json", seg.data["data"])
+        if seg.type == "ark" and "data" in seg.data:  # red
+            return Card("json", seg.data["data"])
+
+
+card = _Card()
+segments = [at_all, at, emoji, image, video, voice, audio, file, card, text, other]
+env = create_local_patterns("nonebot")
+env.sets(segments)
+
+
+class _Segment(UniPattern[Segment]):
+    def solve(self, seg: MessageSegment):
+        for pat in segments:
+            if (res := pat.validate(seg)).success:
+                return res.value
+        return Other(seg)  # type: ignore
+
+
+env[Segment] = _Segment()
