840661e69904fcaaba806fbaef183eaddfc83f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 05:06:10 +0000

    Add package openpaygo/0.5.0/openpaygo-0.5.0.tar.gz

---
+++ packages/openpaygo/openpaygo-0.5.0.tar.gz/openpaygo-0.5.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="openpaygo", 
+    packages=find_packages(),
+    version='0.5.0',
+    license='MIT',
+    author="Solaris Offgrid",
+    url='https://github.com/EnAccess/OpenPAYGO-python/',
+    install_requires=[
+        'siphash',
+    ],
+)
