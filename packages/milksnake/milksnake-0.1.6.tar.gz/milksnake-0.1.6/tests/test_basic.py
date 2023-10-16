25d2b7eaf7b3fd9706a67224f79fe7de965422b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 08:06:21 +0000

    Add package milksnake/0.1.6/milksnake-0.1.6.tar.gz

---
+++ packages/milksnake/milksnake-0.1.6.tar.gz/milksnake-0.1.6/tests/test_basic.py
@@ -0,0 +1,23 @@
+import os
+
+
+def test_example_dev_run(virtualenv):
+    pkg = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                       'res', 'minimal'))
+    virtualenv.run('pip', ['install', '-v', '--editable', pkg])
+    virtualenv.eval('''if 1:
+        from example import test
+        def execute():
+            assert test() == (0.0, 0.0)
+    ''')
+
+
+def test_example_nested_dev_run(virtualenv):
+    pkg = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                       'res', 'nested'))
+    virtualenv.run('pip', ['install', '-v', '--editable', pkg])
+    virtualenv.eval('''if 1:
+        from example.nested import test
+        def execute():
+            assert test() == (0.0, 0.0)
+    ''')
