6ba0c48d58ceb0ce78506c89f84403167ef6f5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:43 +0000

    Add package databricks-vectorsearch/0.5/databricks_vectorsearch-0.5-py3-none-any.whl

---
+++ packages/databricks-vectorsearch/databricks_vectorsearch-0.5-py3-none-any.whl/databricks/vector_search/utils.py
@@ -0,0 +1,77 @@
+import requests
+import logging
+from urllib.parse import urlparse, urlunparse
+
+# logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+class OAuthTokenUtils:
+
+    @staticmethod
+    def get_oauth_token(
+        workspace_url,
+        service_principal_client_id,
+        service_principal_client_secret,
+        authorization_details=None,
+    ):
+        authorization_details = authorization_details or []
+        url = workspace_url + "/oidc/v1/token"
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {
+            "grant_type": "client_credentials",
+            "scope": "all-apis",
+            "authorization_details": authorization_details
+        }
+        logging.info(f"Issuing request to {url} with data {data} and headers {headers}")
+        response = requests.request(
+            url=url,
+            auth=(service_principal_client_id, service_principal_client_secret),
+            headers=headers,
+            method="POST",
+            data=data
+        )
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            logging.warn(f"Error retrieving OAuth Token {e}")
+            raise Exception(
+                f"Response content {response.content}, status_code {response.status_code}"
+            )
+        return response.json()
+
+
+class RequestUtils:
+
+    @staticmethod
+    def issue_request(url, token, method, params=None, json=None, verify=True):
+        headers = dict()
+        headers["Authorization"] = f"Bearer {token}"
+        logging.info(f"Issuing request to {url} with method {method} and \
+            json {json} and headers {headers} and verify {verify}")
+        response = requests.request(
+            url=url,
+            headers=headers,
+            method=method,
+            params=params,
+            json=json,
+            verify=verify
+        )
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            logging.warn(f"Error processing request {e}")
+            raise Exception(
+                f"Response content {response.content}, status_code {response.status_code}"
+            )
+        return response.json()
+
+
+class UrlUtils:
+
+    @staticmethod
+    def add_https_if_missing(url):
+        parsed_url = urlparse(url)
+        if not parsed_url.scheme:
+            # If no scheme is provided, assume "https"
+            parsed_url = parsed_url._replace(scheme="https")
+        return urlunparse(parsed_url)
