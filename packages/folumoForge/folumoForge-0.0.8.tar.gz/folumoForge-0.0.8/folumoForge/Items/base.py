6e7f051a9765fe510ac11c89bf40650ea6b4bb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:07:48 +0000

    Add package folumoForge/0.0.8/folumoForge-0.0.8.tar.gz

---
+++ packages/folumoForge/folumoForge-0.0.8.tar.gz/folumoForge-0.0.8/folumoForge/Items/base.py
@@ -0,0 +1,25 @@
+import pygame
+
+from . import Screen
+from ..Mods import modBase
+
+
+class itemBase:
+    mods = {}
+    rect = pygame.Rect((0, 0), (0, 0))
+    screen: Screen = None
+    xy = (0, 0)
+    wh = ()
+    color = "blue"
+
+    def delete(self):
+        for item in self.screen.Items:
+            if item == self:
+                self.screen.Items.remove(self)
+
+    def addMod(self, mod: modBase):
+        if mod.name not in self.mods:
+            self.mods[mod.name] = mod
+
+    def update(self):
+        pass
---
+++ packages/folumoForge/folumoForge-0.0.8.tar.gz/folumoForge-0.0.8/folumoForge/Mods/__init__.py
@@ -0,0 +1,5 @@
+from .base import modBase
+
+from .Collision import modCollision
+from .Gravity import modGravity
+from .OutOfScreen import modOutOfScreenTP
