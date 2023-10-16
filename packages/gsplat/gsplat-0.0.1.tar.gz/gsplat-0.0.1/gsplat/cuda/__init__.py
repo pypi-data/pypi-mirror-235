797bbbc649ede7c6e3c0f68e431fe684114a7c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:09:39 +0000

    Add package gsplat/0.0.1/gsplat-0.0.1.tar.gz

---
+++ packages/gsplat/gsplat-0.0.1.tar.gz/gsplat-0.0.1/gsplat/cuda/__init__.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+
+def _make_lazy_cuda_func(name: str) -> Callable:
+    def call_cuda(*args, **kwargs):
+        # pylint: disable=import-outside-toplevel
+        from ._backend import _C
+
+        return getattr(_C, name)(*args, **kwargs)
+
+    return call_cuda
+
+
+nd_rasterize_forward = _make_lazy_cuda_func("nd_rasterize_forward")
+nd_rasterize_backward = _make_lazy_cuda_func("nd_rasterize_backward")
+rasterize_forward = _make_lazy_cuda_func("rasterize_forward")
+rasterize_backward = _make_lazy_cuda_func("rasterize_backward")
+compute_cov2d_bounds_forward = _make_lazy_cuda_func("compute_cov2d_bounds_forward")
+project_gaussians_forward = _make_lazy_cuda_func("project_gaussians_forward")
+project_gaussians_backward = _make_lazy_cuda_func("project_gaussians_backward")
+compute_sh_forward = _make_lazy_cuda_func("compute_sh_forward")
+compute_sh_backward = _make_lazy_cuda_func("compute_sh_backward")
+compute_cumulative_intersects = _make_lazy_cuda_func("compute_cumulative_intersects")
+map_gaussian_to_intersects = _make_lazy_cuda_func("map_gaussian_to_intersects")
+get_tile_bin_edges = _make_lazy_cuda_func("get_tile_bin_edges")
+bin_and_sort_gaussians = _make_lazy_cuda_func("bin_and_sort_gaussians")
+rasterize_forward_kernel = _make_lazy_cuda_func("rasterize_forward_kernel")
