6476f7ee9ae78259b316fe50ffea8b5cff259dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 04:04:54 +0000

    Add package eai-commons/0.0.5.dev0/eai_commons-0.0.5.dev0-py3-none-any.whl

---
+++ packages/eai-commons/eai_commons-0.0.5.dev0-py3-none-any.whl/eai_commons_tests/unit_tests/test_s3_client.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+from eai_commons.s3_client import S3Client
+from eai_commons.utils.file import file_relative_path
+
+bucket_name = os.getenv("S3_HDY_BUCKET_NAME")
+
+client = S3Client(
+    os.getenv("S3_ENDPOINT"),
+    False,
+    os.getenv("S3_ACCESS_KEY_ID"),
+    os.getenv("S3_ACCESS_KEY_SECRET"),
+)
+
+
+def test_s3_client_crud():
+    local_file = file_relative_path(__file__, "../resources/csv_file.csv")
+    s3_file_suff_path = "eai_common_test/csv_file.csv"
+    download_dir = file_relative_path(__file__, "../resources/test")
+    with open(local_file, "rb") as file:
+        bytes_ = file.read()
+        client.upload_s3_object(bucket_name, bytes_, s3_file_suff_path)
+
+    files_ = client.list_s3_files(bucket_name, "/eai_common_test")
+    assert s3_file_suff_path in files_
+
+    client.download_s3_files_local(bucket_name, [s3_file_suff_path], download_dir)
+
+    client.delete_file(bucket_name, s3_file_suff_path)
+    if os.path.exists(download_dir):
+        shutil.rmtree(download_dir)
