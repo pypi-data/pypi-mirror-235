bc437f2a98b98c776aa8e2240361e2a5ff07c1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:06:28 +0000

    Add package androtools/0.1.21/androtools-0.1.21-py3-none-any.whl

---
+++ packages/androtools/androtools-0.1.21-py3-none-any.whl/androtools/__init__.py
@@ -0,0 +1,31 @@
+import sys
+from logging import DEBUG, INFO, getLevelName
+
+from loguru import logger
+
+# DEBUG_MODE = True
+DEBUG_MODE = False
+
+
+if 0 in logger._core.handlers:
+    logger.remove(0)
+
+_level = getLevelName(INFO)
+_sink = f"{__name__}.log"
+
+if DEBUG_MODE:
+    _level = getLevelName(DEBUG)
+    _sink = sys.stdout
+
+
+def log_filter(record):
+    return __name__ in record["file"].path
+
+
+logger.add(
+    _sink,
+    filter=log_filter,
+    level=_level,
+    backtrace=True,
+    diagnose=True,
+)
