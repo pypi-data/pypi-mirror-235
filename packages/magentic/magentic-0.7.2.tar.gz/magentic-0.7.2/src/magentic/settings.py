7791b09788b6284b0410593d7373a53cf4a6d09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:08:40 +0000

    Add package magentic/0.7.2/magentic-0.7.2.tar.gz

---
+++ packages/magentic/magentic-0.7.2.tar.gz/magentic-0.7.2/src/magentic/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="MAGENTIC_")
+
+    openai_model: str = "gpt-3.5-turbo"
+    openai_max_tokens: int | None = None
+    openai_temperature: float | None = None
+
+
+def get_settings() -> Settings:
+    return Settings()
