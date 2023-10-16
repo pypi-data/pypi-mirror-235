e01b4efc1432035f8f0c9e24810a01587f6be770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 06:04:53 +0000

    Add package myokit/1.35.4/myokit-1.35.4-py3-none-any.whl

---
+++ packages/myokit/myokit-1.35.4-py3-none-any.whl/myokit/formats/cpp/_ewriter.py
@@ -0,0 +1,15 @@
+#
+# C++ expression writer
+#
+# This file is part of Myokit.
+# See http://myokit.org for copyright, sharing, and licensing details.
+#
+from myokit.formats.ansic import AnsiCExpressionWriter
+
+
+class CppExpressionWriter(AnsiCExpressionWriter):
+    """
+    This :class:`ExpressionWriter <myokit.formats.ExpressionWriter>` translates
+    myokit :class:`expressions <myokit.Expression>` to their C++ equivalent.
+    """
+    pass
