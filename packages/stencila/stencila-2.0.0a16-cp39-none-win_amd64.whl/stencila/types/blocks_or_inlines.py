e9b0857d22178389411a9a3ba3dfbfb52bf4a672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:46 +0000

    Add package stencila/2.0.0a16/stencila-2.0.0a16-cp39-none-win_amd64.whl

---
+++ packages/stencila/stencila-2.0.0a16-cp39-none-win_amd64.whl/stencila/types/blocks_or_inlines.py
@@ -0,0 +1,15 @@
+# Generated file; do not edit. See the Rust `schema-gen` crate.
+
+from .prelude import *
+
+Blocks = ForwardRef("Blocks")
+Inlines = ForwardRef("Inlines")
+
+
+BlocksOrInlines = Union[
+    Blocks,
+    Inlines,
+]
+"""
+`Blocks` or `Inlines`
+"""
