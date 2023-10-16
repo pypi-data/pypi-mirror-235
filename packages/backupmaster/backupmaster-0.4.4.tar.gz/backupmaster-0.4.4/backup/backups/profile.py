f78fbff66f112b1deedadbd3170d9ed29071187d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:19 +0000

    Add package backupmaster/0.4.4/backupmaster-0.4.4.tar.gz

---
+++ packages/backupmaster/backupmaster-0.4.4.tar.gz/backupmaster-0.4.4/backup/backups/profile.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+from .. import backup
+from ..utils import Path, parser
+
+
+@dataclass
+class Backup(backup.Backup):
+    source: Path = Path.HOME
+    quiet: bool = True
+
+    def __post_init__(self):
+        rules = parser.Rules(Path.profile_paths.yaml, root=Backup.source)
+        self.paths = rules.get_paths()
+        super().__post_init__()
+        self.set_dest(self.profile_name)
+
+    def set_dest(self, profile_name: str):
+        self.dest = Path.profiles / profile_name
+
+    @property
+    def profile_name(self):
+        return Path.active_profile.text.strip() or "light"
+
+    @profile_name.setter
+    def profile_name(self, value):
+        Path.active_profile.text = value
+        self.set_dest(value)
+
+    def apply_profile(self, value):
+        if value != self.profile_name:
+            self.push()
+            self.profile_name = value
+            self.pull()
+
+    def reload(self):
+        if Path.active_profile.exists():
+            self.pull()
