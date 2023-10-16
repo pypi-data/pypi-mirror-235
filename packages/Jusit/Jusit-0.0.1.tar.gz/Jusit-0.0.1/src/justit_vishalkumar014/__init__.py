0d77f243661bb2e5d38eb5e2adc8b7944796bc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:04 +0000

    Add package Jusit/0.0.1/Jusit-0.0.1.tar.gz

---
+++ packages/Jusit/Jusit-0.0.1.tar.gz/Jusit-0.0.1/src/justit_vishalkumar014/__init__.py
@@ -0,0 +1,6 @@
+class JustIT:
+    def __init__(self):
+        pass
+
+    def printJust(self):
+        print("This package build by vishal kumar")
---
+++ packages/Jusit/Jusit-0.0.1.tar.gz/Jusit-0.0.1/src/justit_vishalkumar014/core.py
@@ -0,0 +1,1 @@
+print("This package build by vishal kumar")
