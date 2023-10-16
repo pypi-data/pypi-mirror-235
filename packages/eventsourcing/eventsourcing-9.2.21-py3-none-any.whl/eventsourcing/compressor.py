112c63abbb36ff5c4cba6188e90c6bab8f28ed62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 18:07:05 +0000

    Add package eventsourcing/9.2.21/eventsourcing-9.2.21-py3-none-any.whl

---
+++ packages/eventsourcing/eventsourcing-9.2.21-py3-none-any.whl/eventsourcing/compressor.py
@@ -0,0 +1,17 @@
+import zlib
+
+from eventsourcing.persistence import Compressor
+
+
+class ZlibCompressor(Compressor):
+    def compress(self, data: bytes) -> bytes:
+        """
+        Compress bytes using zlib.
+        """
+        return zlib.compress(data)
+
+    def decompress(self, data: bytes) -> bytes:
+        """
+        Decompress bytes using zlib.
+        """
+        return zlib.decompress(data)
