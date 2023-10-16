ba4a62b96e2b10a55faa6b06698ae4f896ba74e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:02 +0000

    Add package cybrex/1.10.6/cybrex-1.10.6.tar.gz

---
+++ packages/cybrex/cybrex-1.10.6.tar.gz/cybrex-1.10.6/cybrex/data_source/geck_data_source.py
@@ -0,0 +1,44 @@
+from typing import (
+    List,
+    Optional,
+)
+
+from stc_geck.advices import BaseDocumentHolder
+from stc_geck.client import StcGeck
+
+from .base import (
+    BaseDataSource,
+    SourceDocument,
+)
+
+
+class GeckDataSource(BaseDataSource):
+    def __init__(self, geck: StcGeck):
+        self.geck = geck
+
+    async def search_documents(
+        self,
+        query: str,
+        limit: int = 5,
+        sources: Optional[List[str]] = None
+    ) -> List[SourceDocument]:
+        documents = await self.geck.get_summa_client().search_documents({
+            'index_alias': self.geck.index_alias,
+            'query': {'boolean': {'subqueries': [
+                {'occur': 'must', 'query': {'match': {'value': query.lower()}}},
+                {'occur': 'must', 'query': {'term': {'field': 'languages', 'value': 'en'}}},
+            ]}},
+            'collectors': [{'top_docs': {'limit': limit}}],
+            'is_fieldnorms_scoring_enabled': False,
+        })
+        source_documents = []
+        for document in documents:
+            document_holder = BaseDocumentHolder(document)
+            document_id = document_holder.get_internal_id()
+            if not document_id:
+                continue
+            source_documents.append(SourceDocument(
+                document=document,
+                document_id=document_id,
+            ))
+        return source_documents
