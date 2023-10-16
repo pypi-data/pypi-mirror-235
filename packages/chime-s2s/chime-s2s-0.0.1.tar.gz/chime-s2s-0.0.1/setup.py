d7d85d980348576bd88a821e8b0bb6853d8eba96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:32 +0000

    Add package chime-s2s/0.0.1/chime-s2s-0.0.1.tar.gz

---
+++ packages/chime-s2s/chime-s2s-0.0.1.tar.gz/chime-s2s-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-s2s',
+    packages=find_packages(include=['chime-s2s']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
