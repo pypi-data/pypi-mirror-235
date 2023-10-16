e75633e9a7a4cef791928b74cd7fc59e5b777571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:32 +0000

    Add package chime-ml-feature-store-client/0.0.1/chime-ml-feature-store-client-0.0.1.tar.gz

---
+++ packages/chime-ml-feature-store-client/chime-ml-feature-store-client-0.0.1.tar.gz/chime-ml-feature-store-client-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-ml-feature-store-client',
+    packages=find_packages(include=['chime-ml-feature-store-client']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
