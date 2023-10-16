8d62b0ba8b9d93d65ad79ee8c52273da9e4c9e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:29 +0000

    Add package pywa/1.7.2/pywa-1.7.2-py3-none-any.whl

---
+++ packages/pywa/pywa-1.7.2-py3-none-any.whl/pywa/utils.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, fields
+from enum import Enum
+from importlib import import_module
+from typing import Protocol, Any, Callable
+
+
+def is_fastapi_app(app):
+    """Check if the app is a FastAPI app."""
+    try:
+        return isinstance(app, import_module("fastapi").FastAPI)
+    except ImportError:
+        return False
+
+
+def is_flask_app(app):
+    """Check if the app is a Flask app."""
+    try:
+        return isinstance(app, import_module("flask").Flask)
+    except ImportError:
+        return False
+
+
+def get_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """
+    Calculate the distance between two points on Earth using the Haversine formula.
+    """
+    math = import_module("math")
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+    return (2 * math.asin(
+        math.sqrt(
+            math.sin((lat2 - lat1) / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1) / 2) ** 2)
+    )) * 6371
+
+
+class StrEnum(str, Enum):
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}.{self.name}"
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class FromDict:
+    """Allows to ignore extra fields when creating a dataclass from a dict."""
+
+    # noinspection PyArgumentList
+    @classmethod
+    def from_dict(cls, data: dict, **kwargs):
+        data.update(kwargs)
+        return cls(**{
+            k: v for k, v in data.items()
+            if k in (f.name for f in fields(cls))
+        })
+
+
+class FastAPI(Protocol):
+    def get(self, path: str) -> Callable:
+        ...
+
+    def post(self, path: str) -> Callable:
+        ...
+
+
+class Flask(Protocol):
+    def route(self, rule: str, **options: Any) -> Callable:
+        ...
+
