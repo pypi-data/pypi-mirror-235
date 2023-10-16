04c34d478cfd0e56e5906ab7f41c3d85be6d889a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 18:06:29 +0000

    Add package pyccel/1.9.2/pyccel-1.9.2.tar.gz

---
+++ packages/pyccel/pyccel-1.9.2.tar.gz/pyccel-1.9.2/tests/errors/semantic/blocking/INTERFACE_WRONG_TYPES.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring
+from pyccel.decorators import template
+
+@template('z', types=['int', 'real'])
+@template('y', types=['int', 'real'])
+def multi_tmplt_1(x : 'z', y : 'z', z : 'y'):
+    """Tests Interfaces"""
+    return x + y + z
+
+def tst_multi_tmplt_1():
+    """Tests call of the above function"""
+    x = multi_tmplt_1(5, 5.3, 7)
+    return x
