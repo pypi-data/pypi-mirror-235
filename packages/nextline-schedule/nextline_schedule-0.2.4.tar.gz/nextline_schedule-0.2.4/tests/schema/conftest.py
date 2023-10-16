75ee6a45d44d66bae419cb71a10c85cb57c66b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:42 +0000

    Add package nextline-schedule/0.2.4/nextline_schedule-0.2.4.tar.gz

---
+++ packages/nextline-schedule/nextline_schedule-0.2.4.tar.gz/nextline_schedule-0.2.4/tests/schema/conftest.py
@@ -0,0 +1,19 @@
+import asyncio
+from typing import AsyncIterator
+
+import pytest
+from async_asgi_testclient import TestClient
+from nextlinegraphql import create_app
+from starlette.applications import Starlette
+
+
+@pytest.fixture
+async def client(app: Starlette) -> AsyncIterator[TestClient]:
+    async with TestClient(app) as y:
+        await asyncio.sleep(0)
+        yield y
+
+
+@pytest.fixture
+def app() -> Starlette:
+    return create_app()
