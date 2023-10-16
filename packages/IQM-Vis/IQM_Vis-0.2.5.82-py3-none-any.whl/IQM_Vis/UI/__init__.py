d922e1ab3dc57a68287c6fd554d4e1c87557d1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:04:46 +0000

    Add package IQM-Vis/0.2.5.82/IQM_Vis-0.2.5.82-py3-none-any.whl

---
+++ packages/IQM-Vis/IQM_Vis-0.2.5.82-py3-none-any.whl/IQM_Vis/UI/__init__.py
@@ -0,0 +1,6 @@
+from .images import images
+from .widgets import widgets
+from .layout import layout
+from .custom_widgets import ProgressBar, ClickLabel
+from .threads import get_range_results_worker
+from .experiment_mode import make_experiment
---
+++ packages/IQM-Vis/IQM_Vis-0.2.5.82-py3-none-any.whl/IQM_Vis/examples/__init__.py
@@ -0,0 +1,6 @@
+from .multiple import run
+from .simple import run
+from .dataset import run
+from .all import run
+from .kodak import run
+import IQM_Vis.examples.images
