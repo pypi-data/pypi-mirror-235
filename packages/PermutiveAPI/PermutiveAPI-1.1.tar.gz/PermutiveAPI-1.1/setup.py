b8a2adb126fab8ef481b3e505d8c2802ac757f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:05:39 +0000

    Add package PermutiveAPI/1.1/PermutiveAPI-1.1.tar.gz

---
+++ packages/PermutiveAPI/PermutiveAPI-1.1.tar.gz/PermutiveAPI-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PermutiveAPI',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
