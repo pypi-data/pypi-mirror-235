d94c7a53a1067554dc18016bf485744e03875f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:11:42 +0000

    Add package bsdiffhs/0.3.0/bsdiffhs-0.3.0-cp38-cp38-win_amd64.whl

---
+++ packages/bsdiffhs/bsdiffhs-0.3.0-cp38-cp38-win_amd64.whl/bsdiffhs/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from bsdiffhs.format import (diff, patch, file_diff, file_patch,
+                            file_patch_inplace)
+
+
+__version__ = "0.3.0"
+
+
+def test(verbosity=1):
+    from .test_all import run
+    return run(verbosity=verbosity)
