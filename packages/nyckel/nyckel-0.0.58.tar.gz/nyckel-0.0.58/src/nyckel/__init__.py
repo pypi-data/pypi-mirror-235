8e62755e080ade8e30d67fa09ee3b886937013fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:04 +0000

    Add package nyckel/0.0.58/nyckel-0.0.58.tar.gz

---
+++ packages/nyckel/nyckel-0.0.58.tar.gz/nyckel-0.0.58/src/nyckel/__init__.py
@@ -0,0 +1,20 @@
+from .auth import OAuth2Renewer  # noqa: F401
+from .data_classes import NyckelId  # noqa: F401
+from .functions.classification.classification import (
+    ClassificationAnnotation,  # noqa: F401
+    ClassificationFunction,  # noqa: F401
+    ClassificationLabel,  # noqa: F401
+    ClassificationPrediction,  # noqa: F401
+    ClassificationSample,  # noqa: F401
+    ImageClassificationSample,  # noqa: F401
+    ImageSampleData,  # noqa: F401
+    TabularClassificationSample,  # noqa: F401
+    TabularFunctionField,  # noqa: F401
+    TabularSampleData,  # noqa: F401
+    TextClassificationSample,  # noqa: F401
+    TextSampleData,  # noqa: F401
+)
+from .functions.classification.factory import ClassificationFunctionFactory  # noqa: F401
+from .functions.classification.image_classification import ImageClassificationFunction  # noqa: F401
+from .functions.classification.tabular_classification import TabularClassificationFunction  # noqa: F401
+from .functions.classification.text_classification import TextClassificationFunction  # noqa: F401
