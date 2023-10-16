b39a9ba561ce4d7b76349264ee092e6108fd22d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:54 +0000

    Add package pyautd3/16.0.0/pyautd3-16.0.0-py3-none-manylinux2014_aarch64.whl

---
+++ packages/pyautd3/pyautd3-16.0.0-py3-none-manylinux2014_aarch64.whl/pyautd3/gain/gain.py
@@ -0,0 +1,54 @@
+'''
+File: gain.py
+Project: gain
+Created Date: 29/08/2023
+Author: Shun Suzuki
+-----
+Last Modified: 29/09/2023
+Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+-----
+Copyright (c) 2023 Shun Suzuki. All rights reserved.
+
+'''
+
+
+import functools
+import numpy as np
+from typing import Callable, Dict
+from abc import ABCMeta, abstractmethod
+from ctypes import POINTER
+
+from pyautd3.native_methods.autd3capi import Drive
+from pyautd3.native_methods.autd3capi import NativeMethods as Base
+from pyautd3.native_methods.autd3capi_def import GainPtr
+from pyautd3.geometry import Transducer, Geometry, Device
+from pyautd3.internal.gain import IGain
+
+
+class Gain(IGain, metaclass=ABCMeta):
+    """Base class of custom Gain
+
+    """
+
+    @abstractmethod
+    def calc(self, geometry: Geometry) -> Dict[int, np.ndarray]:
+        pass
+
+    def gain_ptr(self, geometry: Geometry) -> GainPtr:
+        drives = self.calc(geometry)
+        return functools.reduce(
+            lambda acc, dev: Base().gain_custom_set(acc, dev.idx,
+                                                    drives[dev.idx].ctypes.data_as(POINTER(Drive)),  # type: ignore
+                                                    len(drives[dev.idx])),
+            geometry.devices(),
+            Base().gain_custom(),
+        )
+
+    @staticmethod
+    def transform(geometry: Geometry, f: Callable[[Device, Transducer], Drive]) -> Dict[int, np.ndarray]:
+        return dict(
+            map(
+                lambda dev: (dev.idx, np.fromiter(map(lambda tr: np.void(f(dev, tr)), dev), dtype=Drive)),  # type: ignore
+                geometry.devices(),
+            )
+        )
