7ea25194cfd66f8deaa6b693578792335bfbe893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:09 +0000

    Add package pyfigure98/1.0.1/pyfigure98-1.0.1.tar.gz

---
+++ packages/pyfigure98/pyfigure98-1.0.1.tar.gz/pyfigure98-1.0.1/src/pyfigure98/Figure/_place_holder.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+class Figure:
+    def __init__(self):
+        self.graphs: dict[str, Graph_]
+
+        self.templates: dict[str, dict]
+
+        self.fig: matplotlib.figure.Figure
+
+
+class Graph_:
+    def __init__(self, fig: Figure, subPlot: plt.Axes):
+        self.fig: Figure
+        self.plot: plt.Axes
+        self.plot_labels = [[], []]
+
+        self._x_axis_params: dict[str, bool]
+
+        self._y_axis_params: dict[str, bool]
+
+        self._is_legend_plotted: bool
+        self._mappable: bool
