63a29e93d301b50ce8b778868d41191fbb180563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:06:55 +0000

    Add package embermaker/2.0.0a3/embermaker-2.0.0a3-py3-none-any.whl

---
+++ packages/embermaker/embermaker-2.0.0a3-py3-none-any.whl/embermaker/__init__.py
@@ -0,0 +1,6 @@
+"""
+EmberMaker is a cientific graphic library aimed at (re)producing "burning ember" diagrams
+of the style used in IPCC (Intergovernmental Panel on Climate Change) reports.
+"""
+import importlib.metadata
+__version__= importlib.metadata.version('embermaker')
