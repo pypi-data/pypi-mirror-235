22208d90b7a547dd547ad58926a8665ade126f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:09:04 +0000

    Add package funix/0.5.1/funix-0.5.1.tar.gz

---
+++ packages/funix/funix-0.5.1.tar.gz/funix-0.5.1/backend/funix/app/__init__.py
@@ -0,0 +1,24 @@
+"""
+Save the app instance here
+"""
+
+from secrets import token_hex
+
+import flask
+
+app = flask.Flask(__name__)
+app.secret_key = token_hex(16)
+app.config.update(
+    SESSION_COOKIE_PATH="/",
+    SESSION_COOKIE_SAMESITE="Lax",
+)
+
+
+@app.after_request
+def funix_auto_cors(response: flask.Response) -> flask.Response:
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers[
+        "Access-Control-Allow-Methods"
+    ] = "GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
