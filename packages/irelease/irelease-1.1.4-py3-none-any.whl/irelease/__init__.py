2e39f8570561837634a9b980a60537eb13b63ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:11:38 +0000

    Add package irelease/1.1.4/irelease-1.1.4-py3-none-any.whl

---
+++ packages/irelease/irelease-1.1.4-py3-none-any.whl/irelease/__init__.py
@@ -0,0 +1,28 @@
+from irelease.irelease import(
+    make_script,
+    main)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '1.1.4'
+
+
+# module level doc-string
+__doc__ = """
+irelease - irelease is an Python package to release your package
+=====================================================================
+
+**irelease**
+
+Examples
+--------
+>>> # Go to the root of the directory package you aim to release.
+>>> irelease
+>>> # Description of the input arguments:
+>>> irelease --help
+
+References
+----------
+* https:https://github.com/erdogant/irelease
+
+"""
