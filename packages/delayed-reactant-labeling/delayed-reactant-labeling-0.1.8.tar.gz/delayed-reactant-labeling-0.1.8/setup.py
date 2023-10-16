9b9734520c4514c5b410ffb0068f19f702d5b788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:09:36 +0000

    Add package delayed-reactant-labeling/0.1.8/delayed-reactant-labeling-0.1.8.tar.gz

---
+++ packages/delayed-reactant-labeling/delayed-reactant-labeling-0.1.8.tar.gz/delayed-reactant-labeling-0.1.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    install_requires=['numpy',
+                      'moviepy',
+                      'scipy',
+                      'matplotlib',
+                      'scikit-learn',
+                      'numba',
+                      'tqdm',
+                      'openpyxl',
+                      'pandas'],
+)
