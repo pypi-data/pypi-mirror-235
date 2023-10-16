ef24c5ab50d51d81db89cf235cfb686b7e5c5537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:43:32 +0000

    Add package multimcts/0.6.4/multimcts-0.6.4.tar.gz

---
+++ packages/multimcts/multimcts-0.6.4.tar.gz/multimcts-0.6.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+
+setup(
+    name='multimcts',
+    version='0.6.4',
+    description='Monte Carlo Tree Search for multiple teams',
+    author='Taylor Vance',
+    author_email='mirrors.cities0w@icloud.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/taylorvance/multimcts',
+    packages=find_packages(),
+    ext_modules=cythonize([
+        Extension('multimcts.mcts', ['multimcts/mcts.pyx']),
+    ]),
+    package_data={'multimcts': ['*.pyx']},
+)
