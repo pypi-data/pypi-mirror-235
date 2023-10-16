4fb379f4af9fe0dc674b2ec54b6b05ffb0e9c363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:44 +0000

    Add package xapi-python/0.1.7/xapi-python-0.1.7.tar.gz

---
+++ packages/xapi-python/xapi-python-0.1.7.tar.gz/xapi-python-0.1.7/xapi/__init__.py
@@ -0,0 +1,13 @@
+"""
+.. moduleauthor:: Paweł Knioła <pawel.kn@gmail.com>
+"""
+
+name = "xapi"
+__version__ = "0.1.7"
+
+from .xapi import XAPI, connect
+from .enums import TradeCmd, TradeType, TradeStatus, PeriodCode
+from .connection import Connection
+from .socket import Socket
+from .stream import Stream
+from .exceptions import ConnectionClosed, LoginFailed
