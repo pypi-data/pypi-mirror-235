5c565362f2a4ec8b94a127415ee145137d197b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:08:45 +0000

    Add package emblio/0.1.0/emblio-0.1.0-py3-none-any.whl

---
+++ packages/emblio/emblio-0.1.0-py3-none-any.whl/emblio/__init__.py
@@ -0,0 +1,6 @@
+"""emblio - python package that gets embeddings from various ML models for  different content types, like image video audio text"""
+from emblio.example import test
+
+__version__ = '0.1.0'
+__author__ = 'Nishant Bhansali <nishantbhansali80@gmail.com>'
+__all__ = []
