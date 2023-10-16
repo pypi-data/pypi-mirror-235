d797585e907e86c87b44015584dbb26963d003bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:30 +0000

    Add package chime-schemas/0.0.1/chime-schemas-0.0.1.tar.gz

---
+++ packages/chime-schemas/chime-schemas-0.0.1.tar.gz/chime-schemas-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-schemas',
+    packages=find_packages(include=['chime-schemas']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
