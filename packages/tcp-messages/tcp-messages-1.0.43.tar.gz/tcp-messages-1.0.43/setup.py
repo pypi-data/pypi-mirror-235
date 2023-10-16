3f938008a790325f45c5e75c7c51e4b10513890f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:50:40 +0000

    Add package tcp-messages/1.0.43/tcp-messages-1.0.43.tar.gz

---
+++ packages/tcp-messages/tcp-messages-1.0.43.tar.gz/tcp-messages-1.0.43/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='tcp-messages',description='create json services easily',author='german espinosa',author_email='germanespinosa@gmail.com',packages=['tcp_messages'],install_requires=['json-cpp>=1.0.78'],license='MIT',version='1.0.43',zip_safe=False)
