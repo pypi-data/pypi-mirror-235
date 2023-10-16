5f3009dead2d05e5070e27db811bc3849516f748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:10:09 +0000

    Add package flake8-import-relative-two/0.1.0/flake8_import_relative_two-0.1.0.tar.gz

---
+++ packages/flake8-import-relative-two/flake8_import_relative_two-0.1.0.tar.gz/flake8_import_relative_two-0.1.0/flake8_import_relative_two/__init__.py
@@ -0,0 +1,25 @@
+r"""*Root package definition for* ``flake8-import-relative-two``.
+
+Flake8 plugin that checks for relative imports up to a maximum of level two
+
+**Author**
+    Bartosz Cholewa (bbcholewa@gmail.com)
+
+**File Created**
+    14 Oct 2023
+
+**Copyright**
+    \(c) Bartosz Cholewa 2023
+
+**Source Repository**
+    https://github.com/bartoszcholewa/flake8-import-relative-two
+
+**License**
+    The MIT License; see |license_txt|_ for full license terms
+
+**Members**
+
+"""
+
+from flake8_import_relative_two.core import Plugin  # noqa: F401
+from flake8_import_relative_two.version import __version__  # noqa: F401
