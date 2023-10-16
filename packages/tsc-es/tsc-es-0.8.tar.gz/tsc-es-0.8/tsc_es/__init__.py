80892f45c48aa5f690207127c559a4d32fb7baa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:09:32 +0000

    Add package tsc-es/0.8/tsc-es-0.8.tar.gz

---
+++ packages/tsc-es/tsc-es-0.8.tar.gz/tsc-es-0.8/tsc_es/__init__.py
@@ -0,0 +1,13 @@
+from .mongo_es import (
+    get,
+    creat_template,
+    create_index,
+    check_data_stream_exists,
+    del_template,
+    del_index,
+    es_bulk,
+    get_docs_from_cursor,
+    mongo_to_es,
+    time_to_es_date,
+    es_date_to_datetime,
+)
