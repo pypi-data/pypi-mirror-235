b39e5b4e8e8211d6ba0b8d0b425e1843753b7996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:45:35 +0000

    Add package deadcode/2.2.1/deadcode-2.2.1.tar.gz

---
+++ packages/deadcode/deadcode-2.2.1.tar.gz/deadcode-2.2.1/deadcode/visitor/noqa.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+import re
+from typing import Dict, List, Set
+
+NOQA_REGEXP = re.compile(
+    # Use the same regex as flake8 does.
+    # https://github.com/pycqa/flake8/blob/main/src/flake8/defaults.py
+    # We're looking for items that look like this:
+    # `# noqa`
+    # `# noqa: E123`
+    # `# noqa: E123,W451,F921`
+    # `# NoQA: E123,W451,F921`
+    r"# noqa(?::[\s]?(?P<codes>([A-Z]+[0-9]+(?:[,\s]+)?)+))?",
+    re.IGNORECASE,
+)
+
+NOQA_CODE_MAP = {
+    # flake8 F401: module imported but unused.
+    "F401": "DC007",
+    # flake8 F841: local variable is assigned to but never used.
+    "F841": "DC001",
+    "DC001": "DC001",
+    "DC002": "DC002",
+    "DC003": "DC003",
+    "DC004": "DC004",
+    "DC005": "DC005",
+    "DC006": "DC006",
+    "DC007": "DC007",
+    "DC008": "DC008",
+    "DC009": "DC009",
+    "DC011": "DC011",
+    "DC012": "DC012",
+    "DC013": "DC013",
+}
+
+
+def _parse_error_codes(matches_dict: Dict[str, str]) -> List[str]:
+    # If no error code is specified, add the line to the "all" category.
+    return [c.strip() for c in (matches_dict["codes"] or "all").split(",")]
+
+
+def parse_noqa(code: str) -> Dict[str, Set[int]]:
+    noqa_lines = defaultdict(set)
+    for lineno, line in enumerate(code.split("\n"), start=1):
+        match = NOQA_REGEXP.search(line)
+        if match:
+            for error_code in _parse_error_codes(match.groupdict()):
+                error_code = NOQA_CODE_MAP.get(error_code, error_code)
+                noqa_lines[error_code].add(lineno)
+    return noqa_lines
+
+
+def ignore_line(noqa_lines: Dict[str, Set[int]], lineno: int, error_code: str) -> bool:
+    """Check if the reported line is annotated with "# noqa"."""
+    return lineno in noqa_lines[error_code] or lineno in noqa_lines["all"]
