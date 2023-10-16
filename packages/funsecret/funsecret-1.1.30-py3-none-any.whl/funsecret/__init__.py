15b11d0858451e0c3d44b984d3801f388de03b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:14:14 +0000

    Add package funsecret/1.1.30/funsecret-1.1.30-py3-none-any.whl

---
+++ packages/funsecret/funsecret-1.1.30-py3-none-any.whl/funsecret/__init__.py
@@ -0,0 +1,11 @@
+from .sqlalchemy import create_engine, create_engine_mysql, create_engine_sqlite, Base, BaseTable
+from .fernet import decrypt, encrypt, generate_key, get_md5_file, get_md5_str,file_decrypt,file_encrypt
+from .secret import (
+    SecretManage,
+    SecretTable,
+    load_secret_str,
+    read_secret,
+    save_secret_str,
+    write_secret,
+    load_os_environ,
+)
