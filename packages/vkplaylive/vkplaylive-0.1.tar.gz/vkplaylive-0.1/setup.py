f6069a883b88d411fc86090f08f98286f0b95234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 05:05:56 +0000

    Add package vkplaylive/0.1/vkplaylive-0.1.tar.gz

---
+++ packages/vkplaylive/vkplaylive-0.1.tar.gz/vkplaylive-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='vkplaylive',
+      version='0.1',
+      description='module for connecting to vkplay live and working with it',
+      packages=[''],
+      author_email='michailcoding32@gmail.com',
+      zip_safe=False)
