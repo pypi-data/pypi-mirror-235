aaf04ec75865d7cc2244e92e6740171afcf17581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:20 +0000

    Add package mirageml/0.0.8/mirageml-0.0.8-py3-none-any.whl

---
+++ packages/mirageml/mirageml-0.0.8-py3-none-any.whl/mirageml_pip/commands/__init__.py
@@ -0,0 +1,6 @@
+"""
+This file is the entry point for the commands module. 
+It initializes the module and imports the necessary modules.
+"""
+
+from .hello import hello
---
+++ packages/mirageml/mirageml-0.0.8-py3-none-any.whl/mirageml_pip/commands/hello.py
@@ -0,0 +1,6 @@
+def hello(name):
+    """
+    Prints a greeting to the console.
+    :param name: The name to greet.
+    """
+    print(f"Hello, {name}!")
