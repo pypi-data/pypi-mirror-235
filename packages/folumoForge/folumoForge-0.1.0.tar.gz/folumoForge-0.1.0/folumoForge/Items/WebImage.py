11706c77a140a971fd10fe0b3893e6652c494c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:07:50 +0000

    Add package folumoForge/0.1.0/folumoForge-0.1.0.tar.gz

---
+++ packages/folumoForge/folumoForge-0.1.0.tar.gz/folumoForge-0.1.0/folumoForge/Items/WebImage.py
@@ -0,0 +1,39 @@
+import pygame
+import requests
+from io import BytesIO
+
+from .. import itemBase
+from .. import Screen
+
+
+class WebImage(itemBase):
+    def __init__(self, screen: Screen, url, xy, wh=None):
+        self.screen = screen
+        screen.Items.append(self)
+        self.xy = xy
+
+        response = requests.get(url)
+        image_data = BytesIO(response.content)
+
+        self.img = pygame.image.load(image_data)
+        if wh:
+            self.img = pygame.transform.scale(self.img, wh)
+
+        self.rect = self.img.get_rect()
+
+    def config(self, url=None, xy=None, wh=None):
+        if xy:
+            self.xy = xy
+
+        if url:
+            response = requests.get(url)
+            image_data = BytesIO(response.content)
+            self.img = pygame.image.load(image_data)
+
+        if wh:
+            self.img = pygame.transform.scale(self.img, wh)
+
+        self.rect = self.img.get_rect()
+
+    def update(self):
+        self.screen.root.MainRoot.blit(self.img, self.xy)
---
+++ packages/folumoForge/folumoForge-0.1.0.tar.gz/folumoForge-0.1.0/folumoForge/base.py
@@ -0,0 +1,46 @@
+import pygame
+
+from threading import Thread
+from . import Screen, Forge
+
+
+class modBase:
+    def __init__(self, root: Forge, name, threadFunc):
+        self.name = name
+        if name not in root.modThreads:
+            t = Thread(target=threadFunc)
+            t.start()
+            root.modThreads[name] = t
+
+    def preRender(self, data):
+        ...
+
+    def postRender(self, data):
+        ...
+
+    def Fail(self, data, error):
+        print(f"[ERROR-{self.name}]  : This item has failed to finish loading due to {error}; data PKG: {data}")
+
+    def Success(self, data):
+        print(f"[INFO-{self.name}]   : This item has successfully finished loading; data PKG: {data}")
+
+
+class itemBase:
+    mods = {}
+    rect = pygame.Rect((0, 0), (0, 0))
+    screen: Screen = None
+    xy = (0, 0)
+    wh = ()
+    color = "blue"
+
+    def delete(self):
+        for item in self.screen.Items:
+            if item == self:
+                self.screen.Items.remove(self)
+
+    def addMod(self, mod: modBase):
+        if mod.name not in self.mods:
+            self.mods[mod.name] = mod
+
+    def update(self):
+        pass
