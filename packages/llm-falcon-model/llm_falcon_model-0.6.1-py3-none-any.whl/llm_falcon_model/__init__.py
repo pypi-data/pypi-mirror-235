194249915fab86246a9d8d6c1c24e7f411d64d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:08:01 +0000

    Add package llm-falcon-model/0.6.1/llm_falcon_model-0.6.1-py3-none-any.whl

---
+++ packages/llm-falcon-model/llm_falcon_model-0.6.1-py3-none-any.whl/llm_falcon_model/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.6.1"
+
+from .modelling_RW import FalconStart, FalconMid, FalconEnd, FalconFull
+from .configuration_RW import load_config
+from .tokenization import load_tokenizer
+from .initialization import init_part
+
