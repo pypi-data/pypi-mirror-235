b3eacb7d6ee1240eb891526ee04460783f175baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:08:09 +0000

    Add package jmespath-token/0.3.1/jmespath_token-0.3.1-py3-none-any.whl

---
+++ packages/jmespath-token/jmespath_token-0.3.1-py3-none-any.whl/jmespath_token/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tokens represent values that can be evaluated at a later time. This is a token system built on Jmespath JSON query language.
+"""
+
+from ._version import __version__
+
+__short_description__ = (
+    "Tokens represent values that can be evaluated at a later time. "
+    "This is a token system built on Jmespath JSON query language."
+)
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
---
+++ packages/jmespath-token/jmespath_token-0.3.1-py3-none-any.whl/jmespath_token/types.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import typing as T
+
+# the token that can be evaluated later
+T_TOKEN = T.Any
+
+# the data that can be used to evaluate the token
+T_DATA = T.Any
