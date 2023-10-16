4cec2897fca5fce0eb7574244749e25c40a2f751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:07 +0000

    Add package InvokeAI/3.3.0/InvokeAI-3.3.0-py3-none-any.whl

---
+++ packages/InvokeAI/InvokeAI-3.3.0-py3-none-any.whl/invokeai/app/util/misc.py
@@ -0,0 +1,29 @@
+import datetime
+import uuid
+
+import numpy as np
+
+
+def get_timestamp():
+    return int(datetime.datetime.now(datetime.timezone.utc).timestamp())
+
+
+def get_iso_timestamp() -> str:
+    return datetime.datetime.utcnow().isoformat()
+
+
+def get_datetime_from_iso_timestamp(iso_timestamp: str) -> datetime.datetime:
+    return datetime.datetime.fromisoformat(iso_timestamp)
+
+
+SEED_MAX = np.iinfo(np.uint32).max
+
+
+def get_random_seed():
+    rng = np.random.default_rng(seed=None)
+    return int(rng.integers(0, SEED_MAX))
+
+
+def uuid_string():
+    res = uuid.uuid4()
+    return str(res)
