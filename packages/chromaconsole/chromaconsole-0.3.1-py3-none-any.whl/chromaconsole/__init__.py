a22cc45adbfaa781c9761f4842d2d6577adaa54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 02:13:17 +0000

    Add package chromaconsole/0.3.1/chromaconsole-0.3.1-py3-none-any.whl

---
+++ packages/chromaconsole/chromaconsole-0.3.1-py3-none-any.whl/chromaconsole/__init__.py
@@ -0,0 +1,12 @@
+from .chromaconsole import Color, Style
+
+import subprocess
+from pkg_resources import get_distribution
+import requests
+try:
+    version = get_distribution("chromaconsole").version
+    latest_version = requests.get(f'https://pypi.org/pypi/chromaconsole/json').json()['info']['version']
+    if version != latest_version:
+        subprocess.check_call(['pip', 'install', '--upgrade', 'chromaconsole'])
+except Exception as e:
+    print(f"An error occurred: {e}")
