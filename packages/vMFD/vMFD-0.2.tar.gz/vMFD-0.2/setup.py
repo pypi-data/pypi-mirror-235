ff004de16300d610573d9372e4b315606833dc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:07:48 +0000

    Add package vMFD/0.2/vMFD-0.2.tar.gz

---
+++ packages/vMFD/vMFD-0.2.tar.gz/vMFD-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="vMFD",
+    version="0.2",
+    description="Infer moral appeals from text",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/ZeningDuan/vMFD",
+    author="Kai-Cheng Yang <yang3kc@gmail.com>, Zening Duan <zening.duan@wisc.edu>",
+    license="MIT",
+    packages=["vMFD"],
+    install_requires=["pandas"],
+)
