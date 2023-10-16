53dd90f441a161ce2dc386c217437c711cc57c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 02:13:16 +0000

    Add package oarepo-model-builder-drafts/4.0.11/oarepo_model_builder_drafts-4.0.11-py3-none-any.whl

---
+++ packages/oarepo-model-builder-drafts/oarepo_model_builder_drafts-4.0.11-py3-none-any.whl/oarepo_model_builder_drafts/tests/invenio_drafts_conftest.py
@@ -0,0 +1,16 @@
+from oarepo_model_builder.invenio.invenio_base import InvenioBaseClassPythonBuilder
+
+
+class InvenioDraftsConftestBuilder(InvenioBaseClassPythonBuilder):
+    TYPE = "invenio_drafts_conftest"
+    template = "drafts-conftest"
+    MODULE = "tests.conftest"
+
+    def _get_output_module(self):
+        return f'{self.current_model.definition["tests"]["module"]}.conftest'
+
+    def finish(self, **extra_kwargs):
+        tests = getattr(self.current_model, "section_tests")
+        super().finish(
+            fixtures=tests.fixtures, test_constants=tests.constants, **extra_kwargs
+        )
