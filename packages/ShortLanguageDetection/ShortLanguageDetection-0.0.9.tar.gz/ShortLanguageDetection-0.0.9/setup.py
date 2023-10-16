7a1a0aeb45d8a4b5f9220d92f08d51d82894a348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:44 +0000

    Add package ShortLanguageDetection/0.0.9/ShortLanguageDetection-0.0.9.tar.gz

---
+++ packages/ShortLanguageDetection/ShortLanguageDetection-0.0.9.tar.gz/ShortLanguageDetection-0.0.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='ShortLanguageDetection',
+    description = 'A language detector for short string or chat.',
+    url='https://github.com/Interaction-Bot/LanguageDetection/',
+    author='Jourdelune',
+    license='MIT',
+    version='0.0.9',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'fasttext-wheel',
+        'emoji',
+        'lingua-language-detector'
+    ],
+    keywords = ["language", "detection", "nlp", "identification", "short"],
+    include_package_data=True,
+    package_data={
+        'ShortLanguageDetection.DictDetector': ['*'],
+        'ShortLanguageDetection.DictDetector.dict': ['*'],
+        'ShortLanguageDetection.models': ['*'],
+   },
+)
