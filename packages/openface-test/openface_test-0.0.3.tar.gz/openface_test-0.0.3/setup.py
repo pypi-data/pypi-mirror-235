85dc5d3db3695cf85433894d5c53361b58968896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:08:49 +0000

    Add package openface-test/0.0.3/openface_test-0.0.3.tar.gz

---
+++ packages/openface-test/openface_test-0.0.3.tar.gz/openface_test-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openface_test',
+    version='0.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'openface_test': ['Utilities.pyd'],
+    },
+    data_files=[('', ['opencv_world410.dll'])],
+)
