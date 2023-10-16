81808a96e7ca1074e0c1368f278123b1832807da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:27 +0000

    Add package mknodes/0.43.11/mknodes-0.43.11-py3-none-any.whl

---
+++ packages/mknodes/mknodes-0.43.11-py3-none-any.whl/mknodes/navs/mkemptywebsite.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from mknodes.navs import mknav
+from mknodes.utils import log
+
+
+logger = log.get_logger(__name__)
+
+
+class MkEmptyWebsite(mknav.MkNav):
+    """Non-populated MkNav which parses given static pages dict."""
+
+    def __init__(self, static_pages: dict | None = None, **kwargs):
+        super().__init__(**kwargs)
+        import mknodes
+
+        page = self.add_index_page("Overview", hide="toc")
+        page += mknodes.MkText(r"metadata.description", is_jinja_expression=True)
+        static_pages = static_pages or {}
+        self.parse.json(static_pages)
+
+    @classmethod
+    def for_project(cls, project, **kwargs):
+        root = cls(project=project, **kwargs)
+        project.set_root(root)
+        return root
+
+
+if __name__ == "__main__":
+    import mknodes
+
+    doc = MkEmptyWebsite.for_project(
+        mknodes.Project.for_mknodes(),
+        static_pages={
+            "Usage": "https://raw.githubusercontent.com/mkdocs/mkdocs/master/docs/getting-started.md",
+        },
+    )
+    print(doc)
