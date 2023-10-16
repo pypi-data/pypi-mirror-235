7dfabad760b25840b2bef86bc7c3d594549975d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:04:11 +0000

    Add package chck/0.0.6/chck-0.0.6.tar.gz

---
+++ packages/chck/chck-0.0.6.tar.gz/chck-0.0.6/chck/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.0.6"
+from .bool import *
+from .core import *
+from .comp import *
+
+from .insp import *
+from .strs import *
+from .path import *
+
+from .dcls import *
+from .misc import *
+
+# specific libs:
+# from .isnp import *
+# from .ispd import *
+# from .trch import *
