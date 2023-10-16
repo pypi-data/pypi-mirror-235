400dce4ffb94e97030bc6d2d5cf39a4c9c3e468d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:21 +0000

    Add package mirageml/0.0.10/mirageml-0.0.10.tar.gz

---
+++ packages/mirageml/mirageml-0.0.10.tar.gz/mirageml-0.0.10/mirageml_pip/__main__.py
@@ -0,0 +1,18 @@
+import argparse
+from .commands import hello
+
+def main():
+    parser = argparse.ArgumentParser(description="mypackage CLI")
+    subparsers = parser.add_subparsers(dest="command")
+
+    hello_parser = subparsers.add_parser('hello', help='prints hello world')
+
+    args = parser.parse_args()
+
+    if args.command == "hello":
+        hello()
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
