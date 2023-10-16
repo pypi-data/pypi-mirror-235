e1d0498b31afff2b28a65bdce0ebea0a45c3e2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:54:14 +0000

    Add package pylingdocs/0.1.0/pylingdocs-0.1.0.tar.gz

---
+++ packages/pylingdocs/pylingdocs-0.1.0.tar.gz/pylingdocs-0.1.0/src/pylingdocs/data/project_template/hooks/post_gen_project.py
@@ -0,0 +1,44 @@
+from configparser import ConfigParser
+from pathlib import Path
+
+import cookiecutter
+from writio import dump
+
+from pylingdocs.cldf import generate_autocomplete
+from pylingdocs.helpers import _load_cldf_dataset
+
+PROJECT_DIR = Path.cwd().resolve()
+
+
+def remove_file(filename):
+    (PROJECT_DIR / filename).unlink()
+
+
+def create_file(file):
+    filename = file + ".md"
+    filepath = PROJECT_DIR / "content" / filename
+    dump(
+        f"# {file.capitalize()} [label]({file})\n\nInsert your content here (find this file at {filepath.resolve()})",
+        filepath,
+    )
+
+
+if "No license" == "{{ cookiecutter.license }}":
+    remove_file("LICENSE")
+
+for i, file in enumerate({{cookiecutter.files.split(",")}}):
+    create_file(file)
+
+# add .. to path if relative to pylingdocs project
+config = ConfigParser()
+config.read(PROJECT_DIR / "pylingdocs.cfg")
+path = "{{ cookiecutter.cldf }}"
+if not path.startswith("/"):
+    path = Path("..") / path
+config.set("paths", "cldf", str(path))
+with open(PROJECT_DIR / "pylingdocs.cfg", "w", encoding="utf-8") as configfile:
+    config.write(configfile)
+
+if "Yes" == "{{ cookiecutter.use_sublime_text }}":
+    ds = _load_cldf_dataset(path)
+    generate_autocomplete(ds, PROJECT_DIR)
