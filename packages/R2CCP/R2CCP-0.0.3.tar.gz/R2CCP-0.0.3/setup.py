e975a89b19ac9c9f821d5ddcfddfe3a37fb1b166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:10:04 +0000

    Add package R2CCP/0.0.3/R2CCP-0.0.3.tar.gz

---
+++ packages/R2CCP/R2CCP-0.0.3.tar.gz/R2CCP-0.0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'R2CCP First Package'
+LONG_DESCRIPTION = 'R2CCP First Package'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="R2CCP", 
+        version=VERSION,
+        author="Etash Guha",
+        author_email="etashguha@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+    "ConfigArgParse==1.7",
+    "lightning_bolts==0.7.0",
+    "numpy==1.22.1",
+    "pandas==2.0.3",
+    "pyinterval==1.2.0",
+    "pytorch_lightning==1.9.5",
+    "scikit_learn==1.3.0",
+    "scipy==1.7.3",
+    "six==1.16.0",
+    'torch==2.0.1',
+    "tqdm==4.66.1"
+], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
