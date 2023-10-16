106bc4b86aa1feedd54e8f7cbef07f2fbba2e76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:04:21 +0000

    Add package threedi-mi-utils/0.0.1/threedi-mi-utils-0.0.1.tar.gz

---
+++ packages/threedi-mi-utils/threedi-mi-utils-0.0.1.tar.gz/threedi-mi-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='threedi-mi-utils',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/nens/threedi-mi-utils',
+    license='GNU General Public License v3.0',
+    author='Łukasz Dębek',
+    description='Python package with tools for managing the working 3Di directory structure'
+)
