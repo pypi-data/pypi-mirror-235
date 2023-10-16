fae4d3e6e93c00105dbf4cd1b802584aeaa97b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:07 +0000

    Add package qecore/3.20/qecore-3.20.tar.gz

---
+++ packages/qecore/qecore-3.20.tar.gz/qecore-3.20/qecore/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+QECore __init__ file.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__author__ = """Michal Odehnal <modehnal@redhat.com>"""
+__version__ = "3.20"
+__copyright__ = "Copyright © 2018-2023 Red Hat, Inc."
+__license__ = "GPL"
+__all__ = (
+    "application",
+    "common_steps",
+    "flatpak",
+    "get_node",
+    "image_matching",
+    "logger",
+    "online_accounts",
+    "sandbox",
+    "utility",
+    "icons",
+)
