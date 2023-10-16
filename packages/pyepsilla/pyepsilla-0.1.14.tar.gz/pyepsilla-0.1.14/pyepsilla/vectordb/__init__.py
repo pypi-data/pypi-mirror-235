afcb961e1635179f50208f8586804cf495e64e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:12 +0000

    Add package pyepsilla/0.1.14/pyepsilla-0.1.14.tar.gz

---
+++ packages/pyepsilla/pyepsilla-0.1.14.tar.gz/pyepsilla-0.1.14/pyepsilla/vectordb/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from .client import Client
+from .field import Field, FieldType
+from .sentry import init_sentry
+from .telemetry import TelemetryManager
+
+init_sentry()
+
+telemetry_manager = TelemetryManager.get_singleton()
