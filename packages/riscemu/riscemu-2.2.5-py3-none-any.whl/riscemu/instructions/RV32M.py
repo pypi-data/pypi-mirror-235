a83268fc9bd396845dc822ac607fc683c750e5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:05:10 +0000

    Add package riscemu/2.2.5/riscemu-2.2.5-py3-none-any.whl

---
+++ packages/riscemu/riscemu-2.2.5-py3-none-any.whl/riscemu/instructions/RV32M.py
@@ -0,0 +1,44 @@
+"""
+RiscEmu (c) 2021 Anton Lydike
+
+SPDX-License-Identifier: MIT
+"""
+
+from .instruction_set import *
+from riscemu.core.exceptions import INS_NOT_IMPLEMENTED
+
+
+class RV32M(InstructionSet):
+    """
+    The RV32M Instruction set, containing multiplication and division instructions
+    """
+
+    def instruction_mul(self, ins: "Instruction"):
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins)
+        self.regs.set(rd, rs1 * rs2)
+
+    def instruction_mulh(self, ins: "Instruction"):
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins)
+        self.regs.set(rd, (rs1 * rs2) >> 32)
+
+    def instruction_mulhsu(self, ins: "Instruction"):
+        INS_NOT_IMPLEMENTED(ins)
+
+    def instruction_mulhu(self, ins: "Instruction"):
+        INS_NOT_IMPLEMENTED(ins)
+
+    def instruction_div(self, ins: "Instruction"):
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins)
+        self.regs.set(rd, rs1 // rs2)
+
+    def instruction_divu(self, ins: "Instruction"):
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins, signed=False)
+        self.regs.set(rd, rs1 // rs2)
+
+    def instruction_rem(self, ins: "Instruction"):
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins)
+        self.regs.set(rd, rs1 % rs2)
+
+    def instruction_remu(self, ins: "Instruction"):
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins, signed=False)
+        self.regs.set(rd, rs1 % rs2)
