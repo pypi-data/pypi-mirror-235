b2615a53ef097c7e31ed901b2110f8fe4dc7d964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:10 +0000

    Add package DeltaLogging/1.1/DeltaLogging-1.1.tar.gz

---
+++ packages/DeltaLogging/DeltaLogging-1.1.tar.gz/DeltaLogging-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DeltaLogging',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=["pystyle"],
+    author='toxinsfx',
+    description='Simple python logging library',
+    long_description='Check README.md on github!',
+    url='https://github.com/toxinsfx/deltalogging',
+    license='Open Source',
+)
