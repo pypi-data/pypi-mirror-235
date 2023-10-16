5a04b3ef4d0c36c3cae9e9ca1aa232e37a79dc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:54:15 +0000

    Add package lilac/0.1.8/lilac-0.1.8.tar.gz

---
+++ packages/lilac/lilac-0.1.8.tar.gz/lilac-0.1.8/lilac/sources/duckdb_utils.py
@@ -0,0 +1,22 @@
+"""Utils for duckdb."""
+import os
+
+import duckdb
+
+from ..env import env, get_project_dir
+
+
+def duckdb_setup(con: duckdb.DuckDBPyConnection) -> str:
+  """Setup DuckDB. This includes setting up the extensions directory and GCS access."""
+  con.execute(f"""
+    SET extension_directory='{os.path.join(get_project_dir(), '.duckdb')}';
+  """)
+
+  if env('GCS_REGION'):
+    return f"""
+        SET s3_region='{env('GCS_REGION')}';
+        SET s3_access_key_id='{env('GCS_ACCESS_KEY')}';
+        SET s3_secret_access_key='{env('GCS_SECRET_KEY')}';
+        SET s3_endpoint='storage.googleapis.com';
+      """
+  return ''
