0df7d9e95330b21da3ba2d70d4cb5403f3ed2393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 06:05:13 +0000

    Add package mekartify/0.1.1/mekartify-0.1.1.tar.gz

---
+++ packages/mekartify/mekartify-0.1.1.tar.gz/mekartify-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mekartify",
+    version="0.1.1",
+    description="Mekartify some tool for optimize computations",
+    author="Sugeng D. Cahyo",
+    packages=[],
+    install_requires=[]
+)
