e29de60d6084c6425e92df840c63acb68947a5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:05:47 +0000

    Add package project5/0.0.1/project5-0.0.1.tar.gz

---
+++ packages/project5/project5-0.0.1.tar.gz/project5-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='project5',
+    packages=find_packages(include=['project5']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
