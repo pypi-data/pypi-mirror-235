f591ace442530207e326f56f189429bd560537fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:27 +0000

    Add package flit-gettext/0.1.0/flit_gettext-0.1.0-py3-none-any.whl

---
+++ packages/flit-gettext/flit_gettext-0.1.0-py3-none-any.whl/flit_gettext/core.py
@@ -0,0 +1,18 @@
+# pragma: no cover
+# covered in integration test using subprocess
+from flit_core import buildapi
+from flit_core.buildapi import *  # noqa
+
+from flit_gettext.utils import compile_gettext_translations
+
+
+def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
+    info = buildapi.read_flit_config(buildapi.pyproj_toml)
+    compile_gettext_translations(info)
+    return buildapi.build_wheel(wheel_directory, config_settings, metadata_directory)
+
+
+def build_editable(wheel_directory, config_settings=None, metadata_directory=None):
+    info = buildapi.read_flit_config(buildapi.pyproj_toml)
+    compile_gettext_translations(info)
+    return buildapi.build_editable(wheel_directory, config_settings, metadata_directory)
