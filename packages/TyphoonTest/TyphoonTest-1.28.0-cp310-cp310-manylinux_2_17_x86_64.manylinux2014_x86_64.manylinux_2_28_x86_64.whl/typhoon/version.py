5cbf6ae1252a5cd860f0846af62c32758a05199a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:41 +0000

    Add package TyphoonTest/1.28.0/TyphoonTest-1.28.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/TyphoonTest/TyphoonTest-1.28.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/typhoon/version.py
@@ -0,0 +1,8 @@
+
+def get_typhoontest_version():
+    """Typhoontest official version is registered in this function"""
+    return "1.28.0"
+
+
+if __name__ == "__main__":
+    print(get_typhoontest_version())
