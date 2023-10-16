b9c8336506e1cf79ceaaa511bfc28e314a10e09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 02:13:15 +0000

    Add package mcfinance/0.1.2/mcfinance-0.1.2.tar.gz

---
+++ packages/mcfinance/mcfinance-0.1.2.tar.gz/mcfinance-0.1.2/src/mcfinance/writer.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def excel_writer(daf, words, filepath, years) -> None:
+    '''Write dataframe into pandas dataframe or excel file'''
+    web, name, info = words[1], words[0], words[3]
+    if filepath != "":
+        filepath += "/"
+    if(len(daf.columns) > years+1):
+        fdf = daf.drop(daf.iloc[:, years+1:],axis = 1)
+    writer = pd.ExcelWriter(filepath+web+'_'+name+'_'+info+'.xlsx')
+    fdf.to_excel(writer)
+    writer.close()
+    print(f'{name} {info} data is written successfully to Excel File.')
+    
+def df_writer(daf, years) -> pd.DataFrame:
+    if(len(daf.columns) > years+1):
+        fdf = daf.drop(daf.iloc[:, years+1:],axis = 1)
+    return fdf
