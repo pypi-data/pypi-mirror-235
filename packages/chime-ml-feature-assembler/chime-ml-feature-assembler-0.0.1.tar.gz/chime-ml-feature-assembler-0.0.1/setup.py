6e1d5ffbeb51e79e06d52a6e82b7d75ea8aab365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:12 +0000

    Add package chime-ml-feature-assembler/0.0.1/chime-ml-feature-assembler-0.0.1.tar.gz

---
+++ packages/chime-ml-feature-assembler/chime-ml-feature-assembler-0.0.1.tar.gz/chime-ml-feature-assembler-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-ml-feature-assembler',
+    packages=find_packages(include=['chime-ml-feature-assembler']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
