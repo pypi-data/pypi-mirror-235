378c52bcd574f79cf31ec0526bfb71a7b7b5cef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:09:41 +0000

    Add package trust-track-api/0.9.4/trust-track-api-0.9.4.tar.gz

---
+++ packages/trust-track-api/trust-track-api-0.9.4.tar.gz/trust-track-api-0.9.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trust-track-api',
+    version='0.9.4',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=['requests-cache', 'pytz'],
+    url='',
+    license='',
+    author='Lyubomir Traykov',
+    author_email='lyubomir.traykov@traykovtrans.com',
+    description='TrustTrack Ruptela API Wrapper',
+)
---
+++ packages/trust-track-api/trust-track-api-0.9.4.tar.gz/trust-track-api-0.9.4/src/trust_track_api/__init__.py
@@ -0,0 +1,2 @@
+from .trust_track_api import TrustTrackAPI, TrustTrackURLs
+from .truck import Truck
