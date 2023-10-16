1d35ee5995994cb5a73ef4b43a951965ea5984b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:32 +0000

    Add package deepforge/0.0.4/deepforge-0.0.4-py3-none-any.whl

---
+++ packages/deepforge/deepforge-0.0.4-py3-none-any.whl/deepforge/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+from .cnn import CNN
+from .rnn import RNN
+from .crnn import CRNN
+from .multivariateDnn import multivariateDNN
+from .initialize import initialize
+
+# Version of deepforge library
+__version__ = "0.0.4"
