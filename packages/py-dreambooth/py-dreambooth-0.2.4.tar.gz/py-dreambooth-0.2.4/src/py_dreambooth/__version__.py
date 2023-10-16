c365d5d90aa269cb9e0a4c6f1b7f7dcd1759ca69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:37 +0000

    Add package py-dreambooth/0.2.4/py-dreambooth-0.2.4.tar.gz

---
+++ packages/py-dreambooth/py-dreambooth-0.2.4.tar.gz/py-dreambooth-0.2.4/src/py_dreambooth/__version__.py
@@ -0,0 +1,9 @@
+__title__ = "py-dreambooth"
+__description__ = (
+    "Fine-tuning the Stable Diffusion model and creating your own AI avatars"
+)
+__version__ = "0.2.4"
+__author__ = "Youngmin Kim"
+__author_email__ = "aldente0630@gmail.com"
+__license__ = "creativeml-openrail-m"
+__url__ = "https://github.com/youngmki/py-dreambooth"
