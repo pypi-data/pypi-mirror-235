eef14d65d6797b1c4e83ac60fe4e3062f0a139f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:09:37 +0000

    Add package mewbot-api/0.0.3/mewbot-api-0.0.3.tar.gz

---
+++ packages/mewbot-api/mewbot-api-0.0.3.tar.gz/mewbot-api-0.0.3/src/mewbot/api/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2021 - 2023 Mewbot Developers <mewbot@quicksilver.london>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+Container module for MewBot development APIs.
+
+This module will never contain any content, but will instead have child modules
+for different API versions and common components.
+"""
