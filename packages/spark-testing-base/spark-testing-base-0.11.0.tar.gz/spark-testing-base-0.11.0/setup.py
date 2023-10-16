ae5f8178143b7d70fcf0ba2fffecbfadcfa65f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:12:25 +0000

    Add package spark-testing-base/0.11.0/spark-testing-base-0.11.0.tar.gz

---
+++ packages/spark-testing-base/spark-testing-base-0.11.0.tar.gz/spark-testing-base-0.11.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='spark-testing-base',
+    version='0.11.0',
+    author='Holden Karau',
+    author_email='holden@pigscanfly.ca',
+    packages=['sparktestingbase', 'sparktestingbase.test'],
+    url='https://github.com/holdenk/spark-testing-base',
+    license='LICENSE.txt',
+    description='Spark testing for python',
+    long_description='',
+    install_requires=[
+        'findspark',
+        'pytest'
+    ],
+    test_requires=[
+        'nose',
+        'coverage',
+    ],
+)
