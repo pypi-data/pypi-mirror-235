addf8f4a64831e649a01d8eaa22c0ab3e087b9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:07:40 +0000

    Add package tekore/5.1.1/tekore-5.1.1-py3-none-any.whl

---
+++ packages/tekore/tekore-5.1.1-py3-none-any.whl/tekore/_model/audiobook/base.py
@@ -0,0 +1,37 @@
+from typing import List, Optional
+
+from ..base import Item
+from ..member import Copyright, Image
+from ..serialise import Model
+
+
+class Author(Model):
+    """Audiobook author."""
+
+    name: str
+
+
+class Narrator(Model):
+    """Audiobook narrator."""
+
+    name: str
+
+
+class Audiobook(Item):
+    """Audiobook base."""
+
+    authors: List[Author]
+    available_markets: Optional[List[str]] = None
+    copyrights: List[Copyright]
+    description: str
+    edition: Optional[str]
+    explicit: bool
+    external_urls: dict
+    html_description: str
+    images: List[Image]
+    languages: List[str]
+    media_type: str
+    name: str
+    narrators: List[Narrator]
+    publisher: str
+    total_chapters: Optional[int]
