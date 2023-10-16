72aaae38ac1a3825f54b28e4d11404d84a100ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:52:17 +0000

    Add package aiopath/0.7.3/aiopath-0.7.3.tar.gz

---
+++ packages/aiopath/aiopath-0.7.3.tar.gz/aiopath-0.7.3/setup.py
@@ -0,0 +1,35 @@
+__author__ = "Alex DeLorenzo"
+from setuptools import setup
+from pathlib import Path
+
+
+PKG_NAME: str = "aiopath"
+NAME: str = 'aiopath'
+VERSION: str = "0.7.3"
+LICENSE: str = "LGPL-3.0"
+
+DESC: str = "📁 Async pathlib for Python"
+
+REQUIREMENTS: list[str] = \
+  Path('requirements.txt') \
+    .read_text() \
+    .splitlines()
+
+README: str = Path('README.md').read_text()
+
+
+setup(
+  name=PKG_NAME,
+  version=VERSION,
+  description=DESC,
+  long_description=README,
+  long_description_content_type="text/markdown",
+  url='https://github.com/alexdelorenzo/aiopath',
+  author=__author__,
+  license=LICENSE,
+  packages=[NAME],
+  zip_safe=True,
+  install_requires=REQUIREMENTS,
+  python_requires='>=3.12',
+  include_package_data=True
+)
