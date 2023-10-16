434da07215d5e3fbf2d72aa8c3f4e943010fbe4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:11:49 +0000

    Add package hotfuzz-control-panel/0.1.0/hotfuzz_control_panel-0.1.0-py3-none-any.whl

---
+++ packages/hotfuzz-control-panel/hotfuzz_control_panel-0.1.0-py3-none-any.whl/hotfuzz_control_panel/__main__.py
@@ -0,0 +1,34 @@
+import os
+from hotfuzz import HotFuzz
+from appdirs import user_config_dir
+from pathlib import Path
+import re
+import subprocess
+
+config_path = Path(user_config_dir(appname="hotfuzz_control_panel")) / "commands"
+
+with open(config_path, encoding="utf-8") as f:
+    config = f.read()
+
+class UnexpectedIndentation(Exception):
+    pass
+
+commands_list = {}
+last_group = None
+for line in config.splitlines():
+    if line.strip() == "":
+        continue
+    if re.match(r"\s", line):
+        if last_group is None:
+            raise UnexpectedIndentation()
+        commands_list[last_group].append(line.lstrip())
+    else:
+        last_group = line
+        commands_list[line] = []
+
+names = list(commands_list.keys())
+hotfuzz = HotFuzz(names, initially_invisible=False)
+result = hotfuzz.run()
+if result is not None:
+    commands_list = commands_list[names[result]]
+    process = subprocess.Popen(os.linesep.join(commands_list), shell=True)
