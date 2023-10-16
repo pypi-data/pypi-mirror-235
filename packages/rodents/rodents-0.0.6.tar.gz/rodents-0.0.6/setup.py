23e8a3744f8a14b63bf9c64903a97443a87cbb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:25 +0000

    Add package rodents/0.0.6/rodents-0.0.6.tar.gz

---
+++ packages/rodents/rodents-0.0.6.tar.gz/rodents-0.0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rodents",
+    version="0.0.6",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["rodents = rodents.rodents.jiggler:run_giggler"]},
+    install_requires=open("requirements.txt", encoding="utf-8").readlines(),
+)
