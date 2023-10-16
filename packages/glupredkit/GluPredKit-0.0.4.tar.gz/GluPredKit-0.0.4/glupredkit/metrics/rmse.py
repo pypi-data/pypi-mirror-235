e3fb0e9703a50ce9a1f3b57827da94b0f811c0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:07:46 +0000

    Add package glupredkit/0.0.4/GluPredKit-0.0.4.tar.gz

---
+++ packages/glupredkit/GluPredKit-0.0.4.tar.gz/GluPredKit-0.0.4/glupredkit/metrics/rmse.py
@@ -0,0 +1,19 @@
+from .base_metric import BaseMetric
+import numpy as np
+from ..config_manager import config_manager
+
+
+class Metric(BaseMetric):
+    def __init__(self):
+        super().__init__('RMSE')
+
+    def __call__(self, y_true, y_pred):
+        y_true = np.array(y_true)
+        y_pred = np.array(y_pred)
+
+        rmse = np.sqrt(np.mean(np.square(y_true - y_pred)))
+
+        if config_manager.use_mgdl:
+            return rmse
+        else:
+            return config_manager.convert_value(rmse)
---
+++ packages/glupredkit/GluPredKit-0.0.4.tar.gz/GluPredKit-0.0.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+project_name = "GluPredKit"
+version = "0.0.4"
+author = "Miriam K. Wolff"
+author_email = "miriamkwolff@outlook.com"
+package_name = "glupredkit"  # The package name on pip install
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name=project_name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    packages=find_packages(),
+    package_dir={package_name: package_name},
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    license='MIT',
+    install_requires=[
+        "matplotlib>=3.6.3",
+        "pandas>=1.5.3",
+        "scikit-learn",
+        "xgboost",
+        "tensorflow",
+        "keras-tcn",
+        "numpy",
+        "aiohttp",
+        "click",
+        "dill"
+    ],
+    package_data={
+        'glupredkit': ['config.json'],
+    },
+    entry_points={
+        'console_scripts': [
+            'glupredkit = glupredkit.cli:cli',
+        ],
+    }
+)
+
