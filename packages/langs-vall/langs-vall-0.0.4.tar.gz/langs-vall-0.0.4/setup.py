bf53adf0527086830b1f0975536a7c62d2d33119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:07:22 +0000

    Add package langs-vall/0.0.4/langs-vall-0.0.4.tar.gz

---
+++ packages/langs-vall/langs-vall-0.0.4.tar.gz/langs-vall-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="langs-vall", 
+    version="0.0.4",
+    author="Breyston Gonzalez",
+    author_email="<breystonbarton15@gmail.com>",
+    description="Paquete de vall-e-x para proyecto de traduccion de lenguajes",
+    long_description="",
+    packages=find_packages("/vall-dependency"),
+    keywords=['python', 'vall-e-x']
+)
