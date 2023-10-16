9e0a29ee4c63076da1d63e450de6019093f2ad9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:09:05 +0000

    Add package QElephant/1.0.2/QElephant-1.0.2.tar.gz

---
+++ packages/QElephant/QElephant-1.0.2.tar.gz/QElephant-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="QElephant",
+    version="1.0.2",
+    description="",
+    author="Didictateur",
+    author_email="decosse.adrien@gmail.com",
+    packages=["QElephant"],
+    install_requires=[],
+)
