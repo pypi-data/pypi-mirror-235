9a3da982a3cf6cce352b871491c73b09edb24d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:07:53 +0000

    Add package gearpy/0.2.0/gearpy-0.2.0-py3-none-any.whl

---
+++ packages/gearpy/gearpy-0.2.0-py3-none-any.whl/gearpy/__init__.py
@@ -0,0 +1,7 @@
+from gearpy import gear
+from gearpy import mechanical_object
+from gearpy import motor
+from gearpy import solver
+from gearpy import transmission
+from gearpy import units
+from gearpy import utils
