5dde55aacbbf21131cd07aa304b43bf048d7a3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:12:37 +0000

    Add package nextline/0.5.6/nextline-0.5.6.tar.gz

---
+++ packages/nextline/nextline-0.5.6.tar.gz/nextline-0.5.6/nextline/spawned/plugin/plugins/pdb_/factory.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from logging import getLogger
+
+from apluggy import PluginManager
+
+from nextline.count import PromptNoCounter
+from nextline.spawned.exc import NotOnTraceCall
+from nextline.spawned.plugin.spec import hookimpl
+from nextline.spawned.types import TraceFunction
+
+from .custom import CustomizedPdb
+from .stream import PromptFuncType, StdInOut
+
+
+class PdbInstanceFactory:
+    '''A plugin that creates local trace functions.
+
+    Each time the first result only hook `create_local_trace_func` is called,
+    this plugin creates a new instance of the customized Pdb class and returns
+    its trace function.
+
+    The hook `create_local_trace_func` is called for each async task or thread.
+    '''
+
+    @hookimpl
+    def init(self, hook: PluginManager) -> None:
+        self._factory = Factory(hook=hook)
+
+    @hookimpl
+    def create_local_trace_func(self) -> TraceFunction:
+        return self._factory()
+
+
+def Factory(hook: PluginManager):
+    cmdloop_hook = CmdloopHook(hook=hook)
+    prompt_func = PromptFunc(hook=hook)
+
+    def _factory():
+        stdio = StdInOut(prompt_func=prompt_func)
+        pdb = CustomizedPdb(
+            cmdloop_hook=cmdloop_hook,
+            stdin=stdio,
+            stdout=stdio,
+        )
+        stdio.prompt_end = pdb.prompt
+        return pdb.trace_dispatch
+
+    return _factory
+
+
+def CmdloopHook(hook: PluginManager):
+    '''Return a context manager in which Pdb.cmdloop() is called.'''
+
+    def cmdloop():
+        if not hook.hook.is_on_trace_call():
+            raise NotOnTraceCall
+        return hook.with_.on_cmdloop()
+
+    return cmdloop
+
+
+def PromptFunc(hook: PluginManager) -> PromptFuncType:
+    '''Return a function that responds to Pdb prompts with user commands.'''
+
+    counter = PromptNoCounter(1)
+    logger = getLogger(__name__)
+
+    def _prompt_func(text: str) -> str:
+        prompt_no = counter()
+        logger.debug(f'PromptNo: {prompt_no}')
+        with (context := hook.with_.on_prompt(prompt_no=prompt_no, text=text)):
+            command = hook.hook.prompt(prompt_no=prompt_no, text=text)
+            if command is None:
+                logger.warning(f'command is None: {command!r}')
+            context.gen.send(command)
+        return command
+
+    return _prompt_func
---
+++ packages/nextline/nextline-0.5.6.tar.gz/nextline-0.5.6/nextline/utils/func.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+import asyncio
+import functools
+import threading
+from typing import (
+    TYPE_CHECKING,
+    AsyncGenerator,
+    AsyncIterator,
+    Iterable,
+    Set,
+    Tuple,
+    TypeVar,
+)
+
+if TYPE_CHECKING:
+    from asyncio import Future, Task
+    from threading import Thread
+
+
+def current_task_or_thread() -> Task | Thread:
+    """The asyncio task object if in a task, else the thread object"""
+    try:
+        task = asyncio.current_task()
+    except RuntimeError:
+        task = None
+    return task or threading.current_thread()
+
+
+try:
+    from asyncio import to_thread  # type: ignore
+except ImportError:
+    # for Python 3.8
+    # to_thread() is new in Python 3.9
+
+    async def to_thread(func, /, *args, **kwargs):  # type: ignore
+        loop = asyncio.get_running_loop()
+        func_call = functools.partial(func, *args, **kwargs)
+        return await loop.run_in_executor(None, func_call)
+
+
+T = TypeVar("T")
+
+
+async def agen_with_wait(
+    agen: AsyncIterator[T],
+) -> AsyncGenerator[
+    T | Tuple[Tuple[Future, ...], Tuple[Future, ...]], Iterable[Future]
+]:
+    """Yield from the agen while waiting for received tasks
+
+    Used to raise an exception from tasks
+    """
+    done: Set[Future] = set()
+    pending: Set[Future] = set()
+    anext = asyncio.ensure_future(agen.__anext__())
+    while True:
+        done_, pending_ = await asyncio.wait(
+            pending | {anext},
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        for t in done_ - {anext}:
+            if exc := t.exception():
+                anext.cancel()
+                raise exc
+        if anext in done_:
+            try:
+                item = anext.result()
+            except StopAsyncIteration:
+                break
+            done_.remove(anext)
+            done |= done_
+        else:
+            done |= done_
+            continue
+        pending &= pending_
+        new = yield item
+        if new is not None:
+            pending |= set(new)
+            yield tuple(done), tuple(pending)
+            done.clear()
+        anext = asyncio.ensure_future(agen.__anext__())
+
+
+async def merge_aiters(
+    *aiters: AsyncIterator[T],
+) -> AsyncIterator[Tuple[int, T]]:
+    aiter_map = {a: i for i, a in enumerate(aiters)}
+    task_map = {asyncio.ensure_future(a.__anext__()): a for a in aiter_map.keys()}
+    tasks = set(task_map.keys())
+    while tasks:
+        done, pending = await asyncio.wait(
+            tasks,
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        tasks = pending
+        while done:
+            task = done.pop()
+            try:
+                item = task.result()
+            except StopAsyncIteration:
+                continue
+            aiter = task_map[task]
+            yield aiter_map[aiter], item
+            task = asyncio.ensure_future(aiter.__anext__())
+            tasks.add(task)
+            task_map[task] = aiter
