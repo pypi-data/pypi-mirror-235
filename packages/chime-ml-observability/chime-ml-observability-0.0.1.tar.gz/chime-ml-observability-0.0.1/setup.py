e999bf312d7d43207b32cd1e5a82e1229c4e36da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:32 +0000

    Add package chime-ml-observability/0.0.1/chime-ml-observability-0.0.1.tar.gz

---
+++ packages/chime-ml-observability/chime-ml-observability-0.0.1.tar.gz/chime-ml-observability-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-ml-observability',
+    packages=find_packages(include=['chime-ml-observability']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
