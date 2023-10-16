7b83f75a4f3ff615571d7a0939ecc251c07c2177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 18:06:31 +0000

    Add package tnnt-templates/2.12.0/tnnt_templates-2.12.0-py3-none-any.whl

---
+++ packages/tnnt-templates/tnnt_templates-2.12.0-py3-none-any.whl/tnnt_templates/__init__.py
@@ -0,0 +1,11 @@
+"""
+TN-NT Templates init
+"""
+
+# Standard Library
+from importlib import metadata
+
+__version__ = metadata.version("tnnt-templates")
+__title__ = "Terra Nanotech Alliance Auth Template Overrides"
+
+del metadata
