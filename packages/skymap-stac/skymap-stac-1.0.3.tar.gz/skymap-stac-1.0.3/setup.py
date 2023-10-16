c8b4968b3fd844d45b1c3428353f83d8c2671b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:08:34 +0000

    Add package skymap-stac/1.0.3/skymap-stac-1.0.3.tar.gz

---
+++ packages/skymap-stac/skymap-stac-1.0.3.tar.gz/skymap-stac-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.3' 
+DESCRIPTION = 'Skymap STAC package'
+LONG_DESCRIPTION = 'Python package for reading datacube from rolodex'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="skymap-stac", 
+        version=VERSION,
+        author="Hai Anh Do",
+        author_email="haianhdo193@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['python-dotenv'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'stac', 'skymap', 'rolodex'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: Unix",
+        ]
+)
