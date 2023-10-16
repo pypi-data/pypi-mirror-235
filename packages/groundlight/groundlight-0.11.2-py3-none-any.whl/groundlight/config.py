539764cfc867e3e8664dc35e83512bad10262bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:32 +0000

    Add package groundlight/0.11.2/groundlight-0.11.2-py3-none-any.whl

---
+++ packages/groundlight/groundlight-0.11.2-py3-none-any.whl/groundlight/config.py
@@ -0,0 +1,10 @@
+API_TOKEN_WEB_URL = "https://app.groundlight.ai/reef/my-account/api-tokens"
+API_TOKEN_VARIABLE_NAME = "GROUNDLIGHT_API_TOKEN"
+
+DEFAULT_ENDPOINT = "https://api.groundlight.ai/"
+
+__all__ = [
+    "API_TOKEN_WEB_URL",
+    "API_TOKEN_VARIABLE_NAME",
+    "DEFAULT_ENDPOINT",
+]
