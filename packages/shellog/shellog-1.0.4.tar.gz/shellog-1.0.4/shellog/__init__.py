76163ccdfaa2d042b612740204011be16a587edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:43:30 +0000

    Add package shellog/1.0.4/shellog-1.0.4.tar.gz

---
+++ packages/shellog/shellog-1.0.4.tar.gz/shellog-1.0.4/shellog/__init__.py
@@ -0,0 +1,40 @@
+"""
+shellog.
+
+A Python package to get notifications about the logs of a process.
+"""
+
+__version__ = "1.0.2"
+__author__ = 'Daniele Margiotta'
+__credits__ = 'Reveal s.r.l.'
+
+import telebot
+
+class Bot:
+    def __init__(self):
+        self.bot = telebot.TeleBot('6300373442:AAEeMHpIq_ttEGdmQzE04706UR0rJISCSHM')
+        self.chat_id = []
+
+    def sendMessage(self, text: str):
+        for id in self.chat_id:
+            self.bot.send_message(id, text)
+            
+    def addChatId(self, id: str):
+        self.chat_id.append(id)
+    
+    def addListChatIds(self, ids):
+        for id in ids:
+            self.chat_id.append(id)
+    
+    def removeChatId(self, id:str):
+        self.chat_id.remove(id)
+
+    def removeListChatIds(self, ids):
+        for id in ids:
+            self.chat_id.remove(id)
+    
+    def clearChatId(self):
+        self.chat_id.clear()
+
+bot = Bot()
+
