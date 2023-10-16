87b06253cb0b1bcb40f2ed6ee2254b8f28abda65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:06:56 +0000

    Add package pmvcs/1.0.4/pmvcs-1.0.4-py3-none-any.whl

---
+++ packages/pmvcs/pmvcs-1.0.4-py3-none-any.whl/pmvcs/__version__.py
@@ -0,0 +1,3 @@
+""" __version__ file for Python MVC Shell Framework Package """
+VERSION = (1, 0, 4)
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/pmvcs/pmvcs-1.0.4-py3-none-any.whl/pmvcs/setup/example/models/example_one.py
@@ -0,0 +1,6 @@
+""" Example One Model file for PMVCS APP """
+from app.models.base_model import BaseModel
+
+
+class ExampleOneModel(BaseModel):
+    """ Class for Example One Model """
