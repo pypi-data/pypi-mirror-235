a88202136cc81baf3115ad8901e3b8e2c3ba76de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:51 +0000

    Add package forgejo-api/1.20.1.post0/forgejo_api-1.20.1.post0-py3-none-any.whl

---
+++ packages/forgejo-api/forgejo_api-1.20.1.post0-py3-none-any.whl/forgejo_api/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from forgejo_api.api.activitypub_api import ActivitypubApi
+from forgejo_api.api.admin_api import AdminApi
+from forgejo_api.api.issue_api import IssueApi
+from forgejo_api.api.miscellaneous_api import MiscellaneousApi
+from forgejo_api.api.notification_api import NotificationApi
+from forgejo_api.api.organization_api import OrganizationApi
+from forgejo_api.api.package_api import PackageApi
+from forgejo_api.api.repository_api import RepositoryApi
+from forgejo_api.api.settings_api import SettingsApi
+from forgejo_api.api.user_api import UserApi
