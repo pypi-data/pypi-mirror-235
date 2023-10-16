67f8f7b238a196e886739e724b82bdd574383abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:57 +0000

    Add package onnx-extended/0.2.3/onnx_extended-0.2.3-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/onnx-extended/onnx_extended-0.2.3-cp310-cp310-macosx_10_15_x86_64.whl/onnx_extended/_config.py
@@ -0,0 +1,5 @@
+HAS_CUDA = 0
+CXX_FLAGS = ' -Wall -Wno-unknown-pragmas -Wextra -mavx2 -mf16c -fPIC -Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include'
+CMAKE_CXX_STANDARD_REQUIRED = ''
+CMAKE_CXX_EXTENSIONS = ''
+CMAKE_CXX_STANDARD = 17
