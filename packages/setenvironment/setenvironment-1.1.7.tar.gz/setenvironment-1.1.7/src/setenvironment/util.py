157daf1a90c1916691be81f2f73e18a24e5d7fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:53:27 +0000

    Add package setenvironment/1.1.7/setenvironment-1.1.7.tar.gz

---
+++ packages/setenvironment/setenvironment-1.1.7.tar.gz/setenvironment-1.1.7/src/setenvironment/util.py
@@ -0,0 +1,39 @@
+"""
+Utility functions for setenvironment.
+"""
+
+import os
+
+
+def read_utf8(path: str) -> str:
+    """Reads a file as utf-8."""
+    with open(path, encoding="utf-8", mode="r") as file:
+        return file.read()
+
+
+def write_utf8(path: str, text: str) -> None:
+    """Writes a file as utf-8."""
+    with open(path, encoding="utf-8", mode="w") as file:
+        file.write(text)
+
+
+def parse_paths(path_str: str) -> list[str]:
+    """Parses a path string into a list of paths."""
+    path_str = path_str.strip()
+    if not path_str:
+        return []
+    return path_str.split(os.path.pathsep)
+
+
+def remove_adjascent_duplicates(path_list: list[str]) -> list[str]:
+    """Removes adjascent duplicates."""
+    out = []
+    for i, path in enumerate(path_list):
+        if i < len(path_list) - 1:
+            a = path_list[i + 1]
+            b = path_list[i]
+            if a != b:
+                out.append(path)
+        else:
+            out.append(path)
+    return out
