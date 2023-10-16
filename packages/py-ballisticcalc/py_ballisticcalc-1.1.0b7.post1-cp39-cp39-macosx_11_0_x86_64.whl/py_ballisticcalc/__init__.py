4a23ca87c8275df04bb960f02791cb1158352d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 03:04:51 +0000

    Add package py-ballisticcalc/1.1.0b7.post1/py_ballisticcalc-1.1.0b7.post1-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/py-ballisticcalc/py_ballisticcalc-1.1.0b7.post1-cp39-cp39-macosx_11_0_x86_64.whl/py_ballisticcalc/__init__.py
@@ -0,0 +1,18 @@
+"""LGPL library for small arms ballistic calculations (Python 3.8+)"""
+
+__author__ = "o-murphy"
+__copyright__ = ("",)
+
+__credits__ = ["o-murphy"]
+__version__ = "1.1.0b7-post1"
+
+from .drag_model import *  # pylint: disable=import-error
+from .drag_tables import *
+from .settings import *
+from .multiple_bc import *
+from .interface import *
+from .trajectory_data import *
+from .trajectory_calc import *  # pylint: disable=import-error
+from .conditions import *
+from .munition import *
+from .unit import *
