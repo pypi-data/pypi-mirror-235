00d876c650257ad7f430cb7d97003319f9be896f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:09:11 +0000

    Add package rext/0.1.24/rext-0.1.24-py2.py3-none-any.whl

---
+++ packages/rext/rext-0.1.24-py2.py3-none-any.whl/rexy.py
@@ -0,0 +1,13 @@
+__version__ = '0.1.1'
+
+class str_manage():
+    def remove_space(string):
+        return string.replace(" ","")
+
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+    main()
