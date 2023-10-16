278b80c0bc484ce362277db0c104edf135d02a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:08:40 +0000

    Add package slyp/0.0.3/slyp-0.0.3-py3-none-any.whl

---
+++ packages/slyp/slyp-0.0.3-py3-none-any.whl/slyp/checkers/abstract/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import ast
+
+from .matching_branches import FindEquivalentBranchesVisitor
+
+_VISITORS = [FindEquivalentBranchesVisitor()]
+
+
+def run_ast_checkers(filename: str, content: bytes) -> set[tuple[int, str]]:
+    try:
+        tree = ast.parse(content, filename=filename)
+    except SyntaxError:
+        return {(0, "X001")}
+
+    for visitor in _VISITORS:
+        visitor.filename = filename
+        visitor.visit(tree)
+    return {
+        (lineno, code)
+        for (lineno, error_filename, code) in visitor.errors
+        if error_filename == filename
+        for visitor in _VISITORS
+    }
+
+
+def _clear_visitor_errors() -> None:
+    for visitor in _VISITORS:
+        visitor.errors = set()
