b3cb6d9414a13f9a3037a5a35e22689b06dc41bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:08:46 +0000

    Add package drbot-reddit/2.0.1/drbot-reddit-2.0.1.tar.gz

---
+++ packages/drbot-reddit/drbot-reddit-2.0.1.tar.gz/drbot-reddit-2.0.1/src/drbot/botlings/__init__.py
@@ -0,0 +1,10 @@
+from ..Botling import Botling
+from .ModmailLinker import ModmailLinker
+from .AdminWatcher import AdminWatcher
+from .SelfModerationWatcher import SelfModerationWatcher
+from .Pointling import Pointling
+from .UserFlairGuard import UserFlairGuard
+from .SidebarSyncer import SidebarSyncer
+from .WeekdayFlairEnforcer import WeekdayFlairEnforcer
+
+__all__ = ("Botling", "ModmailLinker", "AdminWatcher", "SelfModerationWatcher", "Pointling", "UserFlairGuard", "SidebarSyncer", "WeekdayFlairEnforcer")
