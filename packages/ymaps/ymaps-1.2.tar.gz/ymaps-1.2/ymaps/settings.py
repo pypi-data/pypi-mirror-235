d9c0b27ea59f8d719413f031939594984432a96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:08:32 +0000

    Add package ymaps/1.2/ymaps-1.2.tar.gz

---
+++ packages/ymaps/ymaps-1.2.tar.gz/ymaps-1.2/ymaps/settings.py
@@ -0,0 +1,12 @@
+"""
+Default Settings for ymaps
+"""
+
+from typing import Dict
+
+
+class DefaultSettings:
+    timeout = 1
+    language = "ru_RU"
+    suggest_language = "ru"
+    client_settings: Dict = {}
