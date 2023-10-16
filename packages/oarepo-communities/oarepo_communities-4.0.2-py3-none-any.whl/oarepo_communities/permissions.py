8d97747f582279115b6599aaa8e26c4d4f0a2537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 05:05:52 +0000

    Add package oarepo-communities/4.0.2/oarepo_communities-4.0.2-py3-none-any.whl

---
+++ packages/oarepo-communities/oarepo_communities-4.0.2-py3-none-any.whl/oarepo_communities/permissions.py
@@ -0,0 +1,11 @@
+from invenio_access.permissions import system_identity
+from invenio_communities import current_communities
+from cachetools import cached, TTLCache
+
+@cached(cache=TTLCache(maxsize=1028, ttl=600))
+def permissions_cache(community_id):
+    return current_communities.service.read(system_identity, community_id).data["custom_fields"]["permissions"]
+
+def usermap(community_id):
+    return current_communities.service.read(system_identity, community_id).data["custom_fields"]["usermap"]
+
