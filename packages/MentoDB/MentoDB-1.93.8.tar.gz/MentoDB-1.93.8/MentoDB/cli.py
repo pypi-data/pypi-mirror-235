66031acfe82514804edd35ce6a4a16e7f836709a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:13 +0000

    Add package MentoDB/1.93.8/MentoDB-1.93.8.tar.gz

---
+++ packages/MentoDB/MentoDB-1.93.8.tar.gz/MentoDB-1.93.8/MentoDB/cli.py
@@ -0,0 +1,2 @@
+def get_cli():
+    print(open("README.md", encoding="utf-8").read())
---
+++ packages/MentoDB/MentoDB-1.93.8.tar.gz/MentoDB-1.93.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name         = "MentoDB",
+    version      = "1.93.8",
+    url          = "https://github.com/fswair/MentoDB",
+    description  = "Sqlite3 based powerful database project.",
+    keywords     = ["MentoDB", "SQL", "ORM"],
+
+    author       = "Mert Sırakaya",
+    author_email = "usirakaya@ogr.iu.edu.tr",
+
+    license      = "GPLv3+",
+    classifiers  = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3"
+    ],
+
+    # ? Paket Bilgileri
+    packages         = find_packages(),
+    python_requires  = ">=3.10",
+    install_requires = open("requirements.txt", encoding="utf-8").read().splitlines(),
+    entry_points = {
+        "console_scripts": [
+            "mento = MentoDB.cli:get_cli",
+        ]
+    },
+
+    long_description_content_type = "text/markdown",
+    long_description              = open("README.md", encoding="utf-8").read(),
+    include_package_data          = True
+)
