ff2bc10bdd40a77ab5a9f6077686329d9202e07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 02:13:07 +0000

    Add package sage-func/0.1.2/sage-func-0.1.2.tar.gz

---
+++ packages/sage-func/sage-func-0.1.2.tar.gz/sage-func-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sage-func",
+    version="0.1.2",
+    url="https://github.com/zhongweili/SageAI",
+    author="Zhongwei",
+    author_email="lizhongwei.nkcs@gmail.com",
+    description="Description of my package",
+    packages=find_packages(),
+    install_requires=[],
+)
