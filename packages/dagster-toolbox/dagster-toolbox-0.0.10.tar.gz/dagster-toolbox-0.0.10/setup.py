b4a916455ce241e31956797c7b7ec5a4e0deb08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:40 +0000

    Add package dagster-toolbox/0.0.10/dagster-toolbox-0.0.10.tar.gz

---
+++ packages/dagster-toolbox/dagster-toolbox-0.0.10.tar.gz/dagster-toolbox-0.0.10/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dagster-toolbox",
+    version="0.0.10",
+    packages=find_packages(),
+    author="Nicolas RAMY",
+    author_email="nicolas.ramy@darkelda.com",
+    license="MIT",
+    description="A set of tools to ease Dagster usage",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "dagster==1.1.5",
+        "dagster-aws==0.17.5",
+        "dagster-pandas==0.17.5",
+        "dagster-pandera==0.17.5",
+        "dagster-postgres==0.17.5",
+        "google-api-python-client==2.68.0",
+        "hvac==1.0.2",
+        "minio==7.1.12",
+        "oauth2client==4.1.3",
+        "pandas==1.5.2",
+        "python-slugify==7.0.0",
+        "requests==2.31.0",
+        "SQLAlchemy==1.4.46",
+    ],
+    url="https://github.com/nicolasramy/dagster-toolbox",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Terminals",
+        "Topic :: Utilities",
+    ],
+)
