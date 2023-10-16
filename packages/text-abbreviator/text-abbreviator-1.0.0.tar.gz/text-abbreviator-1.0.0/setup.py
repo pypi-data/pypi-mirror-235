4b5257e01d2a9ce2e02a5655bdfe1539f5f19cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:06:48 +0000

    Add package text-abbreviator/1.0.0/text-abbreviator-1.0.0.tar.gz

---
+++ packages/text-abbreviator/text-abbreviator-1.0.0.tar.gz/text-abbreviator-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='text-abbreviator',
+    version='1.0.0',
+    description='Text String processing library',
+    author='loeaf',
+    author_email='break8524@gamil.com',
+    url='https://github.com/loeaf/text-abbreviate',
+    packages=find_packages(),
+    install_requires=[],
+)
