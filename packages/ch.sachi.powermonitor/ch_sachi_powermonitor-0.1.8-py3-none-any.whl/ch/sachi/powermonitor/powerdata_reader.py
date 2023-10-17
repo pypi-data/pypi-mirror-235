39b9037d9ea650f75362b2c9d8ebdaa7b3528467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:06:37 +0000

    Add package ch.sachi.powermonitor/0.1.8/ch_sachi_powermonitor-0.1.8-py3-none-any.whl

---
+++ packages/ch.sachi.powermonitor/ch_sachi_powermonitor-0.1.8-py3-none-any.whl/ch/sachi/powermonitor/powerdata_reader.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+from asyncio import Event
+
+from victron_ble.devices import BatteryMonitorData, DeviceData
+
+from ch.sachi.victron_ble.powermonitor_scanner import PowermonitorScanner
+
+logger = logging.getLogger(__name__)
+
+
+class PowerdataReader:
+    result: BatteryMonitorData = None
+    result_written: Event = Event()
+
+    def __init__(self, id: str, key: str):
+        self.id = id
+        self.key = key
+
+    async def read(self) -> BatteryMonitorData:
+        keys = {self.id: self.key}
+        scanner = self.create_scanner(keys)
+        logger.debug('Scanner created')
+        await scanner.start()
+        self.result_written = Event(loop=asyncio.get_running_loop())
+
+        await self.result_written.wait()
+        return self.result
+
+    def create_scanner(self, keys):
+        return PowermonitorScanner(self.cb, keys)
+
+    def cb(self, device_data: DeviceData):
+        if isinstance(device_data, BatteryMonitorData):
+            self.result = device_data
+            logger.debug('cb called on %s', asyncio.get_running_loop())
+            self.result_written.set()
