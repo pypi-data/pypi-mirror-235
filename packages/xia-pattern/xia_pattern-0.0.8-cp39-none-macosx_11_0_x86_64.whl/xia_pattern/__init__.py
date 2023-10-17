c65ba18e82c74725424318be488dce5bb8e2c75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:07:41 +0000

    Add package xia-pattern/0.0.8/xia_pattern-0.0.8-cp39-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-pattern/xia_pattern-0.0.8-cp39-none-macosx_11_0_x86_64.whl/xia_pattern/__init__.py
@@ -0,0 +1,9 @@
+from xia_pattern.pattern import PatternTemplate
+from xia_pattern.pattern import PythonPattern
+
+__all__ = [
+    "PatternTemplate",
+    "PythonPattern"
+]
+
+__version__ = "0.0.8"
