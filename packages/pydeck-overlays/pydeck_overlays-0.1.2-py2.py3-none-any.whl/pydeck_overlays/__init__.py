6a709984012859b182eded3d8076963b75f63295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:07:53 +0000

    Add package pydeck-overlays/0.1.2/pydeck_overlays-0.1.2-py2.py3-none-any.whl

---
+++ packages/pydeck-overlays/pydeck_overlays-0.1.2-py2.py3-none-any.whl/pydeck_overlays/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pydeck-overlays."""
+
+__author__ = """Oceanum Developers"""
+__email__ = "developers@oceanum.science"
+__version__ = "0.1.2"
+
+from .labels import LabelLayer
