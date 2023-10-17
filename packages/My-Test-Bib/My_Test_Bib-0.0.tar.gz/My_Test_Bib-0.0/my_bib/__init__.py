e7ef1b90bd1d2722eb1fd6b7819b0776a760b0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:08:42 +0000

    Add package My-Test-Bib/0.0/My_Test_Bib-0.0.tar.gz

---
+++ packages/My-Test-Bib/My_Test_Bib-0.0.tar.gz/My_Test_Bib-0.0/my_bib/__init__.py
@@ -0,0 +1,3 @@
+from .dir import 
+
+print()
---
+++ packages/My-Test-Bib/My_Test_Bib-0.0.tar.gz/My_Test_Bib-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='My_Test_Bib',
+      version='0.0',
+      description='test',
+      packages=['my_bib'],
+      author_email='rskafh@gmail.com',
+      zip_safe=False)
