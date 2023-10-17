9384a476b09253e5ab44cae3196b1b51e3339567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:10:32 +0000

    Add package check-print/0.1.0/check_print-0.1.0-py3-none-any.whl

---
+++ packages/check-print/check_print-0.1.0-py3-none-any.whl/check_print/main.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import argparse
+import re
+from typing import Sequence
+
+RE_PATTERN = re.compile(b'\sprint\s*\(')
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to check')
+    args = parser.parse_args(argv)
+
+    retcode = 0
+    for filename in args.filenames:
+        with open(filename, 'rb') as inputfile:
+            for i, line in enumerate(inputfile, start=1):
+                if RE_PATTERN.search(line):
+                    print(
+                        f'{filename}:{i}: print found',
+                    )
+                    retcode = 1
+
+    return retcode
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
