61f6bc0fe379e6bed0d1a9d3068655bdf2df52e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:08:11 +0000

    Add package midjpy/1.0.3/midjpy-1.0.3.tar.gz

---
+++ packages/midjpy/midjpy-1.0.3.tar.gz/midjpy-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+# Read the contents of your README.md file as the long description
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='midjpy',
+    version='1.0.3',
+    description='Python client for the MIDJ API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify the content type
+    author='dmaxxy',
+    author_email='dmaxxy.bhw@gmail.com',
+    packages=['midjpy'],
+    install_requires=['requests'],
+)
