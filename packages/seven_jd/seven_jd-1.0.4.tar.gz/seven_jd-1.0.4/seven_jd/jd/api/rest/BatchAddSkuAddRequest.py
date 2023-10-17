33ebd10e07c8d281cefc515e30a30f0331d5bab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:11:55 +0000

    Add package seven_jd/1.0.4/seven_jd-1.0.4.tar.gz

---
+++ packages/seven_jd/seven_jd-1.0.4.tar.gz/seven_jd-1.0.4/seven_jd/jd/api/rest/BatchAddSkuAddRequest.py
@@ -0,0 +1,19 @@
+from jd.api.base import RestApi
+
+class BatchAddSkuAddRequest(RestApi):
+		def __init__(self,domain='gw.api.360buy.com',port=80):
+			"""
+			"""
+			RestApi.__init__(self,domain, port)
+			self.data = None
+			self.merchantCode = None
+
+		def getapiname(self):
+			return 'jingdong.batchAddSku.add'
+
+			
+
+
+
+
+
