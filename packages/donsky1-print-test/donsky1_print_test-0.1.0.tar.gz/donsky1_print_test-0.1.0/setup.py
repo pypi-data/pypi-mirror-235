7ff55d361f878a678ebbe69971ad4197190bbe63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:09:17 +0000

    Add package donsky1-print-test/0.1.0/donsky1_print_test-0.1.0.tar.gz

---
+++ packages/donsky1-print-test/donsky1_print_test-0.1.0.tar.gz/donsky1_print_test-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='donsky1_print_test',
+    version='0.1.0',
+    description='Your package description test',
+    author='Donsky',
+    author_email='donsky1@example.com',
+    packages=['donsky1_print'],
+    install_requires=[
+        # Your dependencies here
+    ],
+)
