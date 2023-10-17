09de8f900737184ce15ad92deb7b77f175b5b170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:15:11 +0000

    Add package json-cpp/1.0.92/json-cpp-1.0.92.tar.gz

---
+++ packages/json-cpp/json-cpp-1.0.92.tar.gz/json-cpp-1.0.92/json_cpp/__init__.py
@@ -0,0 +1,3 @@
+from .json_object import JsonObject, JsonList, JsonDate, JsonString
+from .decorators import json_force_parameter_type, json_parameters_function, json_parameters_method
+from .search import SortOrder, SearchType, NotFoundBehavior
---
+++ packages/json-cpp/json-cpp-1.0.92.tar.gz/json-cpp-1.0.92/json_cpp/decorators.py
@@ -0,0 +1,130 @@
+from .json_object import JsonObject, JsonList
+
+
+def _use_self(funct):
+    from inspect import signature, _empty
+    s = signature(funct)
+    return "self" in s.parameters and s.parameters["self"].annotation is _empty
+
+
+def json_force_parameter_type_function(funct):
+    def decorated(*args, **kwargs):
+        from inspect import signature
+        from json_cpp.json_object import JsonObject
+        if args:
+            args = list(args)
+        parameters = signature(funct).parameters
+        for pi, p in enumerate(parameters):
+            if issubclass(parameters[p].annotation, JsonObject) and parameters[p].annotation is not JsonObject:
+                if p in kwargs and isinstance(kwargs[p], JsonObject):
+                    kwargs[p] = kwargs[p].into(parameters[p].annotation)
+                if pi < len(args):
+                    args.insert(pi, args.pop(pi).into(parameters[p].annotation))
+            elif issubclass(parameters[p].annotation, JsonList) and parameters[p].annotation is not JsonList:
+                if p in kwargs and isinstance(kwargs[p], JsonList):
+                    kwargs[p] = kwargs[p].into(parameters[p].annotation)
+                if pi < len(args):
+                    args.insert(pi, args.pop(pi).into(parameters[p].annotation))
+            elif p in kwargs and type(kwargs[p]) is str and parameters[p].annotation is not str:
+                kwargs[p] = parameters[p].annotation(kwargs[p])
+                if pi < len(args):
+                    args.insert(pi, args.pop(pi).into(parameters[p].annotation))
+        return funct(*args, **kwargs)
+    return decorated
+
+
+def json_force_parameter_type_method(funct):
+    def decorated(self, *args, **kwargs):
+        from inspect import signature
+        from json_cpp.json_object import JsonObject
+        if args:
+            args = list(args)
+        parameters = signature(funct).parameters
+        for pi, p in enumerate(parameters):
+            if issubclass(parameters[p].annotation, JsonObject) and parameters[p].annotation is not JsonObject:
+                if p in kwargs and isinstance(kwargs[p], JsonObject):
+                    kwargs[p] = kwargs[p].into(parameters[p].annotation)
+                if pi < len(args):
+                    args.insert(pi, args.pop(pi).into(parameters[p].annotation))
+            elif issubclass(parameters[p].annotation, JsonList) and parameters[p].annotation is not JsonList:
+                if p in kwargs and isinstance(kwargs[p], JsonList):
+                    kwargs[p] = kwargs[p].into(parameters[p].annotation)
+                if pi < len(args):
+                    args.insert(pi, args.pop(pi).into(parameters[p].annotation))
+            elif p in kwargs and type(kwargs[p]) is str and parameters[p].annotation is not str:
+                kwargs[p] = parameters[p].annotation(kwargs[p])
+                if pi < len(args):
+                    args.insert(pi, args.pop(pi).into(parameters[p].annotation))
+        return funct(self, *args, **kwargs)
+    return decorated
+
+
+def json_force_parameter_type(funct):
+    if _use_self(funct):
+        return json_force_parameter_type_method(funct)
+    else:
+        return json_force_parameter_type_function(funct)
+
+
+def json_parameters_function(funct):
+    def decorated(json_object):
+        if not isinstance(json_object, JsonObject):
+            raise TypeError("Parameter must be JsonObject instance")
+        p = json_object.to_dict()
+        return funct(**p)
+    return decorated
+
+
+def json_parameters_method(funct):
+    def decorated(self, json_object):
+        if not isinstance(json_object, JsonObject):
+            raise TypeError("Parameter must be JsonObject instance")
+        p = json_object.to_dict()
+        return funct(self, **p)
+    return decorated
+
+
+def json_parameters(funct):
+    if _use_self(funct):
+        return json_parameters_method(funct)
+    else:
+        return json_parameters_function(funct)
+
+
+def json_parse(funct):
+    if _use_self(funct):
+        def decorated(self, json_string: str):
+            return funct(JsonObject.load())
+
+        return decorated
+
+
+def json_parse(json_object_type=None):
+    def decorator(funct):
+        if _use_self(funct):
+            if json_object_type:
+                def decorated(self, json_string: str):
+                    return funct(self, json_object_type.parse(json_string))
+            else:
+                def decorated(self, json_string: str):
+                    return funct(self, JsonObject.load(json_string))
+        else:
+            if json_object_type:
+                def decorated(json_string: str):
+                    return funct(json_object_type.parse(json_string))
+            else:
+                def decorated(json_string: str):
+                    return funct(JsonObject.load(json_string))
+        return decorated
+    if callable(json_object_type):
+        funct = json_object_type
+        json_object_type = None
+        return decorator(funct)
+    return decorator
+
+
+class classorinstancemethod(classmethod):
+
+    def __get__(self, instance, type_):
+        descr_get = super().__get__ if instance is None else self.__func__.__get__
+        return descr_get(instance, type_)
