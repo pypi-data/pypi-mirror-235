3136d50355c82056c91e11910750e71828f26136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:12:36 +0000

    Add package scikit-diveMove/0.3.2/scikit-diveMove-0.3.2.tar.gz

---
+++ packages/scikit-diveMove/scikit-diveMove-0.3.2.tar.gz/scikit-diveMove-0.3.2/skdiveMove/__init__.py
@@ -0,0 +1,56 @@
+"""Top-level class for interacting with diveMove
+
+The :class:`TDR` class aims to be a comprehensive class to encapsulate the
+processing of `TDR` records from a data file.
+
+This module instantiates an `R` session to interact with low-level
+functions and methods of package `diveMove`.
+
+Calibration and phase detection
+-------------------------------
+
+.. autosummary::
+
+   TDR
+   TDR.zoc
+   TDR.detect_wet
+   TDR.detect_dives
+   TDR.detect_dive_phases
+   TDR.calibrate_speed
+
+Analyses
+--------
+
+.. autosummary::
+
+   TDR.dive_stats
+   TDR.time_budget
+   TDR.stamp_dives
+
+Plotting
+--------
+
+.. autosummary::
+
+   TDR.plot
+   TDR.plot_zoc
+   TDR.plot_phases
+   TDR.plot_dive_model
+
+Functions
+---------
+
+.. autosummary::
+
+   calibrate
+   dump_config_template
+
+"""
+
+from skdiveMove.tdr import TDR, calibrate
+from skdiveMove.calibconfig import dump_config_template
+
+__author__ = "Sebastian Luque <spluque@gmail.com>"
+__license__ = "AGPLv3"
+__version__ = "0.3.2"
+__all__ = ["TDR", "calibrate", "dump_config_template"]
