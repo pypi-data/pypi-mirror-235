c989fd298dc7dbfdd4430c855f438d58eedba552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:08:51 +0000

    Add package disvia/0.0.2/disvia-0.0.2.tar.gz

---
+++ packages/disvia/disvia-0.0.2.tar.gz/disvia-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Retrieving data from discord via token'
+LONG_DESCRIPTION = 'A package that allows to retrieve information about discord via token'
+
+# Setting up
+setup(
+    name="disvia",
+    version=VERSION,
+    author="MrWhxte",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'discord'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
