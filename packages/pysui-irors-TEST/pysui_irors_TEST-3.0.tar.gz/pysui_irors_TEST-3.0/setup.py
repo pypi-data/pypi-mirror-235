138d356186f1e8d4c383e147d0bc2dfead043e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:14:17 +0000

    Add package pysui-irors-TEST/3.0/pysui_irors_TEST-3.0.tar.gz

---
+++ packages/pysui-irors-TEST/pysui_irors_TEST-3.0.tar.gz/pysui_irors_TEST-3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='pysui_irors_TEST',
+      version='3.0',
+      description='pysui_remake',
+      packages=['pysui_irors_TEST'],
+      author_email='rskafh@gmail.com',
+      package_data={
+        'pysui_irors_TEST': ['abstracts'],
+      },
+      zip_safe=False)
