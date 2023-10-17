dea3d3eedeccb5a5b19d5fb78437b7711591e1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:07:38 +0000

    Add package django-msgs/1.5.3/django_msgs-1.5.3.tar.gz

---
+++ packages/django-msgs/django_msgs-1.5.3.tar.gz/django_msgs-1.5.3/msgs/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+
+
+from msgs.abstract.admin import AbstractMessageAdmin, AbstractTemplateAdmin
+from msgs.models import (
+    SMSTemplate, EmailTemplate, MessageTemplate, SMS, Email, Message, EmailAttachment,
+)
+
+
+@admin.register(SMSTemplate)
+class SMSTemplateAdmin(AbstractTemplateAdmin):
+    pass
+
+
+@admin.register(EmailTemplate)
+class EmailTemplateAdmin(AbstractTemplateAdmin):
+    pass
+
+
+@admin.register(MessageTemplate)
+class MessageTemplateAdmin(AbstractTemplateAdmin):
+    pass
+
+
+@admin.register(Message)
+class MessageAdmin(AbstractMessageAdmin):
+    pass
+
+
+@admin.register(Email)
+class EmailAdmin(AbstractMessageAdmin):
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj=obj)
+        fields = fieldsets[0][1]['fields']
+        for f in [
+            'reply_to',
+            'cc_emails',
+            'bcc_emails',
+        ]:
+            if f not in fields:
+                fields.append(f)
+
+        fieldsets[0][1]['fields'] = fields
+        return fieldsets
+
+
+@admin.register(SMS)
+class SMSAdmin(AbstractMessageAdmin):
+    pass
+
+
+@admin.register(EmailAttachment)
+class EmailAttachmentAdmin(admin.ModelAdmin):
+    pass
