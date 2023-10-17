148ac1f81b1fd1950d0e85be3df3411624989a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:14 +0000

    Add package bg-task-queue/0.0.1/bg_task_queue-0.0.1.tar.gz

---
+++ packages/bg-task-queue/bg_task_queue-0.0.1.tar.gz/bg_task_queue-0.0.1/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bg_task_queue',
+    version='0.0.1',
+    package_dir={"": "src"},
+    install_requires=[],
+    license='MIT',
+    author="jhleee",
+    author_email="ng0301@gmail.com",
+    description="python simple multi threading task queue",
+    url="https://github.com/jhleee/python-bg-task-queue",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
