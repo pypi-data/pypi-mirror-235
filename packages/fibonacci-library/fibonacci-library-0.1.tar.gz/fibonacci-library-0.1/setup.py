92cc07550df121a271792c1245246021c71e55ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:09:21 +0000

    Add package fibonacci-library/0.1/fibonacci-library-0.1.tar.gz

---
+++ packages/fibonacci-library/fibonacci-library-0.1.tar.gz/fibonacci-library-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='fibonacci-library',
+    version='0.1',
+    author="Rakesh Kumar",
+    description="A Simple Python library for generate fibonacci series",
+    packages=['fibonacci'],
+)
