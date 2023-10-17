03b87af70e10de9d610f06e1521522bdb020d8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:48 +0000

    Add package kot-run/0.0.0/kot_run-0.0.0.tar.gz

---
+++ packages/kot-run/kot_run-0.0.0.tar.gz/kot_run-0.0.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+
+setup(
+    name="kot_run",
+    version="0.0.0",
+    description="""KOT""",
+    url="https://github.com/KOT",
+    author='Onur Atakan ULUSOY',
+    author_email='atadogan06@gmail.com',
+    license='MIT',
+    python_requires=">= 3",
+    zip_safe=False,
+)
+
+
