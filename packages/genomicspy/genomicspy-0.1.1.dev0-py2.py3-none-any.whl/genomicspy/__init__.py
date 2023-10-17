5e3afb575f75be1d46733c1039a2da790b3a21d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:10:41 +0000

    Add package genomicspy/0.1.1.dev0/genomicspy-0.1.1.dev0-py2.py3-none-any.whl

---
+++ packages/genomicspy/genomicspy-0.1.1.dev0-py2.py3-none-any.whl/genomicspy/__init__.py
@@ -0,0 +1,10 @@
+"""
+Classes and methods for manipulating VCFs, FASTAs and other sequences
+"""
+
+from .allele_msa import *
+from .alleles import *
+from .main import *
+from .records_chunk import *
+from .records_saver import *
+from .trim import *
