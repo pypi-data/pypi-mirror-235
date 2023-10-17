0d885103532d09055b0a60589508384eff755808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:08:40 +0000

    Add package Maksik-Unzhakov/0.0/Maksik_Unzhakov-0.0.tar.gz

---
+++ packages/Maksik-Unzhakov/Maksik_Unzhakov-0.0.tar.gz/Maksik_Unzhakov-0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+  name='Maksik_Unzhakov',
+  version='0.0',
+  author='levchik',
+  author_email='mamadorogayamoskva@gmail.com',
+  description='This is the simplest module for quick work with files.',
+  
+  long_description_content_type='text/markdown',
+  
+  
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  python_requires='>=3.6'
+)
