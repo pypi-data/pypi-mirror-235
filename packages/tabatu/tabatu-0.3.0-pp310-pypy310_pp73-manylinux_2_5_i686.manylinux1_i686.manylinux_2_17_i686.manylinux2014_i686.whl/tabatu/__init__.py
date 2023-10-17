a60385916dea283463910a445c92ecc93dc30936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:09:10 +0000

    Add package tabatu/0.3.0/tabatu-0.3.0-pp310-pypy310_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/tabatu/tabatu-0.3.0-pp310-pypy310_pp73-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/tabatu/__init__.py
@@ -0,0 +1,19 @@
+from tabatu.multiplos_decrementos import TabuaMDT
+from tabatu.multiplas_vidas import TabuaMultiplasVidas, StatusVidasConjuntas
+from tabatu.unico_decremento import Tabua
+from tabatu.alterar_tabua import alterar_periodicidade_qx
+from tabatu.alterar_tabua import agravar_qx
+from tabatu.juros_constante import JurosConstante
+from tabatu.alterar_juros import alterar_periodicidade_juros
+
+
+__all__ = [
+    "Tabua",
+    "TabuaMDT",
+    "TabuaMultiplasVidas",
+    "StatusVidasConjuntas",
+    "agravar_qx",
+    "alterar_periodicidade_qx",
+    "JurosConstante",
+    "alterar_periodicidade_juros",
+]
