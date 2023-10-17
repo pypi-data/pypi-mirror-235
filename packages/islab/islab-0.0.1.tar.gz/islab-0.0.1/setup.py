f6cc5fa0256fbdbf359048a1a4b86bff8b5b7728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 01:28:44 +0000

    Add package islab/0.0.1/islab-0.0.1.tar.gz

---
+++ packages/islab/islab-0.0.1.tar.gz/islab-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'ISLab-OpenDeid Sample Code'
+LONG_DESCRIPTION = 'The sample code provided for the AICUP 2023 challenge organized by NKUST-ISLab'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="islab", 
+        version=VERSION,
+        author="Hong-Jie Dai",
+        author_email="<hjdai@nkust.edu.tw>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['aicup', 'opendeid'],
+        classifiers= [            
+        ]
+)
