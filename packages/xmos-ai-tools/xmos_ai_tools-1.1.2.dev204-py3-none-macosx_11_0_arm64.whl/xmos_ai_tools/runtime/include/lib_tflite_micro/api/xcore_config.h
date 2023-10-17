f6e80aef01ca417d2777e477a2701a61be6a0e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:07:14 +0000

    Add package xmos-ai-tools/1.1.2.dev204/xmos_ai_tools-1.1.2.dev204-py3-none-macosx_11_0_arm64.whl

---
+++ packages/xmos-ai-tools/xmos_ai_tools-1.1.2.dev204-py3-none-macosx_11_0_arm64.whl/xmos_ai_tools/runtime/include/lib_tflite_micro/api/xcore_config.h
@@ -0,0 +1,11 @@
+#ifndef XCORE_CONFIG_H_
+#define XCORE_CONFIG_H_
+
+#include "../src/thread_call.h"
+
+struct xc_context_config_t {
+  thread_info_t thread_info;
+  void *UNSAFE flash_data; // channel to flash reader.
+};
+
+#endif // XCORE_CONFIG_H_
