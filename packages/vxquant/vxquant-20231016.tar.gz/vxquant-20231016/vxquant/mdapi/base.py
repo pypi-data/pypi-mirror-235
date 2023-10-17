35bf788e248863930db922d0ad3999778c436c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:23 +0000

    Add package vxquant/20231016/vxquant-20231016.tar.gz

---
+++ packages/vxquant/vxquant-20231016.tar.gz/vxquant-20231016/vxquant/mdapi/base.py
@@ -0,0 +1,23 @@
+"""行情接口"""
+
+
+from vxutils.importutils import vxAPIWrappers
+
+
+class vxMdAPI(vxAPIWrappers):
+    __defaults__ = {
+        "calendar": {
+            "class": "vxquant.providers.spiders.CNCalenderProvider",
+            "params": {},
+        },
+        "hq": {"class": "vxquant.providers.spiders.vxTencentHQProvider", "params": {}},
+        "current": {
+            "class": "vxquant.providers.spiders.vxTencentHQProvider",
+            "params": {},
+        },
+    }
+
+
+if __name__ == "__main__":
+    mdapi = vxMdAPI()
+    print(mdapi.current("SHSE.600000"))
