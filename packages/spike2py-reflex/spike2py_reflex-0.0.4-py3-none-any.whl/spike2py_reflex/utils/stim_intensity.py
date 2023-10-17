fbb586dad489cf199017a79733c868d4aa65f413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:11:41 +0000

    Add package spike2py-reflex/0.0.4/spike2py_reflex-0.0.4-py3-none-any.whl

---
+++ packages/spike2py-reflex/spike2py_reflex-0.0.4-py3-none-any.whl/spike2py_reflex/utils/stim_intensity.py
@@ -0,0 +1,24 @@
+from spike2py_preprocess.trial_sections import find_nearest_time_index
+
+
+def get_stim_intensity(info, data) -> list:
+    ch = _get_stim_intensity_channel(info, data)
+    intensities = list()
+    for trigger in info.triggers.extract:
+        intensity_value = None
+        if ch is not None:
+            idx = find_nearest_time_index(ch.times, trigger)
+            intensity_value = round(ch.values[idx])
+        intensities.append(intensity_value)
+    return intensities
+
+
+def _get_stim_intensity_channel(info, data):
+    stim_intensity = None
+    try:
+        stim_intensity = getattr(data, info.channels.stim_intensity)
+    except AttributeError:
+        print(f'\t\tstudy: {info.study}; '
+              f'trial: {info.trial}; '
+              f'section: {info.section} --> no `stim_intensity` channel.')
+    return stim_intensity
