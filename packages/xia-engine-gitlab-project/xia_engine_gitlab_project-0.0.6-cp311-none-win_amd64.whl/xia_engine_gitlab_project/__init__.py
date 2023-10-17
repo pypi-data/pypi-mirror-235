fc90bdd8cd5f12b45a0cf5a0b88d73608e4703c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 01:27:21 +0000

    Add package xia-engine-gitlab-project/0.0.6/xia_engine_gitlab_project-0.0.6-cp311-none-win_amd64.whl

---
+++ packages/xia-engine-gitlab-project/xia_engine_gitlab_project-0.0.6-cp311-none-win_amd64.whl/xia_engine_gitlab_project/__init__.py
@@ -0,0 +1,13 @@
+from xia_engine_gitlab_project.engine import GitlabProjectEngine
+from xia_engine_gitlab_project.engine_milestone import GitlabProjectMilestoneEngine
+from xia_engine_gitlab_project.engine_issue import GitlabProjectMilestoneIssueEngine
+from xia_engine_gitlab_project.engine_notes import GitlabProjectIssueNoteEngine
+
+__all__ = [
+    "GitlabProjectEngine",
+    "GitlabProjectMilestoneEngine",
+    "GitlabProjectMilestoneIssueEngine",
+    "GitlabProjectIssueNoteEngine"
+]
+
+__version__ = "0.0.6"
