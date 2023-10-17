bb9c16cd2ef00260ff818ea11d9143025321b373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:06:58 +0000

    Add package dstwo/1.0.0/dstwo-1.0.0.tar.gz

---
+++ packages/dstwo/dstwo-1.0.0.tar.gz/dstwo-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dstwo',
+    version='1.0.0',
+    author='Reny',
+    author_email='Rjs3175@aliyun.com',
+    description='Basic data structures and algorithms',
+    packages=['data_struct'],
+    install_requires=[],
+)
