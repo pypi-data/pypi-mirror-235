e4d2ec04bba1c8672902130e896c81cc9da74930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:09:07 +0000

    Add package leak-snek/0.2.0/leak_snek-0.2.0-py3-none-any.whl

---
+++ packages/leak-snek/leak_snek-0.2.0-py3-none-any.whl/leak_snek/interfaces/values/rate.py
@@ -0,0 +1,20 @@
+"""Module containing rate model."""
+from __future__ import annotations
+
+import dataclasses
+import time
+from typing import Self, final
+
+
+@dataclasses.dataclass
+@final
+class Rate:
+    """Model representing the current access rate."""
+
+    operations: int
+    updated_at: float
+
+    @classmethod
+    def default(cls: type[Self]) -> Self:
+        """Get the default (zero initialized) rate."""
+        return cls(operations=0, updated_at=time.monotonic())
