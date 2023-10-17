7ecb4f4241ccd46a8bd5da6abdaf4be550040f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:48 +0000

    Add package kot-update/0.0.0/kot_update-0.0.0.tar.gz

---
+++ packages/kot-update/kot_update-0.0.0.tar.gz/kot_update-0.0.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+
+setup(
+    name="kot_update",
+    description="""KOT""",
+    url="https://github.com/KOT",
+    author='Onur Atakan ULUSOY',
+    author_email='atadogan06@gmail.com',
+    license='MIT',
+    python_requires=">= 3",
+    zip_safe=False,
+)
+
+
