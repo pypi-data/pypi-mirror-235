05f3e024bc44b21eee4972262b71801e4b2c5dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 15:06:31 +0000

    Add package hamal-utils/0.0.30/hamal-utils-0.0.30.tar.gz

---
+++ packages/hamal-utils/hamal-utils-0.0.30.tar.gz/hamal-utils-0.0.30/hamal_utils/code/common.py
@@ -0,0 +1,1 @@
+MONITOR_TAG_TMPL = "hamalutils-{name}-{from_percent}-{to_percent}"
---
+++ packages/hamal-utils/hamal-utils-0.0.30.tar.gz/hamal-utils-0.0.30/hamal_utils/code/s3_utils/__init__.py
@@ -0,0 +1,3 @@
+from hamal_utils.code.s3_utils.s3_custom_client import CustomS3Client
+
+s3_client = CustomS3Client()
