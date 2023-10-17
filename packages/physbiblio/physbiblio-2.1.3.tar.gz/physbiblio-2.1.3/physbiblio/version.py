e164b1fbbdc85bd209ce6c4e7b12076f3911f583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:07:06 +0000

    Add package physbiblio/2.1.3/physbiblio-2.1.3.tar.gz

---
+++ packages/physbiblio/physbiblio-2.1.3.tar.gz/physbiblio-2.1.3/physbiblio/version.py
@@ -0,0 +1,7 @@
+__version__ = "2.1.3"
+__version_date__ = "16/09/2023"
+
+__recent_changes__ = """<br>* exportForTexFile runs checkDuplicates<br>
+* small internal improvements<br>
+* bug fixing<br>
+"""
