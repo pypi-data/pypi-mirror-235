59e4f50d5bddf27fe407417f0e663276e675cc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 15:06:51 +0000

    Add package django-signoffs/0.3.4/django_signoffs-0.3.4-py3-none-any.whl

---
+++ packages/django-signoffs/django_signoffs-0.3.4-py3-none-any.whl/signoffs/process.py
@@ -0,0 +1,11 @@
+"""
+    Proxy for Approval Process to simplify import statements and hide core package structure from client code.
+
+    isort:skip_file
+"""
+
+from signoffs.core.process import (
+    ApprovalsProcess,
+    FsmApprovalsProcess,
+    user_can_revoke_approval,
+)
