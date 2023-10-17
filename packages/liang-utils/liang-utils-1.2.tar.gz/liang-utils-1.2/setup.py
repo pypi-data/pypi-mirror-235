b9cb10ff327506ae128e552d1d9bae493ff7b0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:07:52 +0000

    Add package liang-utils/1.2/liang-utils-1.2.tar.gz

---
+++ packages/liang-utils/liang-utils-1.2.tar.gz/liang-utils-1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="liang-utils",
+    version="1.2",
+    packages=find_packages(),    # detect and include all packages automatically
+    install_requires=[
+        'ttp',
+        # 'openpyxl',
+        'tqdm',
+    ],
+    entry_points={
+        "console_scripts": [
+            "match_dict = liang_utils.match_dict:main",
+            "split_log = liang_utils.process_log:main",
+        ],
+    },
+    # scripts=['bin/upgrade_check'],      # CLI tool entry point
+    author="Liang",
+    description="A couple of liang_utils for network automation",
+    python_requires='>=3.8',
+)
