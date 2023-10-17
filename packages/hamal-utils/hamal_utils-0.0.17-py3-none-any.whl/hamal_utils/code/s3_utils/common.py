1af9e1e9b5e6bec6fb97924a444806be06fd7005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:08:12 +0000

    Add package hamal-utils/0.0.17/hamal_utils-0.0.17-py3-none-any.whl

---
+++ packages/hamal-utils/hamal_utils-0.0.17-py3-none-any.whl/hamal_utils/code/s3_utils/common.py
@@ -0,0 +1,19 @@
+import boto3
+
+from hamal_utils.code.prefect_utils.access_manager import s3_access_data
+
+src_bucket = 'hamal-red-feed-excaliber'
+dest_bucket = 'hamal-data-pipelines-dev'
+s3_prefix = "silver/unique-red/"
+
+aws_access_key_id = ''
+aws_secret_access_key = ''
+
+# TODO: remove count argument and get bucket count from designated function
+count = 10000000
+
+region_name, access_key_id, secret_access_key = s3_access_data()
+if access_key_id and secret_access_key:
+    s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+else:
+    s3 = boto3.client('s3')
