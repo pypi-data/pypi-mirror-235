e294d499dedd996265a7aa5edc17b1c5ba6eb9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:07:54 +0000

    Add package doc-template-cloner/0.1.2/doc_template_cloner-0.1.2.tar.gz

---
+++ packages/doc-template-cloner/doc_template_cloner-0.1.2.tar.gz/doc_template_cloner-0.1.2/setup.py
@@ -0,0 +1,50 @@
+
+from __future__ import absolute_import
+
+import os
+from glob import glob
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    """
+    Args:
+        fname:
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+# Declare minimal set for installation
+required_packages = [
+    #"boto3>=1.20.21,<2.0",
+    "pandas",
+]
+
+setup(
+    name="doc_template_cloner",
+    version="0.1.2",
+    description="Custom lib to clone lebaling from one image to another similar",
+    long_description_content_type="text/markdown",
+    long_description=read('README.md'),
+    packages=find_packages(),
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob("doc_template_cloner/*.py")],
+    include_package_data=True,
+    author="Ihor Bilyk",
+    url="https://github.com/bilykigor/doc_template_cloner",
+    license="Apache License 2.0",
+    keywords="Custom lib to clone lebaling from one image to another similar",
+    python_requires=">= 3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    install_requires=required_packages
+)
