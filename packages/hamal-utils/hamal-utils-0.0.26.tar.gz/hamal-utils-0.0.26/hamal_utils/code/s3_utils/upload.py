a7e58b24861830849a18bca78f59735721411f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:10:06 +0000

    Add package hamal-utils/0.0.26/hamal-utils-0.0.26.tar.gz

---
+++ packages/hamal-utils/hamal-utils-0.0.26.tar.gz/hamal-utils-0.0.26/hamal_utils/code/s3_utils/upload.py
@@ -0,0 +1,11 @@
+from hamal_utils.code.s3_utils.common import s3
+
+
+def upload_file_to_s3(bucket, s3_prefix, file_key, body, flatten=True):
+    s3_prefix = s3_prefix.rstrip('/')
+
+    key = file_key if not flatten else file_key.replace('/', '_')
+    s3.put_object(
+        Body=body,
+        Bucket=bucket,
+        Key=s3_prefix + key)
