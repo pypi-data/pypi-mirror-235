8c5a085444d4addc3680b8723410ba8284ccaefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:06:09 +0000

    Add package cesnet-datazoo/0.0.7/cesnet-datazoo-0.0.7.tar.gz

---
+++ packages/cesnet-datazoo/cesnet-datazoo-0.0.7.tar.gz/cesnet-datazoo-0.0.7/cesnet_datazoo/datasets/loaders.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from cesnet_datazoo.constants import APP_COLUMN
+
+
+def load_from_dataloader(dataloader: DataLoader) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    data_ppi = []
+    data_flowstats = []
+    labels = []
+    print("Loading data from dataloader")
+    for batch_ppi, batch_flowstats, batch_labels in tqdm(dataloader, total=len(dataloader)):
+        data_ppi.append(batch_ppi)
+        data_flowstats.append(batch_flowstats)
+        labels.append(batch_labels)
+    data_ppi = np.concatenate(data_ppi)
+    data_flowstats = np.concatenate(data_flowstats)
+    labels = np.concatenate(labels)
+    return data_ppi, data_flowstats, labels
+
+def create_df_from_dataloader(dataloader: DataLoader, feature_names: list[str], flatten_ppi: bool = False) -> pd.DataFrame:
+    data_ppi, data_flowstats, labels = load_from_dataloader(dataloader)
+    if flatten_ppi:
+        data_ppi = data_ppi.reshape(data_ppi.shape[0], -1)
+        data = np.column_stack((data_ppi, data_flowstats))
+        df = pd.DataFrame(data=data, columns=feature_names)
+    else:
+        ppi_column, *feature_names = feature_names
+        df = pd.DataFrame(data=data_flowstats, columns=feature_names)
+        df.insert(0, column=ppi_column, value=list(data_ppi))
+    df[APP_COLUMN] = labels
+    return df
