501367c58b5a65d4cb5f8aaff9131d1833f8f784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:29 +0000

    Add package panda-server/0.0.68/panda-server-0.0.68.tar.gz

---
+++ packages/panda-server/panda-server-0.0.68.tar.gz/panda-server-0.0.68/pandaserver/taskbuffer/PanDAMsgProcessor.py
@@ -0,0 +1,9 @@
+from pandacommon.pandamsgbkr import msg_processor
+from pandacommon.pandalogger import logger_utils
+
+from pandaserver.config import panda_config
+
+
+# Main message processing agent
+class MsgProcAgent(msg_processor.MsgProcAgentBase):
+    pass
