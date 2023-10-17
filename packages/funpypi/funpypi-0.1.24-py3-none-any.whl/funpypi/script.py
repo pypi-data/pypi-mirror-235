e3eb187f7b0b523894c2c93cba639b0b4fc2ecda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:12:12 +0000

    Add package funpypi/0.1.24/funpypi-0.1.24-py3-none-any.whl

---
+++ packages/funpypi/funpypi-0.1.24-py3-none-any.whl/funpypi/script.py
@@ -0,0 +1,20 @@
+import argparse
+import subprocess
+
+
+def install():
+    packages = ["funpypi", "fundb", "funsecret", "farfuntask", "funbuild", "fundrive", "funread"]
+    for package in packages:
+        subprocess.check_call(["pip", "install", package, "-U -i https://pypi.org/simple/ -q"])
+
+
+def funpypi():
+    parser = argparse.ArgumentParser(prog="PROG")
+    subparsers = parser.add_subparsers(help="sub-command help")
+
+    # 添加子命令
+    build_parser = subparsers.add_parser("install", help="install farfarfun packages")
+    build_parser.set_defaults(func=install)
+
+    args = parser.parse_args()
+    args.func(args)
