facb52d00f6896a3d6f177db3f06c7d9bf427d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:07:39 +0000

    Add package fforward/0.1.1/fforward-0.1.1.tar.gz

---
+++ packages/fforward/fforward-0.1.1.tar.gz/fforward-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fforward']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.103.2,<0.104.0', 'sqlalchemy>=2.0.22,<3.0.0']
+
+setup_kwargs = {
+    'name': 'fforward',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '# forward\nForward\n',
+    'author': 'TheGreen',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
