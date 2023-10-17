192b3f22fa242ae97a259dfc748c494295d1357d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 17:08:13 +0000

    Add package netbox-plugin-dns/0.20.0/netbox_plugin_dns-0.20.0.tar.gz

---
+++ packages/netbox-plugin-dns/netbox_plugin_dns-0.20.0.tar.gz/netbox_plugin_dns-0.20.0/netbox_dns/migrations/0022_search.py
@@ -0,0 +1,11 @@
+import sys
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("netbox_dns", "0021_record_ip_address"),
+    ]
+
+    operations = []
