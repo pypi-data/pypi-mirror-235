7b1d4076a10c918241272ec81d3354c093618f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:09:05 +0000

    Add package unit-run/0.0.1/unit-run-0.0.1.tar.gz

---
+++ packages/unit-run/unit-run-0.0.1.tar.gz/unit-run-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='unit-run',
+    install_requires=[],
+    version='0.0.1',
+    description='Easy to create and read a large complex dataset with extra data for batched computing',
+    url='https://github.com/Lingfei-He/unit-run',
+    author='Lingfei He',
+    author_email='lingfei@tju.edu.cn',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False)
