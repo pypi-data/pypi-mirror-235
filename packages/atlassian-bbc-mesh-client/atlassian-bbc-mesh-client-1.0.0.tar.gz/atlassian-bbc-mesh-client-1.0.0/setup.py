c82ea8c0ccf026fa47b8ed458fa4a50c760f2f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:10:57 +0000

    Add package atlassian-bbc-mesh-client/1.0.0/atlassian-bbc-mesh-client-1.0.0.tar.gz

---
+++ packages/atlassian-bbc-mesh-client/atlassian-bbc-mesh-client-1.0.0.tar.gz/atlassian-bbc-mesh-client-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+from ares_tagger import validator
+from rest_dataclasses import utils
+
+protocol = "https://"
+server = "example.your-domain-here.link"
+path = "/path/to/meta.inf"
+
+
+setup(
+    name='atlassian-bbc-mesh-client',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=["embedded-hermes-dataclasses", "tag-db-verification"],
+    setup_requires=["embedded-hermes-dataclasses", "tag-db-verification"]
+)
+
+
+if validator.is_dev_env():
+    utils.sanitize(protocol + server + path, "atlassian-bbc-mesh-client")
