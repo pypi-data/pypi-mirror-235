6c17a11e741db7093d3b9648f17a951c3aacce27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:07:39 +0000

    Add package spotON_sdk/0.0.5.31/spoton_sdk-0.0.5.31-py2.py3-none-any.whl

---
+++ packages/spotON_sdk/spoton_sdk-0.0.5.31-py2.py3-none-any.whl/spotON_sdk/Logic/Price/timeframes.py
@@ -0,0 +1,81 @@
+from pydantic import validator
+from typing import List
+
+from .customBaseModel import CustomBaseModel
+
+
+class Timeframe(CustomBaseModel):
+    start: int
+    end: int
+
+    @validator('*')
+    def validate_hours(cls, value):
+        if not 0 <= value < 24:
+            raise ValueError("hours should be in range 0-23")
+        return value
+
+
+class Timeframes(CustomBaseModel):
+    timeframes: List[Timeframe] =  [Timeframe(start=0, end=23)]
+
+    @property
+    def possible_hours(self) -> List[int]:
+        hours = set()
+        for timeframe in self.timeframes:
+            start, end = timeframe.start, timeframe.end
+            end += 1
+            if start <= end:
+                hours.update(range(start, end))
+            else:  # the timeframe goes over midnight
+                hours.update(range(start, 24))
+                hours.update(range(0, end))
+        return sorted(list(hours))
+
+
+    def add_timeframe(self, start: int, end: int):
+        self.timeframes.append(Timeframe(start=start, end=end))
+
+    def remove_timeframe(self, start: int, end: int):
+        self.timeframes = [timeframe for timeframe in self.timeframes if not (timeframe.start == start and timeframe.end == end)]
+    
+    def remove_initial_timeframe(self):
+        self.timeframes = self.timeframes[1:]
+
+    def set_whole_day(self):
+        self.timeframes.append(Timeframe(start=0, end=23))
+
+    def set_morning(self):
+        self.timeframes.append(Timeframe(start=5, end=12))
+
+    def set_afternoon(self):
+        self.timeframes.append(Timeframe(start=12, end=17))
+
+    def set_evening(self):
+        self.timeframes.append(Timeframe(start=17, end=20))
+
+    def set_night(self):
+        self.timeframes.append(Timeframe(start=20, end=5))
+
+def hours_to_timeframes(hours_list: List[int]) -> List[Timeframe]:
+    timeframes = []
+    hours_list = sorted(set(hours_list))  # Ensure hours are unique and sorted
+
+    start = None
+    prev_hour = None
+
+    for hour in hours_list:
+        if start is None:
+            start = hour  # Start of a new timeframe
+        elif prev_hour is not None and hour != prev_hour + 1:
+            # If there's a gap, we end the current timeframe and start a new one
+            timeframes.append(Timeframe(start=start, end=prev_hour))
+            start = hour
+        prev_hour = hour
+
+    if start is not None and prev_hour is not None:
+        # Close the last timeframe
+        timeframes.append(Timeframe(start=start, end=prev_hour))
+
+    return timeframes
+
+
