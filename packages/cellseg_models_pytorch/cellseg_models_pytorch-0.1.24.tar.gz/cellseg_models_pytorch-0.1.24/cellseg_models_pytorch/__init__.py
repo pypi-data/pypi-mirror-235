681a40168e00da09e563ee0c792d6874efe89a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:51 +0000

    Add package cellseg_models_pytorch/0.1.24/cellseg_models_pytorch-0.1.24.tar.gz

---
+++ packages/cellseg_models_pytorch/cellseg_models_pytorch-0.1.24.tar.gz/cellseg_models_pytorch-0.1.24/cellseg_models_pytorch/__init__.py
@@ -0,0 +1,14 @@
+from . import inference, models, utils
+from .models import CellPoseUnet, HoverNet, StarDistUnet
+
+__version__ = "0.1.24"
+submodules = ["utils", "models", "inference"]
+__all__ = [
+    "__version__",
+    "CellPoseUnet",
+    "StarDistUnet",
+    "HoverNet",
+    "utils",
+    "models",
+    "inference",
+]
