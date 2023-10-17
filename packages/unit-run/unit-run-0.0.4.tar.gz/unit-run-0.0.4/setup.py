836150ef61061634fcdac639bf85d3addbd6b073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:09:01 +0000

    Add package unit-run/0.0.4/unit-run-0.0.4.tar.gz

---
+++ packages/unit-run/unit-run-0.0.4.tar.gz/unit-run-0.0.4/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='unit-run',
+    install_requires=[],
+    description='Easy to store and reperform unit running process.',
+    url='https://github.com/Lingfei-He/unit-run',
+    author='Lingfei He',
+    author_email='lingfei@tju.edu.cn',
+    license='MIT',
+    entry_points={
+        'console_scripts':[
+            'unit-run = unit_run.cli:main'
+        ]
+    },
+    packages=find_packages(),
+    zip_safe=False)
