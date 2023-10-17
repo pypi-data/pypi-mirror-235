c469c559c6dd097e42df56953016e5f7178f12c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:03 +0000

    Add package rhseung.units/0.1.2/rhseung.units-0.1.2.tar.gz

---
+++ packages/rhseung.units/rhseung.units-0.1.2.tar.gz/rhseung.units-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='rhseung.units',
+	version='0.1.2',
+	description='A package for handling units.',
+	author='rhseung',
+	license='MIT',
+	author_email='rhseungg@gmail.com',
+	url='https://github.com/rhseung',
+	packages=find_packages(),
+	keywords=['units', 'physics', 'mathematics'],
+	python_requires='>=3.11',
+	package_data={},
+	zip_safe=False
+)
+
