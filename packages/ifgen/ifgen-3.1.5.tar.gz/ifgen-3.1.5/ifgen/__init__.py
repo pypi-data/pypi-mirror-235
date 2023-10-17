51ba7d9f0e8b7e205a14c01fe2bcd2d335293a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:09:16 +0000

    Add package ifgen/3.1.5/ifgen-3.1.5.tar.gz

---
+++ packages/ifgen/ifgen-3.1.5.tar.gz/ifgen-3.1.5/ifgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=6f05f5ec8ef6e670482d6a0966b7641f
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "An interface generator for distributed computing."
+PKG_NAME = "ifgen"
+VERSION = "3.1.5"
