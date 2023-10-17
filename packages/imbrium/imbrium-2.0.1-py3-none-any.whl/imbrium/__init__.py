37a736e3f04bd212e36e9987b50565515c9b4dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 01:29:25 +0000

    Add package imbrium/2.0.1/imbrium-2.0.1-py3-none-any.whl

---
+++ packages/imbrium/imbrium-2.0.1-py3-none-any.whl/imbrium/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "2.0.1"
+
+from imbrium.predictors.multivarhybrid import HybridMulti
+from imbrium.predictors.multivarpure import PureMulti
+from imbrium.predictors.univarhybrid import HybridUni
+from imbrium.predictors.univarpure import PureUni
