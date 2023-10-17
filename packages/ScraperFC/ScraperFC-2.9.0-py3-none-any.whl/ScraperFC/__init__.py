927168642e8d176458bfbd27f000091d9c3a627b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:15:30 +0000

    Add package ScraperFC/2.9.0/ScraperFC-2.9.0-py3-none-any.whl

---
+++ packages/ScraperFC/ScraperFC-2.9.0-py3-none-any.whl/ScraperFC/__init__.py
@@ -0,0 +1,9 @@
+from .FBRef import FBRef
+from .FiveThirtyEight import FiveThirtyEight
+from .Understat import Understat
+# from .SofaScore import SofaScore
+from .ClubElo import ClubElo
+from .Capology import Capology
+from .Transfermarkt import Transfermarkt
+from .Oddsportal import Oddsportal
+from .shared_functions import *
