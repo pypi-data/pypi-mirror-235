5332251b21e2ef028742b3f5370b7e2392dac821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:11:59 +0000

    Add package InvokeAI/3.3.0.post3/InvokeAI-3.3.0.post3.tar.gz

---
+++ packages/InvokeAI/InvokeAI-3.3.0.post3.tar.gz/InvokeAI-3.3.0.post3/invokeai/app/api/dependencies.py
@@ -0,0 +1,164 @@
+# Copyright (c) 2022 Kyle Schouviller (https://github.com/kyle0654)
+
+import sqlite3
+from logging import Logger
+
+from invokeai.app.services.board_image_record_storage import SqliteBoardImageRecordStorage
+from invokeai.app.services.board_images import BoardImagesService, BoardImagesServiceDependencies
+from invokeai.app.services.board_record_storage import SqliteBoardRecordStorage
+from invokeai.app.services.boards import BoardService, BoardServiceDependencies
+from invokeai.app.services.config import InvokeAIAppConfig
+from invokeai.app.services.image_record_storage import SqliteImageRecordStorage
+from invokeai.app.services.images import ImageService, ImageServiceDependencies
+from invokeai.app.services.invocation_cache.invocation_cache_memory import MemoryInvocationCache
+from invokeai.app.services.resource_name import SimpleNameService
+from invokeai.app.services.session_processor.session_processor_default import DefaultSessionProcessor
+from invokeai.app.services.session_queue.session_queue_sqlite import SqliteSessionQueue
+from invokeai.app.services.urls import LocalUrlService
+from invokeai.backend.util.logging import InvokeAILogger
+from invokeai.version.invokeai_version import __version__
+
+from ..services.default_graphs import create_system_graphs
+from ..services.graph import GraphExecutionState, LibraryGraph
+from ..services.image_file_storage import DiskImageFileStorage
+from ..services.invocation_queue import MemoryInvocationQueue
+from ..services.invocation_services import InvocationServices
+from ..services.invocation_stats import InvocationStatsService
+from ..services.invoker import Invoker
+from ..services.latent_storage import DiskLatentsStorage, ForwardCacheLatentsStorage
+from ..services.model_manager_service import ModelManagerService
+from ..services.processor import DefaultInvocationProcessor
+from ..services.sqlite import SqliteItemStorage
+from ..services.thread import lock
+from .events import FastAPIEventService
+
+
+# TODO: is there a better way to achieve this?
+def check_internet() -> bool:
+    """
+    Return true if the internet is reachable.
+    It does this by pinging huggingface.co.
+    """
+    import urllib.request
+
+    host = "http://huggingface.co"
+    try:
+        urllib.request.urlopen(host, timeout=1)
+        return True
+    except Exception:
+        return False
+
+
+logger = InvokeAILogger.get_logger()
+
+
+class ApiDependencies:
+    """Contains and initializes all dependencies for the API"""
+
+    invoker: Invoker
+
+    @staticmethod
+    def initialize(config: InvokeAIAppConfig, event_handler_id: int, logger: Logger = logger):
+        logger.info(f"InvokeAI version {__version__}")
+        logger.info(f"Root directory = {str(config.root_path)}")
+        logger.debug(f"Internet connectivity is {config.internet_available}")
+
+        events = FastAPIEventService(event_handler_id)
+
+        output_folder = config.output_path
+
+        # TODO: build a file/path manager?
+        if config.use_memory_db:
+            db_location = ":memory:"
+        else:
+            db_path = config.db_path
+            db_path.parent.mkdir(parents=True, exist_ok=True)
+            db_location = str(db_path)
+
+        logger.info(f"Using database at {db_location}")
+        db_conn = sqlite3.connect(db_location, check_same_thread=False)  # TODO: figure out a better threading solution
+
+        if config.log_sql:
+            db_conn.set_trace_callback(print)
+        db_conn.execute("PRAGMA foreign_keys = ON;")
+
+        graph_execution_manager = SqliteItemStorage[GraphExecutionState](
+            conn=db_conn, table_name="graph_executions", lock=lock
+        )
+
+        urls = LocalUrlService()
+        image_record_storage = SqliteImageRecordStorage(conn=db_conn, lock=lock)
+        image_file_storage = DiskImageFileStorage(f"{output_folder}/images")
+        names = SimpleNameService()
+        latents = ForwardCacheLatentsStorage(DiskLatentsStorage(f"{output_folder}/latents"))
+
+        board_record_storage = SqliteBoardRecordStorage(conn=db_conn, lock=lock)
+        board_image_record_storage = SqliteBoardImageRecordStorage(conn=db_conn, lock=lock)
+
+        boards = BoardService(
+            services=BoardServiceDependencies(
+                board_image_record_storage=board_image_record_storage,
+                board_record_storage=board_record_storage,
+                image_record_storage=image_record_storage,
+                url=urls,
+                logger=logger,
+            )
+        )
+
+        board_images = BoardImagesService(
+            services=BoardImagesServiceDependencies(
+                board_image_record_storage=board_image_record_storage,
+                board_record_storage=board_record_storage,
+                image_record_storage=image_record_storage,
+                url=urls,
+                logger=logger,
+            )
+        )
+
+        images = ImageService(
+            services=ImageServiceDependencies(
+                board_image_record_storage=board_image_record_storage,
+                image_record_storage=image_record_storage,
+                image_file_storage=image_file_storage,
+                url=urls,
+                logger=logger,
+                names=names,
+                graph_execution_manager=graph_execution_manager,
+            )
+        )
+
+        services = InvocationServices(
+            model_manager=ModelManagerService(config, logger),
+            events=events,
+            latents=latents,
+            images=images,
+            boards=boards,
+            board_images=board_images,
+            queue=MemoryInvocationQueue(),
+            graph_library=SqliteItemStorage[LibraryGraph](conn=db_conn, lock=lock, table_name="graphs"),
+            graph_execution_manager=graph_execution_manager,
+            processor=DefaultInvocationProcessor(),
+            configuration=config,
+            performance_statistics=InvocationStatsService(graph_execution_manager),
+            logger=logger,
+            session_queue=SqliteSessionQueue(conn=db_conn, lock=lock),
+            session_processor=DefaultSessionProcessor(),
+            invocation_cache=MemoryInvocationCache(max_cache_size=config.node_cache_size),
+        )
+
+        create_system_graphs(services.graph_library)
+
+        ApiDependencies.invoker = Invoker(services)
+
+        try:
+            lock.acquire()
+            db_conn.execute("VACUUM;")
+            db_conn.commit()
+            logger.info("Cleaned database")
+        finally:
+            lock.release()
+
+    @staticmethod
+    def shutdown():
+        if ApiDependencies.invoker:
+            ApiDependencies.invoker.stop()
---
+++ packages/InvokeAI/InvokeAI-3.3.0.post3.tar.gz/InvokeAI-3.3.0.post3/invokeai/app/services/invocation_services.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2022 Kyle Schouviller (https://github.com/kyle0654) and the InvokeAI Team
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from logging import Logger
+
+    from invokeai.app.services.board_images import BoardImagesServiceABC
+    from invokeai.app.services.boards import BoardServiceABC
+    from invokeai.app.services.config import InvokeAIAppConfig
+    from invokeai.app.services.events import EventServiceBase
+    from invokeai.app.services.graph import GraphExecutionState, LibraryGraph
+    from invokeai.app.services.images import ImageServiceABC
+    from invokeai.app.services.invocation_cache.invocation_cache_base import InvocationCacheBase
+    from invokeai.app.services.invocation_queue import InvocationQueueABC
+    from invokeai.app.services.invocation_stats import InvocationStatsServiceBase
+    from invokeai.app.services.invoker import InvocationProcessorABC
+    from invokeai.app.services.item_storage import ItemStorageABC
+    from invokeai.app.services.latent_storage import LatentsStorageBase
+    from invokeai.app.services.model_manager_service import ModelManagerServiceBase
+    from invokeai.app.services.session_processor.session_processor_base import SessionProcessorBase
+    from invokeai.app.services.session_queue.session_queue_base import SessionQueueBase
+
+
+class InvocationServices:
+    """Services that can be used by invocations"""
+
+    # TODO: Just forward-declared everything due to circular dependencies. Fix structure.
+    board_images: "BoardImagesServiceABC"
+    boards: "BoardServiceABC"
+    configuration: "InvokeAIAppConfig"
+    events: "EventServiceBase"
+    graph_execution_manager: "ItemStorageABC[GraphExecutionState]"
+    graph_library: "ItemStorageABC[LibraryGraph]"
+    images: "ImageServiceABC"
+    latents: "LatentsStorageBase"
+    logger: "Logger"
+    model_manager: "ModelManagerServiceBase"
+    processor: "InvocationProcessorABC"
+    performance_statistics: "InvocationStatsServiceBase"
+    queue: "InvocationQueueABC"
+    session_queue: "SessionQueueBase"
+    session_processor: "SessionProcessorBase"
+    invocation_cache: "InvocationCacheBase"
+
+    def __init__(
+        self,
+        board_images: "BoardImagesServiceABC",
+        boards: "BoardServiceABC",
+        configuration: "InvokeAIAppConfig",
+        events: "EventServiceBase",
+        graph_execution_manager: "ItemStorageABC[GraphExecutionState]",
+        graph_library: "ItemStorageABC[LibraryGraph]",
+        images: "ImageServiceABC",
+        latents: "LatentsStorageBase",
+        logger: "Logger",
+        model_manager: "ModelManagerServiceBase",
+        processor: "InvocationProcessorABC",
+        performance_statistics: "InvocationStatsServiceBase",
+        queue: "InvocationQueueABC",
+        session_queue: "SessionQueueBase",
+        session_processor: "SessionProcessorBase",
+        invocation_cache: "InvocationCacheBase",
+    ):
+        self.board_images = board_images
+        self.boards = boards
+        self.configuration = configuration
+        self.events = events
+        self.graph_execution_manager = graph_execution_manager
+        self.graph_library = graph_library
+        self.images = images
+        self.latents = latents
+        self.logger = logger
+        self.model_manager = model_manager
+        self.processor = processor
+        self.performance_statistics = performance_statistics
+        self.queue = queue
+        self.session_queue = session_queue
+        self.session_processor = session_processor
+        self.invocation_cache = invocation_cache
