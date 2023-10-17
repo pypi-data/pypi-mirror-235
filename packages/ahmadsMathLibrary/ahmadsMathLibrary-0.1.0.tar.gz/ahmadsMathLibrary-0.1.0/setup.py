ae7d0956f090b6ddd209e2f4c858b95c121324ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:13 +0000

    Add package ahmadsMathLibrary/0.1.0/ahmadsMathLibrary-0.1.0.tar.gz

---
+++ packages/ahmadsMathLibrary/ahmadsMathLibrary-0.1.0.tar.gz/ahmadsMathLibrary-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ahmadsMathLibrary",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.21.0",   # specify your dependencies here
+    ],
+)
