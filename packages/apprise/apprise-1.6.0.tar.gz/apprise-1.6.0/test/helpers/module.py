54f7bdf90563325f44f9c2a787f046e351a07343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:15:24 +0000

    Add package apprise/1.6.0/apprise-1.6.0.tar.gz

---
+++ packages/apprise/apprise-1.6.0.tar.gz/apprise-1.6.0/test/helpers/module.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# BSD 2-Clause License
+#
+# Apprise - Push Notification Library.
+# Copyright (c) 2023, Chris Caron <lead2gold@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from importlib import import_module, reload
+import sys
+
+
+def reload_plugin(name, replace_in=None):
+    """
+    Reload built-in plugin module, e.g. `NotifyGnome`.
+
+    Reloading plugin modules is needed when testing module-level code of
+    notification plugins.
+
+    See also https://stackoverflow.com/questions/31363311.
+    """
+
+    module_name = f"apprise.plugins.{name}"
+
+    reload(sys.modules['apprise.common'])
+    reload(sys.modules['apprise.attachment'])
+    reload(sys.modules['apprise.config'])
+    if module_name in sys.modules:
+        reload(sys.modules[module_name])
+    reload(sys.modules['apprise.plugins'])
+    reload(sys.modules['apprise.Apprise'])
+    reload(sys.modules['apprise.utils'])
+    reload(sys.modules['apprise'])
+
+    # Fix reference to new plugin class in given module.
+    # Needed for updating the module-level import reference like
+    # `from apprise.plugins.NotifyMacOSX import NotifyMacOSX`.
+    if replace_in is not None:
+        mod = import_module(module_name)
+        setattr(replace_in, name, getattr(mod, name))
