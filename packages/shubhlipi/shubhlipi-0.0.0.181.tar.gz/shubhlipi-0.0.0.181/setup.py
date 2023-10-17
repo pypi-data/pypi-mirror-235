3dfd51acf57b3596fa8227968b2ec3e4ea526143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:07:49 +0000

    Add package shubhlipi/0.0.0.181/shubhlipi-0.0.0.181.tar.gz

---
+++ packages/shubhlipi/shubhlipi-0.0.0.181.tar.gz/shubhlipi-0.0.0.181/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="shubhlipi",
+    version="0.0.0.181",
+    packages=["shubhlipi"],
+    install_requires=["requests", "pyperclip"],
+    python_requires=">=3.6",
+)
