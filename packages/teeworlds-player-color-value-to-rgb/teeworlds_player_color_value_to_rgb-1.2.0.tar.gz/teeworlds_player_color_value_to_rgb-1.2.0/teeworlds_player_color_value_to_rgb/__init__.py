799a384b7b9ee077bad13d2fb9b6118fc6e73b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 15:06:36 +0000

    Add package teeworlds-player-color-value-to-rgb/1.2.0/teeworlds_player_color_value_to_rgb-1.2.0.tar.gz

---
+++ packages/teeworlds-player-color-value-to-rgb/teeworlds_player_color_value_to_rgb-1.2.0.tar.gz/teeworlds_player_color_value_to_rgb-1.2.0/teeworlds_player_color_value_to_rgb/__init__.py
@@ -0,0 +1,44 @@
+# !/usr/bin/env python
+# coding: utf-8
+import math
+import colorsys
+
+
+def value_to_rgb(value: int) -> [int, int, int]:
+    def hsl_to_rgb(h, s, l):
+        # 将h, s, l的值范围映射到0到1之间
+        h /= 360.0
+        s /= 100.0
+        l /= 100.0
+
+        # 调用colorsys模块函数进行转换
+        r, g, b = colorsys.hls_to_rgb(h, l, s)
+
+        # 将0到1之间的浮点数转换为0到255之间的整数
+        r = int(round(r * 255))
+        g = int(round(g * 255))
+        b = int(round(b * 255))
+
+        return r, g, b
+
+    H = round(math.floor(value / 65536) * (360 / 256))
+    L = round(math.floor(value % 256) * (50 / 256) + 50)
+    S = round(math.floor((value / 256) % 256) * (100 / 256))
+
+    if L == 0:
+        L = 50
+
+    rgb = hsl_to_rgb(H, S, L)
+
+    return rgb
+
+
+def value_to_hsl(value: int) -> [int, int, int]:
+    H = round(math.floor(value / 65536) * (360 / 256))
+    L = round(math.floor(value % 256) * (50 / 256) + 50)
+    S = round(math.floor((value / 256) % 256) * (100 / 256))
+
+    if L == 0:
+        L = 50
+
+    return H, S, L
