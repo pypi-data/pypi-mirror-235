271b43834de95aa88493d969deffcbfac36a304e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:10:45 +0000

    Add package conntextual/1.2.11/conntextual-1.2.11.tar.gz

---
+++ packages/conntextual/conntextual-1.2.11.tar.gz/conntextual-1.2.11/conntextual/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.3
+# hash=57ef662d59189f685bc4e6370ff205bb
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A network-application TUI using textual."
+PKG_NAME = "conntextual"
+VERSION = "1.2.11"
