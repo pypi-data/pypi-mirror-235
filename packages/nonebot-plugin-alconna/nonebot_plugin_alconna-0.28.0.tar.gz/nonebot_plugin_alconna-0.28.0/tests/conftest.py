0af5a371745ffa299bbf1166cace15b6498cf169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:07:31 +0000

    Add package nonebot-plugin-alconna/0.28.0/nonebot_plugin_alconna-0.28.0.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.28.0.tar.gz/nonebot_plugin_alconna-0.28.0/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+import nonebot
+from nonebug import NONEBOT_INIT_KWARGS
+
+# 导入适配器
+from nonebot.adapters.discord import Adapter as DiscordAdapter
+from nonebot.adapters.onebot.v11 import Adapter as Onebot11Adapter
+
+
+def pytest_configure(config: pytest.Config):
+    config.stash[NONEBOT_INIT_KWARGS] = {
+        "driver": "~fastapi+~httpx+~websockets",
+        "log_level": "DEBUG",
+    }
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_bot():
+    # 加载适配器
+    driver = nonebot.get_driver()
+    driver.register_adapter(DiscordAdapter)
+    driver.register_adapter(Onebot11Adapter)
+
+    nonebot.require("nonebot_plugin_alconna")
+    return None
