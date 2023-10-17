8251e5f6a37438d9feda61dbc14d980f7704cb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:15:28 +0000

    Add package notebridge/0.5.2/notebridge-0.5.2.tar.gz

---
+++ packages/notebridge/notebridge-0.5.2.tar.gz/notebridge-0.5.2/notebridge/hello_page.py
@@ -0,0 +1,34 @@
+hello_page_html = '''
+<html>
+    <head>
+        <style>
+            body {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                justify-content: flex-start;
+                padding: 26px;
+                row-gap: 6px;
+            }
+            h1 {
+                font-size: 42px;
+                margin-top: 0;
+                margin-bottom: 0;
+            }
+            p {
+                font-family: -apple-system, BlinkMacSystemFont,
+                             "Segoe UI", Roboto, "Helvetica Neue", Arial,
+                             sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+                             "Segoe UI Symbol";
+                font-size: 20px;
+                margin-top: 0;
+                margin-bottom: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>🚀 Haha!</h1>
+        <p>You need to send a POST request to this endpoint to interact with agent. — Notebridge</p>
+    </body>
+</html>
+'''
