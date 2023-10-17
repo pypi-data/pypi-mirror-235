c515afceb7533879d303e899271343a6f3509f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:12:31 +0000

    Add package PromptManager/0.0.18/PromptManager-0.0.18.tar.gz

---
+++ packages/PromptManager/PromptManager-0.0.18.tar.gz/PromptManager-0.0.18/promptmanager/PromptManager/settings/prod.py
@@ -0,0 +1,36 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "/data/db/db.sqlite3",
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            # 'filename': os.path.join(BASE_DIR, "prompt.log"),
+            'filename': '/var/log/promptmanager/backend/prompt.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'pm_log':{
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
+}
