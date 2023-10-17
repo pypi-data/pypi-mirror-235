c1672c965c3d1df9c7caa2bcdc1cbc506a86167f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:15:35 +0000

    Add package scrapy-selenium-mm/0.0.9/scrapy-selenium-mm-0.0.9.tar.gz

---
+++ packages/scrapy-selenium-mm/scrapy-selenium-mm-0.0.9.tar.gz/scrapy-selenium-mm-0.0.9/setup.py
@@ -0,0 +1,28 @@
+"""This module contains the packaging routine for the pybook package"""
+
+from setuptools import setup, find_packages
+import pathlib
+import pkg_resources
+
+def get_requirements(source):
+    """Get the requirements from the given ``source``
+
+    Parameters
+    ----------
+    source: str
+        The filename containing the requirements
+
+    """
+    with pathlib.Path(source).open() as requirements_txt:
+        install_req = [
+            str(requirement)
+            for requirement
+            in pkg_resources.parse_requirements(requirements_txt)
+        ]
+
+    return install_req
+
+setup(
+    packages=find_packages(),
+    install_requires=get_requirements('requirements/requirements.txt')
+)
