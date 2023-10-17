0084d5216f7ea6a548e94e2881141b5a05cfba92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:07:32 +0000

    Add package pysimmods/0.10.1/pysimmods-0.10.1.tar.gz

---
+++ packages/pysimmods/pysimmods-0.10.1.tar.gz/pysimmods-0.10.1/src/pysimmods/generator/wind_turbine_system_sim/presets.py
@@ -0,0 +1,26 @@
+from pysimmods.generator.turbinesim.presets import turbine_preset
+
+
+def wind_presets(
+    pn_max_kw,
+    cos_phi=0.9,
+    q_control="prioritize_p",
+    inverter_mode="inductive",
+    **kwargs
+):
+    wparams, winit = turbine_preset(pn_max_kw, **kwargs)
+
+    params = {
+        "wind_turbine": wparams,
+        "inverter": {
+            "sn_kva": pn_max_kw / cos_phi,
+            "q_control": q_control,
+            "cos_phi": cos_phi,
+            "inverter_mode": inverter_mode,
+        },
+        "sign_convention": "active",
+    }
+
+    inits = {"wind_turbine": winit, "inverter": None}
+
+    return params, inits
