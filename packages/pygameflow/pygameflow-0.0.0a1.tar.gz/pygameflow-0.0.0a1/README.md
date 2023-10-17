cea978aafb8a8640e248e95582f4810088708a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:15:27 +0000

    Add package pygameflow/0.0.0a1/pygameflow-0.0.0a1.tar.gz

---
+++ packages/pygameflow/pygameflow-0.0.0a1.tar.gz/pygameflow-0.0.0a1/README.md
@@ -0,0 +1,12 @@
+# PyGameFlow
+
+Welcome to PyGameFlow Version 0.0.0!
+
+> [!NOTE]  
+> It's important to note that this project is currently in development. Please stay tuned for forthcoming updates and enhancements.
+
+
+## Install
+```sh
+pip install pygameflow
+```
---
+++ packages/pygameflow/pygameflow-0.0.0a1.tar.gz/pygameflow-0.0.0a1/src/pygameflow/__init__.py
@@ -0,0 +1,14 @@
+"""
+PyGameFlow: An Object-Oriented and Modern Pygame Wrapper with Developer-Friendly Features
+"""
+
+# Work in Progress (WIP)
+print(
+    """
+Welcome to PyGameFlow Version 0.0.0!
+It's important to note that this project is currently in development.
+Please stay tuned for forthcoming updates and enhancements.
+For inquiries or to contribute, please contact Ificiana at ificiana@gmail.com
+or visit the GitHub repository at github.com/ificiana/pygameflow.
+"""
+)
