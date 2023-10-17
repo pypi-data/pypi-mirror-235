8f86a5fcd73e3438f4927b4fc3132efe562bd0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 01:27:53 +0000

    Add package accrete/0.0.8/accrete-0.0.8-py3-none-any.whl

---
+++ packages/accrete/accrete-0.0.8-py3-none-any.whl/accrete/contrib/user_registration/config.py
@@ -0,0 +1,37 @@
+import logging
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import gettext_lazy as _
+
+_logger = logging.getLogger(__name__)
+
+ACCRETE_USER_REGISTRATION_MAIL_FROM_NAME = getattr(
+    settings, 'ACCRETE_USER_REGISTRATION_MAIL_FROM_NAME',
+    False
+)
+
+ACCRETE_USER_REGISTRATION_TEMPLATE_NAME = getattr(
+    settings, 'ACCRETE_USER_REGISTRATION_TEMPLATE_NAME',
+    'user_registration/mail_templates/confirmation_mail.html'
+)
+
+ACCRETE_USER_REGISTRATION_MAIL_SUBJECT = getattr(
+    settings, 'ACCRETE_USER_REGISTRATION_MAIL_SUBJECT',
+    _('Registration Confirmation')
+)
+
+ACCRETE_USER_REGISTRATION_ALLOWED = getattr(
+    settings, 'ACCRETE_USER_REGISTRATION_ALLOWED', True
+)
+
+if not ACCRETE_USER_REGISTRATION_MAIL_FROM_NAME:
+    _logger.warning(
+        'Setting "USER_REGISTRATION_MAIL_FROM_NAME" missing.\n'
+        'User Registration won\'t work. Set it or remove '
+        'the app "user_registration" from INSTALLED_APPS and your urls.'
+    )
+    # raise ImproperlyConfigured(
+    #     'Setting "USER_REGISTRATION_MAIL_FROM_NAME" missing.\n'
+    #     'User Registration won\'t work. Set it or remove '
+    #     'the app "user_registration" from INSTALLED_APPS and your urls.'
+    # )
