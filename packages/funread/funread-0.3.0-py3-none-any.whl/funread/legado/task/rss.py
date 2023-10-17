334393dfbd8b0af3fcb8b53bbb5e12c932d1057f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:30 +0000

    Add package funread/0.3.0/funread-0.3.0-py3-none-any.whl

---
+++ packages/funread/funread-0.3.0-py3-none-any.whl/funread/legado/task/rss.py
@@ -0,0 +1,31 @@
+import os.path
+
+from fundrive import GithubDrive
+from funtask import Task
+
+
+class UpdateRssTask(Task):
+    def __init__(self):
+        self.drive = GithubDrive()
+        self.drive.login("farfarfun/funread-cache")
+        super(UpdateRssTask, self).__init__()
+
+    def book(self, dir_path="funread/legado/snapshot/lasted/book"):
+        files = self.drive.get_file_list(dir_path)
+        files = sorted(files, key=lambda x: x["time"])
+        return {
+            "title": os.path.basename(dir_path),
+            "time": files[0]["time"],
+            "pic": "https://gitee.com/alanskycn/yuedu/raw/master/JS/icon.jpg",
+            "url": f"https://farfarfun.github.io/funread-cache/{dir_path}/index.html",
+            "content": "this is content",
+        }
+
+    def run(self, dir_path="funread/legado/snapshot/lasted"):
+        data1 = {
+            "name": "test",
+            "next": "https://json.extendsclass.com/bin/b62da68f7d4d",
+            "list": [],
+        }
+        data1["list"].append(self.book(f"{dir_path}/book"))
+        self.drive.upload_file(git_path=f"{dir_path}/source.json", content=data1)
