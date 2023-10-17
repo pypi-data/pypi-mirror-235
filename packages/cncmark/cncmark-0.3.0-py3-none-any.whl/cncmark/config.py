300c19cee0f6d6c3111378473f4b120e80006b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:52 +0000

    Add package cncmark/0.3.0/cncmark-0.3.0-py3-none-any.whl

---
+++ packages/cncmark/cncmark-0.3.0-py3-none-any.whl/cncmark/config.py
@@ -0,0 +1,24 @@
+import os
+
+# For github actions
+CI_MYSQL_CONFIG = dict(
+    host="127.0.0.1",
+    port=3306,
+    user="root",
+    password="root",
+)
+
+
+MYSQL_CONFIG = dict(
+    host="192.168.122.76",
+    port=3306,
+    user="root",
+    password="root",
+)
+
+if os.environ.get("CI"):
+    MYSQL_CONFIG = CI_MYSQL_CONFIG
+
+
+MEARURE_FEEDRATE = 300
+MOVE_FEEDRATE = 600
