783a718a26e04e0ea224640bd43451458024c2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 01:29:08 +0000

    Add package argrelay/0.4.0.dev7/argrelay-0.4.0.dev7.tar.gz

---
+++ packages/argrelay/argrelay-0.4.0.dev7.tar.gz/argrelay-0.4.0.dev7/src/argrelay/custom_integ/DemoInterp.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from argrelay.plugin_interp.FuncArgsInterp import FuncArgsInterp
+
+
+class DemoInterp(FuncArgsInterp):
+    """
+    `DemoInterp` is a configured use case of :class:`FuncArgsInterp` to demo auto-completion.
+
+    See FS_55_57_45_04.
+    """
+
+    pass
