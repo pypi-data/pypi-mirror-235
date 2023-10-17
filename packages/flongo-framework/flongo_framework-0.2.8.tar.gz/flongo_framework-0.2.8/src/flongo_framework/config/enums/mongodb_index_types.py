1a063448e5afb45be482487bc5ad2f295df3216f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:07:39 +0000

    Add package flongo-framework/0.2.8/flongo_framework-0.2.8.tar.gz

---
+++ packages/flongo-framework/flongo_framework-0.2.8.tar.gz/flongo_framework-0.2.8/src/flongo_framework/config/enums/mongodb_index_types.py
@@ -0,0 +1,8 @@
+from src.flongo_framework.config.enums.base.base_str_enum import BaseStrEnum
+
+class MONGODB_INDEX_TYPES(BaseStrEnum):
+    """ Types of indices supported for MongoDB """
+
+    TEXT = "text"
+    COMPOUND = "compound"
+    STANDARD = "standard"
