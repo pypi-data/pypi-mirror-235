669520274510d3ef09abf59615658b56f49c2948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:06:58 +0000

    Add package dstwo/1.0.0/dstwo-1.0.0-py3-none-any.whl

---
+++ packages/dstwo/dstwo-1.0.0-py3-none-any.whl/data_struct/basic.py
@@ -0,0 +1,58 @@
+class Stack:
+    def __init__(self):
+        self.items = []  # 尾部为栈顶
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def peek(self):
+        return self.items[-1]
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+
+class Deque:
+    def __init_(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def add_front(self, item):
+        self.items.insert(0, item)
+
+    def add_rear(self, item):
+        self.items.append(item)
+
+    def remove_front(self):
+        return self.item.pop(0)
+
+    def remove_rear(self):
+        return self.item.pop()
+
+    def size(self):
+        return len(self.items)
