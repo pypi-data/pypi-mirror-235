6744960b8430c1f58ca0e1fcf35dd547ddf519f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 15:06:41 +0000

    Add package aminfo/0.1.1/aminfo-0.1.1.tar.gz

---
+++ packages/aminfo/aminfo-0.1.1.tar.gz/aminfo-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+from pybind11.setup_helpers import Pybind11Extension
+
+ext_modules = [
+    Pybind11Extension(
+        'aminfo',
+        ['src/aminfo.cpp'],
+    ),
+]
+
+setup(
+    name='aminfo',
+    version='0.1.1',
+    ext_modules=ext_modules,
+    setup_requires=["pybind11>=2.11.0"],
+    license='MIT'
+)
