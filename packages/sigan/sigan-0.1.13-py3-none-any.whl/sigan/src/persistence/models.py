80b8557fd74f82a75b87cf202fcc65f9cab9a6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:08:00 +0000

    Add package sigan/0.1.13/sigan-0.1.13-py3-none-any.whl

---
+++ packages/sigan/sigan-0.1.13-py3-none-any.whl/sigan/src/persistence/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import TIMESTAMP, Boolean, DateTime
+
+import sys
+sys.path.append('.')
+
+from sigan.src.infrastructure.database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+    
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    team_id = Column(String(100), nullable=False)
+    channel_id = Column(String(100), nullable=False)
+    channel_name = Column(String(100), nullable=False)
+    access_token = Column(String(100), nullable=True)
+
+
+class Alarm(Base):
+    __tablename__ = "alarm"
+    
+    alarm_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("user.user_id"), nullable=False)
+    content = Column(String(300), nullable=False)
+    deadline = Column(String(100), nullable=True)
+    alarm_date = Column(String(100), nullable=False)
+    interval = Column(String(100), nullable=True)
+    scheduled_message_id = Column(String(100), nullable=True)
+    slack_channel_name = Column(String(100), nullable=True)
+    slack_channel_id = Column(String(100), nullable=True)
+    confirm_alarm_date = Column(DateTime, nullable=True)
+    sub_scheduled_message_id = Column(String(100), nullable=True)
