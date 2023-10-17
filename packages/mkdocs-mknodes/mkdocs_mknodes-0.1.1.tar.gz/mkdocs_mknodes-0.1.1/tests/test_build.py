d5f0fcf5457c020def10fe31b7b8afcfc2d9191f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:13:42 +0000

    Add package mkdocs-mknodes/0.1.1/mkdocs_mknodes-0.1.1.tar.gz

---
+++ packages/mkdocs-mknodes/mkdocs_mknodes-0.1.1.tar.gz/mkdocs_mknodes-0.1.1/tests/test_build.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import pathlib
+import tempfile
+
+from mkdocs_mknodes import buildcollector, mkdocsconfig
+from mkdocs_mknodes.backends import markdownbackend, mkdocsbackend
+
+
+_dir = tempfile.TemporaryDirectory(prefix="mknodes_")
+build_folder = pathlib.Path(_dir.name)
+
+
+def test_build(project):
+    project.build()
+    cfg = mkdocsconfig.Config()
+    cfg.update_from_context(project.context)
+    mkdocs_backend = mkdocsbackend.MkDocsBackend(
+        config=cfg,
+        directory=build_folder,
+    )
+
+    markdown_backend = markdownbackend.MarkdownBackend(
+        directory=pathlib.Path(cfg.site_dir) / "src",
+        extension=".original",
+    )
+    collector = buildcollector.BuildCollector(
+        backends=[mkdocs_backend, markdown_backend],
+        show_page_info=True,
+    )
+    assert project._root
+    build_info = collector.collect(project._root, project.theme)
+    assert build_info
