21a5ab5c32e00b89c829ca3c652a7d339cab2605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:08:27 +0000

    Add package fastkml/1.0a6/fastkml-1.0a6.tar.gz

---
+++ packages/fastkml/fastkml-1.0a6.tar.gz/fastkml-1.0a6/fastkml/atom.py
@@ -0,0 +1,266 @@
+# Copyright (C) 2012 - 2021  Christian Ledermann
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+"""
+KML 2.2 supports new elements for including data about the author and related
+website in your KML file. This information is displayed in geo search results,
+both in Earth browsers such as Google Earth, and in other applications such as
+Google Maps. The ascription elements used in KML are as follows:
+
+atom:author element - parent element for atom:name
+atom:name element - the name of the author
+atom:link element - contains the href attribute
+href attribute - URL of the web page containing the KML/KMZ file
+
+These elements are defined in the Atom Syndication Format. The complete
+specification is found at http://atompub.org.
+
+This library only implements a subset of Atom that is useful with KML
+"""
+
+import logging
+import re
+from typing import Optional
+from typing import Tuple
+
+from fastkml.base import _XMLObject
+from fastkml.config import ATOMNS as NS
+from fastkml.enums import Verbosity
+from fastkml.helpers import o_from_attr
+from fastkml.helpers import o_from_subelement_text
+from fastkml.helpers import o_int_from_attr
+from fastkml.helpers import o_to_attr
+from fastkml.helpers import o_to_subelement_text
+from fastkml.types import Element
+from fastkml.types import KmlObjectMap
+
+logger = logging.getLogger(__name__)
+regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+email_match = re.compile(regex).match
+
+
+def check_email(email: str) -> bool:
+    """Check if the email address is valid."""
+    return bool(email_match(email))
+
+
+class Link(_XMLObject):
+    """
+    Identifies a related Web page. The type of relation is defined by
+    the rel attribute. A feed is limited to one alternate per type and
+    hreflang.
+    <link> is patterned after html's link element. It has one required
+    attribute, href, and five optional attributes: rel, type, hreflang,
+    title, and length.
+    """
+
+    __name__ = "link"
+
+    kml_object_mapping: Tuple[KmlObjectMap, ...] = (
+        {
+            "kml_attr": "href",
+            "obj_attr": "href",
+            "from_kml": o_from_attr,
+            "to_kml": o_to_attr,
+            "required": True,
+            "validator": None,
+        },
+        {
+            "kml_attr": "rel",
+            "obj_attr": "rel",
+            "from_kml": o_from_attr,
+            "to_kml": o_to_attr,
+            "required": False,
+            "validator": None,
+        },
+        {
+            "kml_attr": "type",
+            "obj_attr": "type",
+            "from_kml": o_from_attr,
+            "to_kml": o_to_attr,
+            "required": False,
+            "validator": None,
+        },
+        {
+            "kml_attr": "hreflang",
+            "obj_attr": "hreflang",
+            "from_kml": o_from_attr,
+            "to_kml": o_to_attr,
+            "required": False,
+            "validator": None,
+        },
+        {
+            "kml_attr": "title",
+            "obj_attr": "title",
+            "from_kml": o_from_attr,
+            "to_kml": o_to_attr,
+            "required": False,
+            "validator": None,
+        },
+        {
+            "kml_attr": "length",
+            "obj_attr": "length",
+            "from_kml": o_int_from_attr,
+            "to_kml": o_to_attr,
+            "required": False,
+            "validator": None,
+        },
+    )
+
+    href = None
+    # href is the URI of the referenced resource
+
+    rel = None
+    # rel contains a single link relationship type.
+    # It can be a full URI, or one of the following predefined values
+    # (default=alternate):
+    # alternate: an alternate representation
+    # enclosure: a related resource which is potentially large in size
+    # and might require special handling, for example an audio or video
+    # recording.
+    # related: an document related to the entry or feed.
+    # self: the feed itself.
+    # via: the source of the information provided in the entry.
+
+    type = None
+    # indicates the media type of the resource
+
+    hreflang = None
+    # indicates the language of the referenced resource
+
+    title = None
+    # human readable information about the link
+
+    length = None
+    # the length of the resource, in bytes
+
+    def __init__(
+        self,
+        ns: Optional[str] = None,
+        href: Optional[str] = None,
+        rel: Optional[str] = None,
+        type: Optional[str] = None,
+        hreflang: Optional[str] = None,
+        title: Optional[str] = None,
+        length: Optional[int] = None,
+    ) -> None:
+        self.ns: str = NS if ns is None else ns
+        self.href = href
+        self.rel = rel
+        self.type = type
+        self.hreflang = hreflang
+        self.title = title
+        self.length = length
+
+    def from_element(self, element: Element) -> None:
+        super().from_element(element)
+
+    def etree_element(
+        self,
+        precision: Optional[int] = None,
+        verbosity: Verbosity = Verbosity.normal,
+    ) -> Element:
+        return super().etree_element(precision=precision, verbosity=verbosity)
+
+
+class _Person(_XMLObject):
+    """
+    <author> and <contributor> describe a person, corporation, or similar
+    entity. It has one required element, name, and two optional elements:
+    uri, email.
+    """
+
+    __name__ = ""
+    kml_object_mapping: Tuple[KmlObjectMap, ...] = (
+        {
+            "kml_attr": "name",
+            "obj_attr": "name",
+            "from_kml": o_from_subelement_text,
+            "to_kml": o_to_subelement_text,
+            "required": True,
+            "validator": None,
+        },
+        {
+            "kml_attr": "uri",
+            "obj_attr": "uri",
+            "from_kml": o_from_subelement_text,
+            "to_kml": o_to_subelement_text,
+            "required": False,
+            "validator": None,
+        },
+        {
+            "kml_attr": "email",
+            "obj_attr": "email",
+            "from_kml": o_from_subelement_text,
+            "to_kml": o_to_subelement_text,
+            "required": False,
+            "validator": check_email,
+        },
+    )
+
+    name: Optional[str] = None
+    # conveys a human-readable name for the person.
+
+    uri: Optional[str] = None
+    # contains a home page for the person.
+
+    email: Optional[str] = None
+    # contains an email address for the person.
+
+    def __init__(
+        self,
+        ns: Optional[str] = None,
+        name: Optional[str] = None,
+        uri: Optional[str] = None,
+        email: Optional[str] = None,
+    ) -> None:
+        self.ns: str = NS if ns is None else ns
+        self.name = name
+        self.uri = uri
+        self.email = email
+
+    def etree_element(
+        self,
+        precision: Optional[int] = None,
+        verbosity: Verbosity = Verbosity.normal,
+    ) -> Element:
+        return super().etree_element(precision=precision, verbosity=verbosity)
+
+    def from_element(self, element: Element) -> None:
+        super().from_element(element)
+
+
+class Author(_Person):
+    """
+    Return the names one author of the feed/entry.
+
+    A feed/entry may have multiple authors.
+    """
+
+    __name__ = "author"
+
+
+class Contributor(_Person):
+    """
+    Return the names one contributor to the feed/entry.
+
+    A feed/entry may have multiple contributor elements.
+    """
+
+    __name__ = "contributor"
+
+
+__all__ = ["Author", "Contributor", "Link"]
---
+++ packages/fastkml/fastkml-1.0a6.tar.gz/fastkml-1.0a6/fastkml/data.py
@@ -0,0 +1,397 @@
+# Copyright (C) 2022  Christian Ledermann
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+"""Add Custom Data"""
+import logging
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Union
+from typing import overload
+
+from typing_extensions import TypedDict
+
+import fastkml.config as config
+from fastkml.base import _BaseObject
+from fastkml.base import _XMLObject
+from fastkml.enums import Verbosity
+from fastkml.types import Element
+
+logger = logging.getLogger(__name__)
+
+
+class SimpleField(TypedDict):
+    name: str
+    type: str
+    displayName: Optional[str]  # noqa: N815
+
+
+SimpleFields = List[Dict[str, str]]
+SimpleFieldsListInput = List[Union[Dict[str, str], List[Dict[str, str]]]]
+SimpleFieldsTupleInput = Tuple[Union[Dict[str, str], Tuple[Dict[str, str]]]]
+SimpleFieldsDictInput = Dict[str, str]
+SimpleFieldsInput = Optional[
+    Union[SimpleFieldsListInput, SimpleFieldsTupleInput, SimpleFieldsDictInput]
+]
+SimpleFieldsOutput = Tuple[SimpleField, ...]
+
+
+class Schema(_BaseObject):
+    """
+    Specifies a custom KML schema that is used to add custom data to
+    KML Features.
+    The "id" attribute is required and must be unique within the KML file.
+    <Schema> is always a child of <Document>.
+    """
+
+    __name__ = "Schema"
+
+    def __init__(
+        self,
+        ns: Optional[str] = None,
+        id: Optional[str] = None,
+        target_id: Optional[str] = None,
+        name: Optional[str] = None,
+        fields: SimpleFieldsInput = None,
+    ) -> None:
+        if id is None:
+            raise ValueError("Id is required for schema")
+        super().__init__(ns=ns, id=id, target_id=target_id)
+        self.name = name
+        self._simple_fields: SimpleFields = []
+        self.simple_fields = fields  # type: ignore[assignment]
+
+    @property
+    def simple_fields(self) -> SimpleFieldsOutput:
+        return tuple(
+            SimpleField(
+                type=simple_field.get("type", ""),
+                name=simple_field.get("name", ""),
+                displayName=simple_field.get("displayName") or None,
+            )
+            for simple_field in self._simple_fields
+            if simple_field.get("type") and simple_field.get("name")
+        )
+
+    @simple_fields.setter
+    @overload
+    def simple_fields(self, fields: SimpleFieldsListInput) -> None:
+        ...
+
+    @simple_fields.setter
+    @overload
+    def simple_fields(self, fields: SimpleFieldsTupleInput) -> None:
+        ...
+
+    @simple_fields.setter
+    @overload
+    def simple_fields(self, fields: SimpleFieldsDictInput) -> None:
+        ...
+
+    @simple_fields.setter
+    def simple_fields(self, fields: SimpleFieldsInput) -> None:
+        if isinstance(fields, dict):
+            self.append(**fields)
+        elif isinstance(fields, (list, tuple)):
+            for field in fields:
+                if isinstance(field, (list, tuple)):
+                    self.append(*field)
+                elif isinstance(field, dict):
+                    self.append(**field)
+        elif fields is None:
+            self._simple_fields = []
+        else:
+            raise ValueError("Fields must be of type list, tuple or dict")
+
+    def append(self, type: str, name: str, display_name: Optional[str] = None) -> None:
+        """
+        append a field.
+        The declaration of the custom field, must specify both the type
+        and the name of this field.
+        If either the type or the name is omitted, the field is ignored.
+
+        The type can be one of the following:
+            string
+            int
+            uint
+            short
+            ushort
+            float
+            double
+            bool
+
+        <displayName>
+        The name, if any, to be used when the field name is displayed to
+        the Google Earth user. Use the [CDATA] element to escape standard
+        HTML markup.
+        """
+        allowed_types = [
+            "string",
+            "int",
+            "uint",
+            "short",
+            "ushort",
+            "float",
+            "double",
+            "bool",
+        ]
+        if type not in allowed_types:
+            logger.warning(
+                "%s has the type %s which is invalid. "
+                "The type must be one of "
+                "'string', 'int', 'uint', 'short', "
+                "'ushort', 'float', 'double', 'bool'",
+                name,
+                type,
+            )
+        self._simple_fields.append(
+            {"type": type, "name": name, "displayName": display_name or ""}
+        )
+
+    def from_element(self, element: Element) -> None:
+        super().from_element(element)
+        self.name = element.get("name")
+        simple_fields = element.findall(f"{self.ns}SimpleField")
+        for simple_field in simple_fields:
+            sfname = simple_field.get("name")
+            sftype = simple_field.get("type")
+            display_name = simple_field.find(f"{self.ns}displayName")
+            sfdisplay_name = display_name.text if display_name is not None else None
+            self.append(sftype, sfname, sfdisplay_name)
+
+    def etree_element(
+        self,
+        precision: Optional[int] = None,
+        verbosity: Verbosity = Verbosity.normal,
+    ) -> Element:
+        element = super().etree_element(precision=precision, verbosity=verbosity)
+        if self.name:
+            element.set("name", self.name)
+        for simple_field in self.simple_fields:
+            sf = config.etree.SubElement(  # type: ignore[attr-defined]
+                element, f"{self.ns}SimpleField"
+            )
+            sf.set("type", simple_field["type"])
+            sf.set("name", simple_field["name"])
+            if simple_field.get("displayName"):
+                dn = config.etree.SubElement(  # type: ignore[attr-defined]
+                    sf, f"{self.ns}displayName"
+                )
+                dn.text = simple_field["displayName"]
+        return element
+
+
+class Data(_XMLObject):
+    """Represents an untyped name/value pair with optional display name."""
+
+    __name__ = "Data"
+
+    def __init__(
+        self,
+        ns: Optional[str] = None,
+        name: Optional[str] = None,
+        value: Optional[str] = None,
+        display_name: Optional[str] = None,
+    ) -> None:
+        super().__init__(ns)
+
+        self.name = name
+        self.value = value
+        self.display_name = display_name
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"ns='{self.ns}',"
+            f"name='{self.name}', value='{self.value}'"
+            f"display_name='{self.display_name}')"
+        )
+
+    def etree_element(
+        self,
+        precision: Optional[int] = None,
+        verbosity: Verbosity = Verbosity.normal,
+    ) -> Element:
+        element = super().etree_element(precision=precision, verbosity=verbosity)
+        element.set("name", self.name or "")
+        value = config.etree.SubElement(  # type: ignore[attr-defined]
+            element, f"{self.ns}value"
+        )
+        value.text = self.value
+        if self.display_name:
+            display_name = config.etree.SubElement(  # type: ignore[attr-defined]
+                element, f"{self.ns}displayName"
+            )
+            display_name.text = self.display_name
+        return element
+
+    def from_element(self, element: Element) -> None:
+        super().from_element(element)
+        self.name = element.get("name")
+        tmp_value = element.find(f"{self.ns}value")
+        if tmp_value is not None:
+            self.value = tmp_value.text
+        display_name = element.find(f"{self.ns}displayName")
+        if display_name is not None:
+            self.display_name = display_name.text
+
+
+class ExtendedData(_XMLObject):
+    """Represents a list of untyped name/value pairs. See docs:
+
+    -> 'Adding Untyped Name/Value Pairs'
+       https://developers.google.com/kml/documentation/extendeddata
+
+    """
+
+    __name__ = "ExtendedData"
+
+    def __init__(
+        self,
+        ns: Optional[str] = None,
+        elements: Optional[List[Union[Data, "SchemaData"]]] = None,
+    ) -> None:
+        super().__init__(ns)
+        self.elements = elements or []
+
+    def etree_element(
+        self,
+        precision: Optional[int] = None,
+        verbosity: Verbosity = Verbosity.normal,
+    ) -> Element:
+        element = super().etree_element(precision=precision, verbosity=verbosity)
+        for subelement in self.elements:
+            element.append(subelement.etree_element())
+        return element
+
+    def from_element(self, element: Element) -> None:
+        super().from_element(element)
+        self.elements = []
+        untyped_data = element.findall(f"{self.ns}Data")
+        for ud in untyped_data:
+            el_data = Data(self.ns)
+            el_data.from_element(ud)
+            self.elements.append(el_data)
+        typed_data = element.findall(f"{self.ns}SchemaData")
+        for sd in typed_data:
+            el_schema_data = SchemaData(self.ns, "dummy")
+            el_schema_data.from_element(sd)
+            self.elements.append(el_schema_data)
+
+
+SchemaDataType = List[Dict[str, Union[int, str]]]
+SchemaDataListInput = List[Union[Dict[str, str], SchemaDataType]]
+SchemaDataTupleInput = Tuple[Union[Dict[str, str], Tuple[Dict[str, Union[int, str]]]]]
+SchemaDataDictInput = Dict[str, Union[int, str]]
+SchemaDataInput = Optional[
+    Union[
+        SchemaDataListInput,
+        SchemaDataTupleInput,
+        SchemaDataDictInput,
+    ]
+]
+SchemaDataOutput = Tuple[Dict[str, Union[int, str]], ...]
+
+
+class SchemaData(_XMLObject):
+    """
+    <SchemaData schemaUrl="anyURI">
+    This element is used in conjunction with <Schema> to add typed
+    custom data to a KML Feature. The Schema element (identified by the
+    schemaUrl attribute) declares the custom data type. The actual data
+    objects ("instances" of the custom data) are defined using the
+    SchemaData element.
+    The <schemaURL> can be a full URL, a reference to a Schema ID defined
+    in an external KML file, or a reference to a Schema ID defined
+    in the same KML file.
+    """
+
+    __name__ = "SchemaData"
+
+    def __init__(
+        self,
+        ns: Optional[str] = None,
+        schema_url: Optional[str] = None,
+        data: Optional[List[Dict[str, str]]] = None,
+    ) -> None:
+        super().__init__(ns)
+        if (not isinstance(schema_url, str)) or (not schema_url):
+            raise ValueError("required parameter schema_url missing")
+        self.schema_url = schema_url
+        self._data: SchemaDataType = []
+        self.data = data  # type: ignore[assignment]
+
+    @property
+    def data(self) -> SchemaDataOutput:
+        return tuple(self._data)
+
+    @data.setter
+    @overload
+    def data(self, data: SchemaDataListInput) -> None:
+        ...
+
+    @data.setter
+    @overload
+    def data(self, data: SchemaDataTupleInput) -> None:
+        ...
+
+    @data.setter
+    @overload
+    def data(self, data: SchemaDataDictInput) -> None:
+        ...
+
+    @data.setter
+    def data(self, data: SchemaDataInput) -> None:
+        if isinstance(data, (tuple, list)):
+            self._data = []
+            for d in data:
+                if isinstance(d, (tuple, list)):
+                    self.append_data(*d)
+                elif isinstance(d, dict):
+                    self.append_data(**d)
+        elif data is None:
+            self._data = []
+        else:
+            raise TypeError("data must be of type tuple or list")
+
+    def append_data(self, name: str, value: Union[int, str]) -> None:
+        if isinstance(name, str) and name:
+            self._data.append({"name": name, "value": value})
+        else:
+            raise TypeError("name must be a nonempty string")
+
+    def etree_element(
+        self,
+        precision: Optional[int] = None,
+        verbosity: Verbosity = Verbosity.normal,
+    ) -> Element:
+        element = super().etree_element(precision=precision, verbosity=verbosity)
+        element.set("schemaUrl", self.schema_url)
+        for data in self.data:
+            sd = config.etree.SubElement(  # type: ignore[attr-defined]
+                element, f"{self.ns}SimpleData"
+            )
+            sd.set("name", data["name"])
+            sd.text = data["value"]
+        return element
+
+    def from_element(self, element: Element) -> None:
+        super().from_element(element)
+        self.data = []  # type: ignore[assignment]
+        self.schema_url = element.get("schemaUrl")
+        simple_data = element.findall(f"{self.ns}SimpleData")
+        for sd in simple_data:
+            self.append_data(sd.get("name"), sd.text)
