f26be298e5d6fe55abd9277388a33de7a8dfd0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:09:07 +0000

    Add package chime-hawker/0.0.2/chime-hawker-0.0.2.tar.gz

---
+++ packages/chime-hawker/chime-hawker-0.0.2.tar.gz/chime-hawker-0.0.2/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-hawker',
+    packages=find_packages(include=['chime-hawker']),
+    version='0.0.2',
+    description='x',
+    author=
+    'x',
+)
