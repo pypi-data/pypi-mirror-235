e74a24f2c31243adc15fa581bf15d0b85dbb3823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:14:27 +0000

    Add package utemplates/0.2/utemplates-0.2.tar.gz

---
+++ packages/utemplates/utemplates-0.2.tar.gz/utemplates-0.2/UTemplates/html_specific/base.py
@@ -0,0 +1,62 @@
+from ..general_base import GeneralBaseElement
+
+
+class BaseHTMLElement(GeneralBaseElement):
+    def __init__(
+            self,
+            tag_name: str,
+            attributes: dict[str, any] = None,
+            content: any = None,
+            self_closing: bool = False,
+            declaration: bool = False
+    ) -> None:
+        self.tag_name: str = tag_name
+        self.attributes: dict[str, any] = attributes if attributes is not None else {}
+        if content is None:
+            self.content: list[any] = []
+        elif not isinstance(content, (str, bytes)) and hasattr(content, "__iter__"):
+            self.content: list[any] = content
+        else:
+            self.content: list[any] = [content]
+        self.self_closing: bool = self_closing
+        self.declaration: bool = declaration
+        if self.declaration:
+            self.self_closing: bool = True
+
+    @property
+    def _attributes(self) -> str:
+        attributes_str: str = ""
+        for key, value in self.attributes.items():
+            if value is not None:
+                if isinstance(value, bool):
+                    if value:
+                        attributes_str += f" {key}"
+                else:
+                    attributes_str += f' {key}="{value}"'
+        return attributes_str
+
+    @property
+    def _opening_tag(self) -> str:
+        if self.self_closing and not self.declaration:
+            return f"<{self.tag_name}{self._attributes}/>"
+        else:
+            return f"<{self.tag_name}{self._attributes}>"
+
+    @property
+    def _content(self) -> str:
+        if self.self_closing:
+            return ""
+        content_strs: list[str] = []
+        for item in self.content:
+            content_strs.append(str(item))
+        return "".join(content_strs)
+
+    @property
+    def _closing_tag(self) -> str:
+        if self.self_closing:
+            return ""
+        else:
+            return f"</{self.tag_name}>"
+
+    def to_string(self) -> str:
+        return f"{self._opening_tag}{self._content}{self._closing_tag}"
