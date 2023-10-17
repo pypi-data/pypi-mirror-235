69702a18336ced2a591de0f139c896bb605645d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:14:15 +0000

    Add package trongrid-extractoor/3.0.4/trongrid_extractoor-3.0.4.tar.gz

---
+++ packages/trongrid-extractoor/trongrid_extractoor-3.0.4.tar.gz/trongrid_extractoor-3.0.4/trongrid_extractoor/exceptions.py
@@ -0,0 +1,18 @@
+class IllegitimateEmptyResponseError(Exception):
+    pass
+
+
+class RateLimitExceededError(Exception):
+    pass
+
+
+class TrongridError(Exception):
+    pass
+
+
+class UnparseableResponse(Exception):
+    pass
+
+
+class InvalidAddressError(Exception):
+    pass
