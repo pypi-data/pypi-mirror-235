553787961c22f3d0f8568c96909dd2c1f6dc1789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:12:54 +0000

    Add package ENCHUFLA/0.1.6/ENCHUFLA-0.1.6-py3-none-any.whl

---
+++ packages/ENCHUFLA/ENCHUFLA-0.1.6-py3-none-any.whl/ENCHUFLA/MODULES/CLIQUE/DUOM/__init__.py
@@ -0,0 +1,22 @@
+
+
+from .ED448 import ED448
+
+def DUOM (GROUP):
+	import click
+	@GROUP.group ("DUOM")
+	def GROUP ():
+		pass
+		
+	import click
+	@GROUP.command ("")
+	def EXAMPLE ():	
+		print ("START")
+
+
+
+		return;
+		
+	ED448 (GROUP)
+
+	return;
