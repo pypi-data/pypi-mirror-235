6c2ba58a6d75c70ebeb24087e609a5c91037c542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:07:00 +0000

    Add package depyf/0.7.0/depyf-0.7.0.tar.gz

---
+++ packages/depyf/depyf-0.7.0.tar.gz/depyf-0.7.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='depyf',
+    version=open("depyf/VERSION.txt").read().strip(),
+    description='Decompile python functions, from bytecode to source code!',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/youkaichao/depyf',
+    author='Kaichao You',
+    author_email="youkaichao@gmail.com",
+    license="MIT",
+    packages=find_packages(include=["depyf"]),
+    python_requires=">=3.7",
+    install_requires=[
+        "astor",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ]
+    }
+)
