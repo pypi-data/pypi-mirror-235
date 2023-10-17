bf6e93c951972bd3d75cc65c6a844ebf8aa6977b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:09:30 +0000

    Add package fraplustreewww/1.0.0/fraplustreewww-1.0.0.tar.gz

---
+++ packages/fraplustreewww/fraplustreewww-1.0.0.tar.gz/fraplustreewww-1.0.0/printmyname/main.py
@@ -0,0 +1,2 @@
+def print_my_name(name):
+    print(f'Your name is {name}')
---
+++ packages/fraplustreewww/fraplustreewww-1.0.0.tar.gz/fraplustreewww-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fraplustreewww",
+    version='1.0.0',
+    author="Guido Xhindoli",
+    author_email="<mail@gmail.com>",
+    description='A package draws tree and fractals',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[ 'fraplustree', 'SDA', 'fra', 'tree'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
