82bcefcfffd560d1cdec7cf70f0823142314384e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:08:31 +0000

    Add package bellows/0.36.6/bellows-0.36.6-py3-none-any.whl

---
+++ packages/bellows/bellows-0.36.6-py3-none-any.whl/bellows/exception.py
@@ -0,0 +1,13 @@
+from zigpy.exceptions import APIException, ControllerException
+
+
+class EzspError(APIException):
+    pass
+
+
+class InvalidCommandError(EzspError):
+    pass
+
+
+class ControllerError(ControllerException):
+    pass
