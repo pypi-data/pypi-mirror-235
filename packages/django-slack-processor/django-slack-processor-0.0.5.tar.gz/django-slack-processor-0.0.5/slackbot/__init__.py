47e9c3962f2d095d6c0677ce12f0fbf9a5aa8d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:09:30 +0000

    Add package django-slack-processor/0.0.5/django-slack-processor-0.0.5.tar.gz

---
+++ packages/django-slack-processor/django-slack-processor-0.0.5.tar.gz/django-slack-processor-0.0.5/slackbot/__init__.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+
+__version__ = '0.0.5'
+
+# set default_app_config when using django earlier than 3.2
+try:
+    import django
+
+    if django.VERSION < (3, 2):
+        default_app_config = 'slackbot.apps.SlackbotConfig'
+except ImportError:
+    pass
+
+
+@lru_cache
+def get_user_model():
+    """
+    use settings.SLACKBOT_USER_MODEL to use your own model for Slack users
+    pretty much like django.contrib.auth.get_user_model
+    """
+    # all imports inside so package can be imported without django
+    from django.apps import apps
+    from django.conf import settings
+    from django.core.exceptions import ImproperlyConfigured
+
+    try:
+        return apps.get_model(settings.SLACKBOT_USER_MODEL, require_ready=False)
+    except ValueError:
+        raise ImproperlyConfigured("SLACKBOT_USER_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured(
+            "SLACKBOT_USER_MODEL refers to model '%s' that has not been installed" % settings.SLACKBOT_USER_MODEL
+        )
