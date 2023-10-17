d0889dbf4f4693a13671c21bb24b11548a3fb492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:11:12 +0000

    Add package error-grids/0.1.0/error_grids-0.1.0.tar.gz

---
+++ packages/error-grids/error_grids-0.1.0.tar.gz/error_grids-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='error_grids',
+    version='0.1.0',
+    py_modules=['error_grids'],
+    author='Your Name',
+    author_email='your@email.com',
+    description='Python Implementation of Clarke and Parkes Error Grids for Blood Glucose Accuracy Analysis',
+    url='https://github.com/kriventsov/Clarke-and-Parkes-Error-Grids',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
