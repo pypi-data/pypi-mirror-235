d91c2ec9f17de54ae5ce5406a8b6776956202140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:06:57 +0000

    Add package avwx-engine/1.8.20/avwx_engine-1.8.20-py3-none-any.whl

---
+++ packages/avwx-engine/avwx_engine-1.8.20-py3-none-any.whl/avwx/service/__init__.py
@@ -0,0 +1,18 @@
+"""
+.. include:: ../../docs/service.md
+"""
+
+from .base import Service
+from .files import NOAA_GFS, NOAA_NBM
+from .scrape import (
+    get_service,
+    NOAA,
+    NOAA_ADDS,
+    AMO,
+    AVT,
+    MAC,
+    AUBOM,
+    OLBS,
+    NAM,
+    FAA_NOTAM,
+)
