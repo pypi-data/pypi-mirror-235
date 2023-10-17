00185c40129752fa6a9ca7d3c54be56aacecf48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 15:07:06 +0000

    Add package Alexandr-Varnakin/2.0/Alexandr_Varnakin-2.0.tar.gz

---
+++ packages/Alexandr-Varnakin/Alexandr_Varnakin-2.0.tar.gz/Alexandr_Varnakin-2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Alexandr_Varnakin',
+      version='2.0',
+      description='Coolest man on Earth',
+      packages=['Alexandr_Varnakin'],
+      author_email='mamadorogayamoskva@gmail.com',
+      zip_safe=False)
