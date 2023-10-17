d32974248753ad972402274cd798decd7fabcd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:42 +0000

    Add package PyMutSpec/0.0.7/PyMutSpec-0.0.7.tar.gz

---
+++ packages/PyMutSpec/PyMutSpec-0.0.7.tar.gz/PyMutSpec-0.0.7/pymutspec/utils/logging.py
@@ -0,0 +1,20 @@
+import os
+import logging
+import logging.config
+
+import yaml
+
+DEFAULT_PATH_TO_LOGCONF = os.path.join(os.path.dirname(__file__), "configs/log_settings.yaml")
+
+
+def load_logger(path=None, stream_level: str = None, filename=None):
+    path = path or DEFAULT_PATH_TO_LOGCONF
+    with open(path, "r") as file:
+        log_config = yaml.safe_load(file)
+        if stream_level is not None:
+            log_config["handlers"]["stream_handler"]["level"] = stream_level
+        if filename is not None:
+            log_config["handlers"]["full_file_handler"]["filename"] = filename
+        logging.config.dictConfig(log_config)
+    logger = logging.getLogger('MutSpecCalc')
+    return logger
---
+++ packages/PyMutSpec/PyMutSpec-0.0.7.tar.gz/PyMutSpec-0.0.7/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+extra_index_urls = []
+packages = []
+scripts = []
+
+with open("requirements.txt", encoding="utf-8") as file:
+    for line in map(str.strip, file):
+        if line:
+            if line.startswith("-f"):
+                extra_index_urls.append(line.split()[1])
+            elif line.startswith("#"):
+                continue
+            else:
+                packages.append(line)
+
+if os.path.exists("scripts"):
+    for file in os.listdir("scripts"):
+        if file.endswith(".py") and not file.startswith("_"):
+            fp = os.path.join("scripts", file)
+            with open(fp) as fin:
+                if "python" in fin.readline():
+                    scripts.append(fp)
+
+setup(
+    name="PyMutSpec",
+    version="0.0.7",
+    author="kpotoh",
+    description="Utilities for advanced analysis of mutational spectra",
+    url="https://github.com/mitoclub/PyMutSpec",
+    author_email="None",
+    license="MIT",
+    install_requires=packages,
+    dependency_links=extra_index_urls,
+    scripts=scripts,
+    packages=find_packages(),
+    # include_package_data=True,
+    package_data={'pymutspec': ['utils/configs/log_settings.yaml']},
+    python_requires=">=3.8",
+)
