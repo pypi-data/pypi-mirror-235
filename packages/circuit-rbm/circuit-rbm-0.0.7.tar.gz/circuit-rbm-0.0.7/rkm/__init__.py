b5947a38f8d1f31ac6d4bd21c434910ea653a35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:10:01 +0000

    Add package circuit-rbm/0.0.7/circuit-rbm-0.0.7.tar.gz

---
+++ packages/circuit-rbm/circuit-rbm-0.0.7.tar.gz/circuit-rbm-0.0.7/rkm/__init__.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("/circuit-rbm/rkm/")
+from rbm_pythorch import RBM
+from convolutional_RBM import convolutional_RBM
+from utils_pythorch import *
---
+++ packages/circuit-rbm/circuit-rbm-0.0.7.tar.gz/circuit-rbm-0.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7' 
+DESCRIPTION = 'Train a RKM'
+LONG_DESCRIPTION = 'A Python package to simulate and train a RKM model'
+
+# Setting up
+setup(
+        name="circuit-rbm", 
+        version=VERSION,
+        author="Simone Ciarella",
+        author_email="<simoneciarella@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy','pandas','matplotlib','torch','torchvision'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'statistical physics', 'machine learning', 'rbm'],
+        classifiers= [
+            "Development Status :: 1 - Planning",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+             "Operating System :: Unix",
+            # "Operating System :: MacOS :: MacOS X",
+            # "Operating System :: Microsoft :: Windows",
+        ]
+)
