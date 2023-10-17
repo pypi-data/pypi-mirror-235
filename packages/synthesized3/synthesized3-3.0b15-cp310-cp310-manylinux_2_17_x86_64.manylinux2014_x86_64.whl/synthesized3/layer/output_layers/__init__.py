92a68cfebfd9e522b8c4040779d1a1501247bca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:09:55 +0000

    Add package synthesized3/3.0b15/synthesized3-3.0b15-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/synthesized3/synthesized3-3.0b15-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/synthesized3/layer/output_layers/__init__.py
@@ -0,0 +1,15 @@
+from .binary_categorical_output import BinaryCategoricalOutput
+from .continuous_output import ContinuousOutput
+from .multi_categorical_output import MultiCategoricalOutput
+from .output_layer import OutputLayer
+from .output_layer_collection import OutputLayerCollection
+from .output_layer_factory import OutputLayerFactory
+
+__all__ = [
+    "BinaryCategoricalOutput",
+    "ContinuousOutput",
+    "MultiCategoricalOutput",
+    "OutputLayer",
+    "OutputLayerCollection",
+    "OutputLayerFactory",
+]
