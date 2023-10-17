8d89b2da823d2ceb9e545c00d55dc520afd51cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 17:08:11 +0000

    Add package ingestion-game/0.1.1/ingestion_game-0.1.1.tar.gz

---
+++ packages/ingestion-game/ingestion_game-0.1.1.tar.gz/ingestion_game-0.1.1/src/application/__init__.py
@@ -0,0 +1,13 @@
+from application.get_output import get_output
+from application.logic_ingestion import logic_ingestion
+from application.read_file import read_file
+from application.read_hierarchy import read_hierarchy
+from application.read_important_keys import read_important_keys
+
+__all__ = [
+    "get_output",
+    "logic_ingestion",
+    "read_file",
+    "read_hierarchy",
+    "read_important_keys"
+]
---
+++ packages/ingestion-game/ingestion_game-0.1.1.tar.gz/ingestion_game-0.1.1/tests/applications/test_read_important_keys.py
@@ -0,0 +1,13 @@
+from application import read_important_keys
+
+
+def test_read_important_keys(
+        new_important_keys: str,
+) -> None:
+    expected = dict(
+        id=int,
+        name=str,
+        food=str,
+        type=str,
+    )
+    assert read_important_keys(new_important_keys) == expected
