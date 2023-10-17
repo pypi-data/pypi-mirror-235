f0bd76497bfab8f80b22960943ba6975db5ba21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:06:46 +0000

    Add package valohai-yaml/0.34.0/valohai_yaml-0.34.0-py3-none-any.whl

---
+++ packages/valohai-yaml/valohai_yaml-0.34.0-py3-none-any.whl/valohai_yaml/utils/lint.py
@@ -0,0 +1,19 @@
+from typing import TYPE_CHECKING, Any, Iterable
+
+from valohai_yaml.types import LintContext
+
+if TYPE_CHECKING:
+    from valohai_yaml.lint import LintResult
+
+
+def lint_iterables(
+    lint_result: "LintResult",
+    context: LintContext,
+    iterables: Iterable[Iterable[Any]],
+) -> None:
+    for iterable in iterables:
+        if isinstance(iterable, dict):
+            iterable = iterable.values()
+        for item in iterable:
+            if hasattr(item, "lint"):
+                item.lint(lint_result, context)
