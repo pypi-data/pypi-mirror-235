3ad6d307f7ecf89b51a7fc257180235fb193d765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:09:36 +0000

    Add package mpcjax/0.2.1/mpcjax-0.2.1.tar.gz

---
+++ packages/mpcjax/mpcjax-0.2.1.tar.gz/mpcjax-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+setup(
+    name="mpcjax",
+    version="0.2.1",
+    packages=find_packages(),
+    long_description=Path("README.md").read_text(),
+    install_requires=[
+        "jfi @ git+https://github.com/rdyro/jfi-JAXFriendlyInterface",
+    ],
+)
