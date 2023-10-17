d6974c4788c2d7d6f09d2fe6d376f930b3d8c724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:10:59 +0000

    Add package hep-cli/0.1.1/hep-cli-0.1.1.tar.gz

---
+++ packages/hep-cli/hep-cli-0.1.1.tar.gz/hep-cli-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hep_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['rich>=13.6.0,<14.0.0']
+
+entry_points = \
+{'console_scripts': ['hep-cli = hep_cli.cli:run_app']}
+
+setup_kwargs = {
+    'name': 'hep-cli',
+    'version': '0.1.1',
+    'description': 'A basic CLI app',
+    'long_description': '# python-cli-template\nA template repository for creating CLI apps \n\n\n## Dependencies\n\n- poetry https://python-poetry.org/\n- typer https://typer.tiangolo.com/\n\n\n## Commands\n\n```bash\npoetry new <package-name>\n\npoetry add <dep-package-name>\n```',
+    'author': 'Oguzhan Yilmaz',
+    'author_email': 'oguzhan@hepapi.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<3.13',
+}
+
+
+setup(**setup_kwargs)
