c89fe901b767611030129ba1c7fb7866f595a874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 15:06:19 +0000

    Add package embykeeper/2.4.9/embykeeper-2.4.9.tar.gz

---
+++ packages/embykeeper/embykeeper-2.4.9.tar.gz/embykeeper-2.4.9/embykeeper/telechecker/messager/pornemby.py
@@ -0,0 +1,9 @@
+from .base import Messager
+
+__ignore__ = True
+
+
+class PornembyMessager(Messager):
+    name = "Pornemby"
+    chat_name = "Pornemby"
+    default_messages = ["pornemby-common-wl@v2.yaml * 100"]
