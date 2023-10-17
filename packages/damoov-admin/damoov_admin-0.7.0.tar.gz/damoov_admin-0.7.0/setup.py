364b5d9e9bd588633a269759ff87f0adf86d4860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:08:14 +0000

    Add package damoov-admin/0.7.0/damoov_admin-0.7.0.tar.gz

---
+++ packages/damoov-admin/damoov_admin-0.7.0.tar.gz/damoov_admin-0.7.0/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="damoov_admin",
+    version="0.7.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "sentry_sdk"
+    ],
+    author="Damoov",
+    author_email="admin@damoov.com",
+    description="SDK for Damoov's APIs",
+    keywords="damoov sdk telematics mobiletelematics safety driving safe-driving fleetmanagement fleet-management fleet driving-behavior driving-behaviour driving-behavior-analysis driving-behaviour-analysis driving-behavior-monitoring driving-behaviour-monitoring driving-behavior-score driving-behaviour-score driving-behavior-scoring driving-behaviour-scoring driving-behavior-analysis driving-behaviour-analysis driving-behavior-monitoring driving-behaviour-monitoring driving-behavior-score driving-behaviour-score driving-behavior-scoring driving-behaviour-scoring driving-behavior-analysis driving-behaviour-analysis driving-behavior-monitoring driving-behaviour-monitoring driving-behavior-score driving-behaviour-score driving-behavior-scoring driving-behaviour-scoring driving-behavior-analysis driving-behaviour-analysis driving-behavior-monitoring driving-behaviour-monitoring driving-behavior-score driving-behaviour-score driving-behavior-scoring driving-behaviour-scoring",
+)
