d0c95888d461f833be2d2e330a1592e8be87ccde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:31 +0000

    Add package viqi-api/0.6.4.36/viqi_api-0.6.4.36-py3-none-any.whl

---
+++ packages/viqi-api/viqi_api-0.6.4.36-py3-none-any.whl/vqapi/__init__.py
@@ -0,0 +1,6 @@
+#
+from .cmd import bisque_argument_parser, bisque_config, bisque_session
+from .comm import BQServer, BQSession
+from .exception import *
+from .services import ResponseFile, ResponseFolder
+from .vqclass import *
