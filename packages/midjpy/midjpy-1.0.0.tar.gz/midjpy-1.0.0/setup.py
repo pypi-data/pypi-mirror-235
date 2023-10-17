f12412dfca25da8f4d5dc29050c577eb3e4a2df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:41 +0000

    Add package midjpy/1.0.0/midjpy-1.0.0.tar.gz

---
+++ packages/midjpy/midjpy-1.0.0.tar.gz/midjpy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='midjpy',
+    version='1.0.0',
+    description='Python client for the MIDJ API',
+    author='dmaxxy',
+    author_email='dmaxxy.bhw@gmail.com',
+    packages=['midjpy'],
+    install_requires=['requests'],
+)
