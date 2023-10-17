231f2bcd7cd1b47c1c9add2ef7e4171308536faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:10:20 +0000

    Add package transformers-re/1.0.0/transformers-re-1.0.0.tar.gz

---
+++ packages/transformers-re/transformers-re-1.0.0.tar.gz/transformers-re-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='transformers-re',
+    version='1.0.0',
+    author='kuangzh',
+    author_email='1432245553@qq.com',
+    url='https://github.com/kuangkzh/transformers-re',
+    description='A Regular Expression constraint for Language Models of transformers. With this module, you can force '
+                'the LLMs to generate following your regex. Using regex in tokens and tensors are also implemented in '
+                'this project.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "multiprocess",
+        "transformers>=4.0.0",
+        "regex>=2014.04.10"
+    ],
+)
