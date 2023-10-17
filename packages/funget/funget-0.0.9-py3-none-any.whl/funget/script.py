1ac15a79588e6cd2d765bbabd28f1b9fcb5436e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 15:06:31 +0000

    Add package funget/0.0.9/funget-0.0.9-py3-none-any.whl

---
+++ packages/funget/funget-0.0.9-py3-none-any.whl/funget/script.py
@@ -0,0 +1,31 @@
+
+import argparse
+import logging
+import os
+import time
+
+from funget import simple_download,split_download
+from git import Repo
+
+
+def download(args):
+    if args.multi:
+        return split_download(args.url,worker_num=worker,capacity=capacity)
+    else:
+        return simple_download(args.url)
+
+
+
+
+def funget():
+    parser = argparse.ArgumentParser(prog="PROG")
+
+    # 添加子命令
+    parser.add_argument("--multi", default=False, action="store_true", help="build multi package")
+    parser.add_argument("--worker", default=10, help="下载的多线程数量")
+    parser.add_argument("--capacity", default=100, help="下载的容量")
+    
+    parser.set_defaults(func=download)  # 设置默认函数
+    
+    args = parser.parse_args()
+    args.func(args)
