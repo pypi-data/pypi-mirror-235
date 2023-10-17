27eed4dcfba6a592ac6a7b3185588afee5dde524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:10 +0000

    Add package lapizlazuli/0.0.1/lapizlazuli-0.0.1-py3-none-any.whl

---
+++ packages/lapizlazuli/lapizlazuli-0.0.1-py3-none-any.whl/lulu/__init__.py
@@ -0,0 +1,19 @@
+def p1():
+	return """
+		hello, this will return 
+"""
+
+def p2():
+
+	s = """ 
+
+	hello, this will not print
+"""
+	print(s)
+
+def p3():
+        n = """
+
+        tunne unne le LA
+"""
+        print(n)
