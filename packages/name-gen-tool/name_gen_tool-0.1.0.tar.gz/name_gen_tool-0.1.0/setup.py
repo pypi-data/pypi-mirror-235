563cf438be2cc063edb5733a162b65a3003b6f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:11:40 +0000

    Add package name-gen-tool/0.1.0/name_gen_tool-0.1.0.tar.gz

---
+++ packages/name-gen-tool/name_gen_tool-0.1.0.tar.gz/name_gen_tool-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="name_gen_tool",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  
+    author="Bellamy",
+    author_email="bellamyy.blakee100@gmail.com",
+    description="A package to generate user names based on styles.",
+    keywords="username generator, instagram username generator, social media name generator",
+    url="https://github.com/bellamy-blakee/username-generator-tool",
+)
