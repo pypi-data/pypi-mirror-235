c1218ecaca6d57b05f48749edd4c688ae3ba03d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:09:12 +0000

    Add package hamal-utils/0.0.22/hamal-utils-0.0.22.tar.gz

---
+++ packages/hamal-utils/hamal-utils-0.0.22.tar.gz/hamal-utils-0.0.22/hamal_utils/code/s3_utils/common.py
@@ -0,0 +1,15 @@
+import boto3
+
+from hamal_utils.code.s3_utils.access_manager import s3_access_data
+
+aws_access_key_id = ''
+aws_secret_access_key = ''
+
+# TODO: remove count argument and get bucket count from designated function
+count = 10000000
+
+region_name, access_key_id, secret_access_key = s3_access_data()
+if access_key_id and secret_access_key:
+    s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+else:
+    s3 = boto3.client('s3')
