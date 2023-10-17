8628c5bd1744d944e7b66d6e1f4962c52a83bfd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:07:36 +0000

    Add package nycu-tdx-py/0.1.45/nycu_tdx_py-0.1.45.tar.gz

---
+++ packages/nycu-tdx-py/nycu_tdx_py-0.1.45.tar.gz/nycu_tdx_py-0.1.45/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='nycu_tdx_py',
+    version='0.1.45',
+    description='Python library for connecting to TDX',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author="Chia-Jung (Robert) Yeh",
+    author_email='chia-jung.yeh@sydney.edu.au',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ChiaJung-Yeh/nycu_tdx_py',
+    keywords='tdx transport',
+    install_requires=[
+          'pandas',
+          'geopandas',
+          'numpy',
+          'shapely',
+          'requests',
+          'tqdm'
+      ],
+)
