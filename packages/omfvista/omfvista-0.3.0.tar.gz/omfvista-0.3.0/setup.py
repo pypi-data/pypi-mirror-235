382e21516ca8c49983f24033bb974781cb55465d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 16:08:08 +0000

    Add package omfvista/0.3.0/omfvista-0.3.0.tar.gz

---
+++ packages/omfvista/omfvista-0.3.0.tar.gz/omfvista-0.3.0/setup.py
@@ -0,0 +1,33 @@
+"""``omfvista``: 3D visualization for the Open Mining Format (omf)
+"""
+import setuptools
+
+__version__ = "0.3.0"
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="omfvista",
+    version=__version__,
+    author="Bane Sullivan",
+    author_email="info@pvgeo.org",
+    description="3D visualization for the Open Mining Format (omf)",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/OpenGeoVis/omfvista",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "omf>=1.0.0",
+        "vectormath>=0.2.2",
+        "pyvista>=0.20.1",
+        "numpy",
+        "matplotlib",
+    ],
+    classifiers=(
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ),
+)
