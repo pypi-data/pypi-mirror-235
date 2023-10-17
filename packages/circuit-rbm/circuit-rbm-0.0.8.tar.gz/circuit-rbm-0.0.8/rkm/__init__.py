55a7130ccc8c95fa4f641bb9f5fc9cf729b753c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:10:09 +0000

    Add package circuit-rbm/0.0.8/circuit-rbm-0.0.8.tar.gz

---
+++ packages/circuit-rbm/circuit-rbm-0.0.8.tar.gz/circuit-rbm-0.0.8/rkm/__init__.py
@@ -0,0 +1,9 @@
+from rkm import rbm_pytorch
+from rkm import convolutional_RBM
+from rkm import utils_pythorch
+
+#import sys
+#sys.path.append("/circuit-rbm/rkm/")
+#from rbm_pythorch import RBM
+#from convolutional_RBM import convolutional_RBM
+#from utils_pythorch import *
