6b690869a1498f82e94bffc7a5999a0956d8e93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 03:13:30 +0000

    Add package pybamm/23.9rc0/pybamm-23.9rc0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pybamm/pybamm-23.9rc0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pybamm/models/full_battery_models/lithium_ion/electrode_soh_half_cell.py
@@ -0,0 +1,58 @@
+#
+# A model to calculate electrode-specific SOH, adapted to a half-cell
+#
+import pybamm
+
+
+class ElectrodeSOHHalfCell(pybamm.BaseModel):
+    """Model to calculate electrode-specific SOH for a half-cell, adapted from
+    :footcite:t:`Mohtat2019`.
+    This model is mainly for internal use, to calculate summary variables in a
+    simulation.
+
+    .. math::
+        V_{max} = U_w(x_{100}),
+    .. math::
+        V_{min} = U_w(x_{0}),
+    .. math::
+        x_0 = x_{100} - \\frac{C}{C_w}.
+
+    Subscript w indicates working electrode and subscript c indicates counter electrode.
+
+    """
+
+    def __init__(self, name="Electrode-specific SOH model"):
+        pybamm.citations.register("Mohtat2019")
+        super().__init__(name)
+        param = pybamm.LithiumIonParameters({"working electrode": "positive"})
+
+        x_100 = pybamm.Variable("x_100", bounds=(0, 1))
+        x_0 = pybamm.Variable("x_0", bounds=(0, 1))
+        Q_w = pybamm.InputParameter("Q_w")
+        T_ref = param.T_ref
+        U_w = param.p.prim.U
+        Q = Q_w * (x_100 - x_0)
+
+        V_max = param.ocp_soc_100_dimensional
+        V_min = param.ocp_soc_0_dimensional
+
+        self.algebraic = {
+            x_100: U_w(x_100, T_ref) - V_max,
+            x_0: U_w(x_0, T_ref) - V_min,
+        }
+        self.initial_conditions = {x_100: 0.8, x_0: 0.2}
+
+        self.variables = {
+            "x_100": x_100,
+            "x_0": x_0,
+            "Q": Q,
+            "Uw(x_100)": U_w(x_100, T_ref),
+            "Uw(x_0)": U_w(x_0, T_ref),
+            "Q_w": Q_w,
+            "Q_w * (x_100 - x_0)": Q_w * (x_100 - x_0),
+        }
+
+    @property
+    def default_solver(self):
+        # Use AlgebraicSolver as CasadiAlgebraicSolver gives unnecessary warnings
+        return pybamm.AlgebraicSolver()
