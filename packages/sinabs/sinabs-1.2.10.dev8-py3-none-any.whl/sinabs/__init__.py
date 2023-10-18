f7c45961037d68fcd2238129103b80c088d96f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:10:14 +0000

    Add package sinabs/1.2.10.dev8/sinabs-1.2.10.dev8-py3-none-any.whl

---
+++ packages/sinabs/sinabs-1.2.10.dev8-py3-none-any.whl/sinabs/__init__.py
@@ -0,0 +1,9 @@
+from pbr.version import VersionInfo
+
+__version__ = VersionInfo("sinabs").release_string()
+
+from . import conversion, utils
+from .from_torch import from_model
+from .network import Network
+from .synopcounter import SNNAnalyzer, SynOpCounter
+from .utils import reset_states, set_batch_size, zero_grad
