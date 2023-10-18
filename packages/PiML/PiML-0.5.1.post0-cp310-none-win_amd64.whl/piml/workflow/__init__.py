1614a0d86b06097edeba0ed4cbf23d4c406ac645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 03:12:32 +0000

    Add package PiML/0.5.1.post0/PiML-0.5.1.post0-cp310-none-win_amd64.whl

---
+++ packages/PiML/PiML-0.5.1.post0-cp310-none-win_amd64.whl/piml/workflow/__init__.py
@@ -0,0 +1,9 @@
+from .base import Model
+from .data_api import DataAPI
+from .eda_api import EdaAPI
+from .data_quality_api import OutlierDetectionAPI
+from .compare_api import CompareAPI
+from .diagnose_api import DiagnoseAPI
+from .explain_api import ExplainAPI
+from .interpret_api import InterpretAPI
+from .fairness_api import FairnessAPI, FairnessAPI_solas, FairnessCompareAPI
