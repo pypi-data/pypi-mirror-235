64a1741095edc47c926aecdabe1d5d6e06e35a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:08:13 +0000

    Add package pyodmongo/0.2.11/pyodmongo-0.2.11.tar.gz

---
+++ packages/pyodmongo/pyodmongo-0.2.11.tar.gz/pyodmongo-0.2.11/pyodmongo/models/id_model.py
@@ -0,0 +1,40 @@
+from typing import Any
+from bson import ObjectId
+from pydantic import GetCoreSchemaHandler
+import pydantic_core
+from pydantic_core import core_schema, CoreSchema
+from pydantic_core.core_schema import ValidationInfo, str_schema
+from packaging import version
+
+if version.parse(pydantic_core.__version__) < version.parse("2.10.0"):
+    plain_validator_function = core_schema.general_plain_validator_function
+else:
+    plain_validator_function = core_schema.with_info_plain_validator_function
+
+
+class Id(str):
+    @classmethod
+    def validate(cls, v, _: ValidationInfo):
+        if v is None:
+            return v
+        if not ObjectId.is_valid(v):
+            raise ValueError("invalid Id")
+        return str(v)
+
+    @classmethod
+    def serialization(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("invalid Id")
+        return str(v)
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:  # type: ignore
+        return core_schema.json_or_python_schema(
+            python_schema=plain_validator_function(cls.validate),
+            json_schema=str_schema(),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda v: str(v) if ObjectId.is_valid(v) else v
+            ),
+        )
