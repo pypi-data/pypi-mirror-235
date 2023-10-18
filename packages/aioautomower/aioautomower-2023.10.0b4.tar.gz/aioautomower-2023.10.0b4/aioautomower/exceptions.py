9ab7155974e562ec335694dd4007362c02f7e4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:07:28 +0000

    Add package aioautomower/2023.10.0b4/aioautomower-2023.10.0b4.tar.gz

---
+++ packages/aioautomower/aioautomower-2023.10.0b4.tar.gz/aioautomower-2023.10.0b4/aioautomower/exceptions.py
@@ -0,0 +1,25 @@
+"""Library for exceptions using the Husqvarna Automower API."""
+
+
+class HusqvarnaAAutomowerException(Exception):
+    """Base class for all client exceptions."""
+
+
+class ApiException(HusqvarnaAAutomowerException):
+    """Raised during problems talking to the API."""
+
+
+class AuthException(HusqvarnaAAutomowerException):
+    """Raised due to auth problems talking to API."""
+
+
+class InvalidSyncTokenException(HusqvarnaAAutomowerException):
+    """Raised when the sync token is invalid."""
+
+
+class ApiForbiddenException(HusqvarnaAAutomowerException):
+    """Raised due to permission errors talking to API."""
+
+
+class NoDataAvailableException(HusqvarnaAAutomowerException):
+    """Raised due updating data, when no data is available."""
