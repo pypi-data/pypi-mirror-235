7f4f1e8902b309549c3f5ef68afffc448d726706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:10:22 +0000

    Add package mdtraj-duplicate/1.9.9/mdtraj-duplicate-1.9.9.tar.gz

---
+++ packages/mdtraj-duplicate/mdtraj-duplicate-1.9.9.tar.gz/mdtraj-duplicate-1.9.9/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.9.9'
+short_version = '1.9.9'
+full_version = '1.9.9+a862e92497ed096ddab1de039564d0c0dfe87e89'
+git_revision = 'a862e92497ed096ddab1de039564d0c0dfe87e89'
+release = False
