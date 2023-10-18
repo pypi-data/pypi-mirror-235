0588cbf9f0d2a2610e062fe9e57d7d53b552e939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 05:09:51 +0000

    Add package ofdcomparer/0.0/ofdcomparer-0.0.tar.gz

---
+++ packages/ofdcomparer/ofdcomparer-0.0.tar.gz/ofdcomparer-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ofdcomparer',
+      version='0.0',
+      description='Библиотека для сравнивания ФД из ФН и ОФД',
+      packages=['ofdcomparer'],
+      author_email='k.kabisova@atol.ru',
+      zip_safe=False)
