5a4ad2ffcc680ae321085efdcd44e536dc142fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 03:13:05 +0000

    Add package tvoverlay/1.0.1/TVOverlay-1.0.1.tar.gz

---
+++ packages/tvoverlay/TVOverlay-1.0.1.tar.gz/TVOverlay-1.0.1/tvoverlay/const.py
@@ -0,0 +1,30 @@
+"""Constants for the library."""
+
+from enum import Enum
+from typing import Final
+
+DEFAULT_TITLE: Final = "Notification"
+DEFAULT_APP_TITLE: Final = "Notification"
+DEFAULT_APP_ICON: Final = "mdi:bell"
+DEFAULT_SMALL_ICON: Final = "mdi:bell"
+DEFAULT_LARGE_ICON: Final = "mdi:home-assistant"
+DEFAULT_COLOR: Final = "#049cdb"
+DEFAULT_DURATION: Final = 5
+
+
+class Positions(Enum):
+    """Supported positions for the notification overlay."""
+
+    BOTTOM_RIGHT = "bottom_end"
+    BOTTOM_LEFT = "bottom_start"
+    TOP_RIGHT = "top_end"
+    TOP_LEFT = "top_start"
+
+
+class Shapes(Enum):
+    """Supported positions for the notification overlay."""
+
+    CIRCLE = "circle"
+    ROUNDED = "rounded"
+    RECTANGULAR = "rectangular"
+
