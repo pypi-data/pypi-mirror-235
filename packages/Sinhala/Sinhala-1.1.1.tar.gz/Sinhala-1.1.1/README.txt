50296015626ce282f566e0cc8f1159115f0a60d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 05:08:50 +0000

    Add package Sinhala/1.1.1/Sinhala-1.1.1.tar.gz

---
+++ packages/Sinhala/Sinhala-1.1.1.tar.gz/Sinhala-1.1.1/README.txt
@@ -0,0 +1,6 @@
+﻿sinhala Prasthawa Pirulu
+
+Developed by @dotLK from sasindu (c) 2020
+
+
+Check out: https://www.youtube.com/@sasindu
---
+++ packages/Sinhala/Sinhala-1.1.1.tar.gz/Sinhala-1.1.1/sinhala/__init__.py
@@ -0,0 +1,26 @@
+import random
+from .Datas import *
+
+
+# prastha_pirulu
+def prastha_pirulu():
+    return random.choice(Prastha_Pirulu_List)
+
+# prastha_pirulu
+def pp():
+    return random.choice(Prastha_Pirulu_List)
+
+
+# theravili
+def theravili():
+    return random.choice(theravili_List)
+
+# thun_theravili
+def thun_theravili():
+    return random.choice(thun_theravili_List)
+
+
+# thun_theravili shorts
+def thun():
+    return random.choice(thun_theravili_List)
+
