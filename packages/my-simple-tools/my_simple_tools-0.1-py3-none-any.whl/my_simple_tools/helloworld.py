1a51b643f13b24138dd6f4b72045e46f213e1ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:08:35 +0000

    Add package my-simple-tools/0.1/my_simple_tools-0.1-py3-none-any.whl

---
+++ packages/my-simple-tools/my_simple_tools-0.1-py3-none-any.whl/my_simple_tools/helloworld.py
@@ -0,0 +1,8 @@
+def hello_person(name_person: str) -> print:
+    
+    """
+    Função responsavel por gerar uma simples mensagem de console de boas vindas com a mensagem definida no parametro. 
+    """
+    
+    return print(f'Hello {str(name_person)} !')
+
