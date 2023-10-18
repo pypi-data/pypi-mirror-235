dc3cac395cef31c6748e36c81c8923e0d238808f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:08:43 +0000

    Add package tjax/0.30.1/tjax-0.30.1-py3-none-any.whl

---
+++ packages/tjax/tjax-0.30.1-py3-none-any.whl/tjax/_src/dataclasses/__init__.py
@@ -0,0 +1,2 @@
+from .dataclass import *
+from .helpers import *
---
+++ packages/tjax/tjax-0.30.1-py3-none-any.whl/tjax/gradient.py
@@ -0,0 +1,26 @@
+from tjax._src.gradient.aliases import (DPSGD, LARS, SGD, SM3, AdaBelief, AdaFactor, AdaGrad, Adam,
+                                        AdamW, Fromage, Lamb, NoisySGD, RAdam, RMSProp, Yogi)
+from tjax._src.gradient.chain import ChainedGradientState, ChainedGradientTransformation
+from tjax._src.gradient.smd import SMDGradient, SMDState
+from tjax._src.gradient.transform import (GenericGradientState, GradientState,
+                                          GradientTransformation, SecondOrderGradientTransformation,
+                                          ThirdOrderGradientTransformation)
+from tjax._src.gradient.transforms import (AddDecayedWeights, AddNoise, ApplyEvery, Centralize, Ema,
+                                           Scale, ScaleByAdam, ScaleByBelief, ScaleByParamBlockNorm,
+                                           ScaleByParamBlockRMS, ScaleByRAdam, ScaleByRms,
+                                           ScaleByRss, ScaleBySchedule, ScaleBySM3, ScaleByStddev,
+                                           ScaleByTrustRatio, ScaleByYogi, Schedule, Trace)
+
+__all__ = ['ChainedGradientTransformation', 'ChainedGradientState', 'SMDGradient', 'SMDState',
+           # transform.py
+           'GradientState', 'GenericGradientState', 'GradientTransformation',
+           'SecondOrderGradientTransformation', 'ThirdOrderGradientTransformation',
+           # aliases.py
+           'AdaBelief', 'AdaFactor', 'AdaGrad', 'Adam', 'AdamW', 'Fromage', 'LARS', 'Lamb',
+           'NoisySGD', 'RAdam', 'RMSProp', 'SGD', 'SM3', 'Yogi', 'DPSGD',
+           # transforms.py
+           'Trace', 'Ema', 'ScaleByRss', 'ScaleByRms', 'ScaleByStddev', 'ScaleByAdam', 'Scale',
+           'ScaleByParamBlockNorm', 'ScaleByParamBlockRMS', 'ScaleByBelief', 'ScaleByYogi',
+           'ScaleByRAdam', 'AddDecayedWeights', 'ScaleBySchedule', 'ScaleByTrustRatio', 'AddNoise',
+           'ApplyEvery', 'Centralize', 'ScaleBySM3',
+           'Schedule']
