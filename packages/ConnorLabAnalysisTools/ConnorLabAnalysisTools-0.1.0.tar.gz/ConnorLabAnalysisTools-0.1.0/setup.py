05c67f179ea141a9cd00602a6236e542b9f8c127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:06:36 +0000

    Add package ConnorLabAnalysisTools/0.1.0/ConnorLabAnalysisTools-0.1.0.tar.gz

---
+++ packages/ConnorLabAnalysisTools/ConnorLabAnalysisTools-0.1.0.tar.gz/ConnorLabAnalysisTools-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='ConnorLabAnalysisTools',
+    version='0.1.0',
+    description='Tools for analyzing data in the Connor Lab',
+    # long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Allen Chen',
+    author_email='allenmuhanchen@gmail.com',
+    url='https://github.com/EdConnorLab/ConnorLabAnalysisTools',
+    packages=find_packages(),
+    install_requires=required,
+)
+
