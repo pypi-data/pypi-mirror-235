702cd81289cf258bc9b774784807594132fdc701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 05:08:26 +0000

    Add package flick-python-sdk/1.0.0rc1/flick-python-sdk-1.0.0rc1.tar.gz

---
+++ packages/flick-python-sdk/flick-python-sdk-1.0.0rc1.tar.gz/flick-python-sdk-1.0.0rc1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "flick-python-sdk",
+    version = "1.0.0rc1",
+    author = "Sivadas Rajan",
+    author_email = "sivadasrajan@gmail.com",
+    description = "A Python wrapper for interacting with APIs from Flick.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    install_requires=["aiohttp>=3.5.0"],
+    url = "https://pypi.org/project/flick-python-sdk/",
+    maintainer="Sivadas Rajan",
+    maintainer_email="sivadasrajan@gmail.com",
+    project_urls = {
+        "Bug Tracker": "https://github.com/flick-network/flick-python-sdk/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = ['flick'],
+    python_requires = ">=3.6"
+)
