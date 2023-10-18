aa030b1e021ff05a5d7903043492897cf948704c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 03:12:31 +0000

    Add package scitbx/0.0.46/scitbx-0.0.46-py3-none-any.whl

---
+++ packages/scitbx/scitbx-0.0.46-py3-none-any.whl/scitbx/easy_import.py
@@ -0,0 +1,29 @@
+import pip
+from operator import imod
+import os
+import sys
+import warnings
+import shutil
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+from copy import deepcopy
+from scipy import stats
+from datetime import datetime, timedelta
+try:
+    import seaborn as sns
+except Exception as e:
+    print(e)
+    install('seaborn')
+    import seaborn as sns
+try:
+    import xarray as xr
+except Exception as e:
+    print(e)
+    install('xarray')
+    import xarray as xr
+
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+nature_colors = ["#E64B35B2", "#4DBBD5B2", "#00A087B2", "#3C5488B2", "#F39B7FB2", "#8491B4B2", "#91D1C2B2", "#DC0000B2", "#7E6148B2"]
