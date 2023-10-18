c62471ec474044105a5688b98176a68d995fa140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 05:08:24 +0000

    Add package check-print/0.1.5/check_print-0.1.5-py3-none-any.whl

---
+++ packages/check-print/check_print-0.1.5-py3-none-any.whl/tests/test.py
@@ -0,0 +1,46 @@
+print()
+
+# access
+fprint(
+
+# access
+    f_print(
+
+# access
+f_0print(
+
+# access
+.print (
+
+# access
+. print (
+
+# access
+.print(
+
+# access
+    .
+    print(
+
+# access
+def print(
+
+# access
+def
+ print(
+
+# access
+.
+print(
+
+# decline
+print(
+
+# decline
+(print(
+
+# decline
+= print (
+
+# decline
+    print(
