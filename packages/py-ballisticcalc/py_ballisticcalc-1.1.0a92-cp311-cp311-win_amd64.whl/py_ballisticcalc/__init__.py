13d22c33813b9c34b8c7423579cbc67197bf8736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:08:50 +0000

    Add package py-ballisticcalc/1.1.0a92/py_ballisticcalc-1.1.0a92-cp311-cp311-win_amd64.whl

---
+++ packages/py-ballisticcalc/py_ballisticcalc-1.1.0a92-cp311-cp311-win_amd64.whl/py_ballisticcalc/__init__.py
@@ -0,0 +1,24 @@
+"""LGPL library for small arms ballistic calculations (Python 3.8+)"""
+
+__author__ = "o-murphy"
+__copyright__ = ("",)
+
+__credits__ = ["o-murphy"]
+__version__ = "1.1.0a92"
+
+import logging
+try:
+    from .drag_model import *  # pylint: disable=import-error
+    from .trajectory_calc import *  # pylint: disable=import-error
+except ImportError:
+    logging.warning("Package installed in --no-binary (pure python) mode, "
+                    "use .whl packages to get better performance")
+    from .pure import *
+from .drag_tables import *
+from .settings import *
+from .multiple_bc import *
+from .interface import *
+from .trajectory_data import *
+from .conditions import *
+from .munition import *
+from .unit import *
