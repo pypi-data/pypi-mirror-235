8b9b70cdcbb32390462a7041f6b0392bb4e6cac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:07:47 +0000

    Add package hamal-utils/0.0.41/hamal_utils-0.0.41-py3-none-any.whl

---
+++ packages/hamal-utils/hamal_utils-0.0.41-py3-none-any.whl/hamal_utils/code/common.py
@@ -0,0 +1,10 @@
+from hamal_utils.code.prefect_utils.env_manager import get_env
+
+MONITOR_TAG_TMPL = "hamalutils-{name}-{from_percent}-{to_percent}"
+
+_EXTENSIONS_FROM_ENV = get_env('LIST_FILES_EXTENSIONS')
+
+EXTENSIONS = _EXTENSIONS_FROM_ENV.split(',') if _EXTENSIONS_FROM_ENV else None
+
+FROM_PERCENT = float(get_env('FROM_PERCENT', .0))
+TO_PERCENT = float(get_env('TO_PERCENT', 1.0))
