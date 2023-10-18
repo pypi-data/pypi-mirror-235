2c6c768b9f91efd0748c09d75d86479a1bc9c317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 03:13:26 +0000

    Add package pyfilehandling/2.0.2/pyfilehandling-2.0.2.tar.gz

---
+++ packages/pyfilehandling/pyfilehandling-2.0.2.tar.gz/pyfilehandling-2.0.2/README.md
@@ -0,0 +1,25 @@
+# PyFileHandling
+[![Run-Tests](https://github.com/JeelDobariya38/PyFileHandling/actions/workflows/Run-Tests.yaml/badge.svg)](https://github.com/JeelDobariya38/PyFileHandling/actions/workflows/Run-Tests.yaml)
+
+## Overview
+PyFileHandling is a Python package that provides file handling capabilities and offers various functions for file manipulation operations.
+
+## Installation
+
+You can install PyFileHandling using pip:
+
+```shell
+pip install pyfilehandling
+```
+
+## Contributing
+
+Contributions are welcome! To contribute to PyFileHandling, please read the [Contribution Guidelines](CONTRIBUTING.md).
+
+## Code of Conduct
+
+We strive to maintain a positive and inclusive community. We encourage everyone participating in PyFileHandling to read the [Code of Conduct](CODE_OF_CONDUCT.md).
+
+## License
+
+By contributing to `PyFileHandling`, you agree that your contributions will be licensed under the [MIT License](LICENSE.txt). Make sure you understand and agree with this before submitting your contributions.
---
+++ packages/pyfilehandling/pyfilehandling-2.0.2.tar.gz/pyfilehandling-2.0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read()
+
+long_description = read_file('README.md')
+
+setup(
+    name='pyfilehandling',
+    version='2.0.2',
+    description='A Python package for file manipulation operations.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='JeelDobariya',
+    author_email='dobariyaj34@gmail.com',
+    url='https://github.com/JeelDobariya38/PyFileHandling',
+    packages=['pyfilehandling'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    keywords=['file handling','file manipulation','file operation'],
+    project_urls={
+        'Bug Tracker': 'https://github.com/JeelDobariya38/PyFileHandling/issues',
+        'Documentation': 'https://jeeldobariya38.github.io/PyFileHandling/',
+        'Source Code': 'https://github.com/JeelDobariya38/PyFileHandling',
+    },
+    package_data={
+        'pyfilehandling': ['py.typed', '*.pyi'],
+    },
+    license='MIT',
+    python_requires='>=3.9',
+)
