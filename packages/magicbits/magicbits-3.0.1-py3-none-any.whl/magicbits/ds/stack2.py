6dbca9329898a7d7ce3b3d42a2f6455ccb9ee91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 05:08:57 +0000

    Add package magicbits/3.0.1/magicbits-3.0.1-py3-none-any.whl

---
+++ packages/magicbits/magicbits-3.0.1-py3-none-any.whl/magicbits/ds/stack2.py
@@ -0,0 +1,12 @@
+my_stack = []
+
+print("Stack without element : \n",my_stack)
+my_stack.append('a')
+my_stack.append('b')
+my_stack.append('c')
+
+print("The desired stack is :\n ", my_stack)
+
+my_stack.pop()
+my_stack.pop()
+print("The desired stack after popping out is :\n ", my_stack)
