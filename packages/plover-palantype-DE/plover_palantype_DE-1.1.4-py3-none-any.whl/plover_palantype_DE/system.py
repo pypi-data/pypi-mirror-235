65668a6144ffd165ecf77e7bf3576fb4bc698d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:08:06 +0000

    Add package plover-palantype-DE/1.1.4/plover_palantype_DE-1.1.4-py3-none-any.whl

---
+++ packages/plover-palantype-DE/plover_palantype_DE-1.1.4-py3-none-any.whl/plover_palantype_DE/system.py
@@ -0,0 +1,73 @@
+from plover.system import english_stenotype
+
+# cf. https://plover.readthedocs.io/en/4.0.0-dev8/api/system.html
+
+KEYS = (
+    'v-', 'D-', 'b-', 'ʃ-', 'S-', 'F-', 'G-', 'N-', 'B-', 'M-', '+-', 'L-',
+    'Ä-', 'E-', 'A-', '~-',
+    '-U', '-I', '-O', '-Ü',
+    '-M', '-+', '-L', '-G', '-N', '-B', '-ʃ', '-S', '-F', '-n', '-D', '-s'
+)
+
+IMPLICIT_HYPHEN_KEYS = ('Ä-', 'E-', 'A-', '~-', '-U', '-I', '-O', '-Ü')
+
+SUFFIX_KEYS = ()
+
+NUMBER_KEY = None
+
+NUMBERS = {}
+
+# Type: str or None
+UNDO_STROKE_STENO = 'I+NSD'
+
+ORTHOGRAPHY_RULES = []
+
+ORTHOGRAPHY_RULES_ALIASES = {}
+
+ORTHOGRAPHY_WORDLIST = None
+
+KEYMAPS = {
+    'Keyboard': {
+        'v-': 'q',
+        'D-': 'a',
+        'b-': 'z',
+        'ʃ-': '2',
+        'S-': 'w',
+        'F-': 's',
+        'G-': '3',
+        'N-': 'e',
+        'B-': 'd',
+        'M-': '4',
+        '+-': 'r',
+        'L-': 'f',
+        'Ä-': 'c',
+        'E-': 'v',
+        'A-': 'b',
+        '~-': 'g',
+        '-U': 'h',
+        '-I': 'n',
+        '-O': 'm',
+        '-Ü': ',',
+        '-M': '7',
+        '-+': 'u',
+        '-L': 'j',
+        '-G': '8',
+        '-N': 'i',
+        '-B': 'k',
+        '-ʃ': '9',
+        '-S': 'o',
+        '-F': 'l',
+        '-n': 'p',
+        '-D': ';',
+        '-s': '/',
+        'arpeggiate': 'space',
+    },
+}
+
+DICTIONARIES_ROOT = 'asset:plover_palantype_DE:dictionaries'
+DEFAULT_DICTIONARIES = [
+    'palantype-DE.json',
+    'palantype-DE-anglicisms.json',
+    'palantype-DE-extra.json',
+    'palantype-DE-numbers.json'
+]
