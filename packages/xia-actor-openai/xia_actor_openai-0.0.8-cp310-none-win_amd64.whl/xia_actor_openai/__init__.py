9a14bc796dff804fcc21ad8338b3b2129ebf04b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:08:49 +0000

    Add package xia-actor-openai/0.0.8/xia_actor_openai-0.0.8-cp310-none-win_amd64.whl

---
+++ packages/xia-actor-openai/xia_actor_openai-0.0.8-cp310-none-win_amd64.whl/xia_actor_openai/__init__.py
@@ -0,0 +1,10 @@
+from xia_actor_openai.actor import GptActor, GptMindset
+from xia_actor_openai.graphist import PollinationActor, PollinationMindset
+
+
+__all__ = [
+    "GptActor", "GptMindset",
+    "PollinationActor", "PollinationMindset"
+]
+
+__version__ = "0.0.8"
