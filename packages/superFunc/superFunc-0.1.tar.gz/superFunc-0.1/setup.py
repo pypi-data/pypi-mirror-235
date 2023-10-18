c7363f322362bcf0924d8cb935a15e1c11303944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:07:26 +0000

    Add package superFunc/0.1/superFunc-0.1.tar.gz

---
+++ packages/superFunc/superFunc-0.1.tar.gz/superFunc-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="superFunc",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Gereksinimler buraya yazılır, şimdilik boş bırakıyoruz.
+    ],
+    author="Abdullah Fatih Hocu",
+    author_email="hocuaf@gmail.com",
+    description="Bu harika bir Python kütüphanesi örneğidir. Yorumlarinizi lutfen bizimle paylasiniz.",
+    keywords="ornek kutuphane python",
+    url="https://github.com/hocuf",
+)
