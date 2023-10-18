1687a79f2ab97899963c58a0046207a949f620eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:07:59 +0000

    Add package jibrish-to-hebrew/1.0/jibrish_to_hebrew-1.0.tar.gz

---
+++ packages/jibrish-to-hebrew/jibrish_to_hebrew-1.0.tar.gz/jibrish_to_hebrew-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jibrish_to_hebrew',
+    version='1.0',
+    packages=['jibrish_to_hebrew'],
+    install_requires=[
+        # List any dependencies here
+    ],
+)
