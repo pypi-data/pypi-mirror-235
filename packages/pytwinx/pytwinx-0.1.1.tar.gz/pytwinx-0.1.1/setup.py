8e6cf2e3dc0759625ec88dfd566c0713e6bdee57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:06:58 +0000

    Add package pytwinx/0.1.1/pytwinx-0.1.1.tar.gz

---
+++ packages/pytwinx/pytwinx-0.1.1.tar.gz/pytwinx-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages 
+setup( 
+       name="pytwinx",    # pypi project name
+       version="0.1.1", 
+       packages=find_packages(), 
+       description="twinx package",
+       python_requires='>=3.6',
+)
