2593fa9a0f400bbc3be7454558a454939e69dd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 05:08:50 +0000

    Add package gh-license/0.3.0/gh-license-0.3.0.tar.gz

---
+++ packages/gh-license/gh-license-0.3.0.tar.gz/gh-license-0.3.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+from setup.setup_hook import PostDevelopCommand
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name="gh-license",
+    version="0.3.0",
+    author="Mte90",
+    author_email="mte90net@gmail.com",
+    url="https://github.com/Mte90/GH-License",
+    install_requires=open(os.path.join(BASE_DIR, "requirements.txt")).readlines(),
+    license="GPLv3",
+    description="Scan github, bitbucket or other providers repositories for missing license files and add them",
+    download_url='https://github.com/Mte90/GH-License/tarball/0.3.0',
+    package_data={"ghlicense": ["*.json"]},
+    packages=find_packages(exclude=["setup", "setup.*"]),
+    entry_points={
+        'console_scripts': [
+            'gh-license=ghlicense.cmd:main'
+        ]
+    },
+    cmdclass={
+        'develop': PostDevelopCommand
+    }
+)
