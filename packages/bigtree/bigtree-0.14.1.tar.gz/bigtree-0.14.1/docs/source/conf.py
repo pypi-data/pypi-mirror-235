8291a617ab5a5089e3f3c657dbc324de237d1779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:08:44 +0000

    Add package bigtree/0.14.1/bigtree-0.14.1.tar.gz

---
+++ packages/bigtree/bigtree-0.14.1.tar.gz/bigtree-0.14.1/docs/source/conf.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../.."))
+import bigtree
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "bigtree"
+copyright = "2022, Kay Jan WONG"
+author = "Kay Jan WONG"
+release = bigtree.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "autodocsumm",
+    "myst_parser",
+    "sphinxemoji.sphinxemoji",
+    "sphinx.ext.mathjax",
+    "rtds_action",
+]
+autodoc_default_options = {"autosummary": True}
+sphinxemoji_style = "twemoji"
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "Python"
+
+# GitHub Action Integration
+rtds_action_github_repo = "kayjan/bigtree"
+rtds_action_path = "../build/"
+rtds_action_artifact_prefix = "docs-"
+rtds_action_github_token = os.getenv("GH_TOKEN", "")
+rtds_action_error_if_missing = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "karma_sphinx_theme"
+html_static_path = ["_static"]
+html_favicon = "_static/favicon.ico"
+html_logo = "_static/favicon.ico"
+
+
+def setup(app):
+    app.add_css_file("custom.css")
