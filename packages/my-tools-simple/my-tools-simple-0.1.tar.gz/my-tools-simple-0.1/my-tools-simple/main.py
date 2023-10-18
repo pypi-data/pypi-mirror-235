a6d66d402488ed06d1611ca9a1fbcf9b65ce650c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:07:36 +0000

    Add package my-tools-simple/0.1/my-tools-simple-0.1.tar.gz

---
+++ packages/my-tools-simple/my-tools-simple-0.1.tar.gz/my-tools-simple-0.1/my-tools-simple/main.py
@@ -0,0 +1,8 @@
+def helloworld(message: str) -> print:
+    
+    """
+    Função responsavel por gerar uma simples mensagem de console de boas vindas com a mensagem definida no parametro. 
+    """
+    
+    return print(f'{str(message)}')
+
---
+++ packages/my-tools-simple/my-tools-simple-0.1.tar.gz/my-tools-simple-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-tools-simple',
+    version='0.1',
+    packages=find_packages(),
+    description='Projeto simples com funções usuais do dia a dia como enviar emails smtp e validar emails',
+    long_description=open('README.md').read(),
+    author='Camilo Costa de Carvalho',
+    author_email='camilo.costa1993@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='tools, email-tools, email-validation',
+    install_requires=[],
+)
