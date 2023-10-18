50648e1ca99f9680eefa5e43a56c456e811896fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:07:58 +0000

    Add package superFunc/1.1.0/superFunc-1.1.0-py3-none-any.whl

---
+++ packages/superFunc/superFunc-1.1.0-py3-none-any.whl/superFunc/func.py
@@ -0,0 +1,42 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+
+def levenshtein_distance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
+
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
