050be442aab6fdd3c27f9d2dd8ecb5cd3aa30413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 03:12:52 +0000

    Add package fhnw-thermo-tools/0.0.8/fhnw_thermo_tools-0.0.8.tar.gz

---
+++ packages/fhnw-thermo-tools/fhnw_thermo_tools-0.0.8.tar.gz/fhnw_thermo_tools-0.0.8/src/fhnw_thermo_tools/globals.py
@@ -0,0 +1,5 @@
+hfg0 = 2500  # [kJ/kg] Verdampfungsenthalpie Wasser bei 0°C
+CpL = 1.004  # [kJ/kgK] Cp Luft
+CpW = 1.86  # [kJ/kgK] Cp Wasser (Dampf)
+CW = 4.19  # [kJ/kgK] Cp Wasser (flüssig)
+CtoK = 273.15  # [K] Temperaturkonstante für Umrechnung °C -> K
