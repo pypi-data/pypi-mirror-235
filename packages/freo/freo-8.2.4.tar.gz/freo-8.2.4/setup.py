5ad0039a18d69523c0696c82a1494968fe0c1087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 04:06:29 +0000

    Add package freo/8.2.4/freo-8.2.4.tar.gz

---
+++ packages/freo/freo-8.2.4.tar.gz/freo-8.2.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='freo',
+    version='8.2.4',
+    packages=find_packages(include=['freo', 'freo.*']),
+    description='This is the almighty freo package'
+)
