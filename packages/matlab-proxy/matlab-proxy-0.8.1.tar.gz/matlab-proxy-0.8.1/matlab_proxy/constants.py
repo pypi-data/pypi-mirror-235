fbea2726a82765dbb3599737f4547bd088bb9ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 03:12:14 +0000

    Add package matlab-proxy/0.8.1/matlab-proxy-0.8.1.tar.gz

---
+++ packages/matlab-proxy/matlab-proxy-0.8.1.tar.gz/matlab-proxy-0.8.1/matlab_proxy/constants.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2023 The MathWorks, Inc.
+
+"""This module defines project-level constants"""
+CONNECTOR_SECUREPORT_FILENAME = "connector.securePort"
+VERSION_INFO_FILE_NAME = "VersionInfo.xml"
+MAX_HTTP_REQUEST_SIZE = 500_000_000  # 500MB
