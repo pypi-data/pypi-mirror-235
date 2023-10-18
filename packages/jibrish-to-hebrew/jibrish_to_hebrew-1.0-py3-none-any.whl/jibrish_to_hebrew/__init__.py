e38ec6c2f447724b57e5717a01a0e4144e277938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:07:59 +0000

    Add package jibrish-to-hebrew/1.0/jibrish_to_hebrew-1.0-py3-none-any.whl

---
+++ packages/jibrish-to-hebrew/jibrish_to_hebrew-1.0-py3-none-any.whl/jibrish_to_hebrew/__init__.py
@@ -0,0 +1,3 @@
+from jibrish_to_hebrew import jibrish_to_hebrew  # Import the function(s) you want to expose
+
+__all__ = ['jibrish_to_hebrew']  # List the functions or objects you want to make available
