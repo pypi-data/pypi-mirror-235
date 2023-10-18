1050965f61c1d9c41c66f777405cb8f365ff1722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 06:07:51 +0000

    Add package haupt/2.0.0rc52/haupt-2.0.0rc52-py3-none-any.whl

---
+++ packages/haupt/haupt-2.0.0rc52-py3-none-any.whl/haupt/orchestration/executor/subscriptions/run.py
@@ -0,0 +1,11 @@
+from haupt.common.events.registry import run
+from haupt.orchestration import executor
+
+executor.subscribe(run.RunCreatedEvent)
+executor.subscribe(run.RunResumedActorEvent)
+executor.subscribe(run.RunStoppedActorEvent)
+executor.subscribe(run.RunApprovedActorEvent)
+executor.subscribe(run.RunNewStatusEvent)
+executor.subscribe(run.RunDoneEvent)
+executor.subscribe(run.RunDeletedActorEvent)
+executor.subscribe(run.RunNewArtifactsEvent)
