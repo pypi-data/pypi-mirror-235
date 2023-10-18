59d71ff65c7434f2799c75b4ab711a8dd5a5d846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-18 05:08:23 +0000

    Add package Starlette-Login/0.2.1/Starlette_Login-0.2.1-py3-none-any.whl

---
+++ packages/Starlette-Login/Starlette_Login-0.2.1-py3-none-any.whl/starlette_login/backends.py
@@ -0,0 +1,55 @@
+import asyncio
+import typing as t
+
+from starlette.requests import HTTPConnection
+
+from .login_manager import LoginManager
+from .mixins import UserMixin
+from .utils import create_identifier
+
+
+class BaseAuthenticationBackend:
+    # name: str = None
+
+    async def authenticate(self, request: HTTPConnection):
+        ...  # pragma: no cover
+
+
+class SessionAuthBackend(BaseAuthenticationBackend):
+    def __init__(self, login_manager: LoginManager):
+        self.login_manager = login_manager
+
+    async def authenticate(
+        self, conn: HTTPConnection
+    ) -> t.Optional[UserMixin]:
+        # Load user from session
+        config = self.login_manager.config
+        remember_cookie = config.REMEMBER_COOKIE_NAME
+        session_fresh = config.SESSION_NAME_FRESH
+
+        session = conn.session.get(config.SESSION_NAME_ID)
+        identifier = create_identifier(conn)
+
+        if identifier != session:
+            if self.login_manager.protection_is_strong():
+                # Strong protection
+                for key in config.session_keys:
+                    conn.session.pop(key, None)
+                conn.session[remember_cookie] = "clear"
+            else:
+                conn.session[session_fresh] = False
+        user_id = conn.session.get(config.SESSION_NAME_KEY)
+
+        if user_id is None and conn.session.get(remember_cookie) != "clear":
+            cookie = conn.cookies.get(config.COOKIE_NAME)
+            if cookie:
+                user_id = self.login_manager.get_cookie(cookie)
+                conn.session[session_fresh] = False
+
+        if user_id is None:
+            return None
+        elif asyncio.iscoroutinefunction(self.login_manager.user_loader):
+            user = await self.login_manager.user_loader(conn, user_id)
+        else:
+            user = self.login_manager.user_loader(conn, user_id)
+        return user
---
+++ packages/Starlette-Login/Starlette_Login-0.2.1-py3-none-any.whl/starlette_login/middleware.py
@@ -0,0 +1,68 @@
+import typing as t
+
+from starlette.requests import HTTPConnection
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
+
+from .backends import BaseAuthenticationBackend
+from .login_manager import LoginManager
+
+
+class AuthenticationMiddleware:
+    def __init__(
+        self,
+        app: ASGIApp,
+        backend: BaseAuthenticationBackend,
+        login_manager: LoginManager,
+        excluded_dirs: t.Optional[t.List[str]] = None,
+        allow_websocket: bool = True,
+    ):
+        self.app = app
+        self.backend = backend
+        self.excluded_dirs = excluded_dirs or []
+        self.login_manager = login_manager
+        self.secret_key = login_manager.secret_key
+        self.allow_websocket = allow_websocket
+
+    async def __call__(
+        self, scope: Scope, receive: Receive, send: Send
+    ) -> None:
+        if self.allow_websocket is False and scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+        elif scope["type"] not in ("http", "websocket"):
+            await self.app(scope, receive, send)
+            return
+
+        # Excluded prefix path. E.g. /static
+        for prefix_dir in self.excluded_dirs:
+            if scope["path"].startswith(prefix_dir):
+                await self.app(scope, receive, send)
+                return
+
+        conn = HTTPConnection(scope=scope, receive=receive)
+
+        user = await self.backend.authenticate(conn)
+        if not user or user.is_authenticated is False:
+            conn.scope["user"] = self.login_manager.anonymous_user_cls()
+        else:
+            conn.scope["user"] = user
+
+        async def custom_send(message: Message):
+            user_ = conn.scope["user"]
+            if user and user_.is_authenticated:
+                operation = conn.session.get(
+                    self.login_manager.config.REMEMBER_COOKIE_NAME
+                )
+                if operation == "set":
+                    message = self.login_manager.set_cookie(
+                        message=message, user_id=user_.identity
+                    )
+                elif operation == "clear":
+                    try:
+                        del conn.cookies[self.login_manager.config.COOKIE_NAME]
+                    except KeyError:
+                        pass
+            await send(message)
+
+        await self.app(scope, receive, custom_send)
+        return
